16b073e8ec313da7ac73219956bc533c83221316
manon618 <manon2029@gmail.com>
2023-08-21 14:47:11 +0700

    test

--- app/Http/Controllers/PDFcasesController.php
+++ app/Http/Controllers/PDFcasesController.php
@@ -4,14 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Cases;
-use PDF;
+use Dompdf\Dompdf;
 class PDFcasesController extends Controller
 {
     //
     public function pdf()
     {
-        $Cases = Cases::all();
-        $pdf = PDF::loadView('reportcase.pdf',['Cases' => $Cases]);
-        return @$pdf->stream();
+    $cases = Cases::all();
+    $pdf = new Dompdf();
+    $pdf->loadHtml(view('reportcase.pdf', ['Cases' => $cases])->render());
+
+    // (Optional) Set paper size and orientation
+    $pdf->setPaper('A4', 'portrait');
+
+    // Render the PDF
+    $pdf->render();
+
+    // Stream the PDF to the browser
+    return $pdf->stream('report.pdf');
     }
 }
