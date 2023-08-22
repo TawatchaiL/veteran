44dda877cf10db6793c325edfe5eca0aa3b665db
manon618 <manon2029@gmail.com>
2023-08-22 14:37:37 +0700

    card

--- resources/views/cases/index.blade.php
+++ resources/views/cases/index.blade.php
@@ -49,7 +49,7 @@
 
             <div class="row">
                 <div class="col-12">
-                    <div class="card card-success">
+                    <div class="card card-primary">
                         <div class="card-header">
                             <h3 class="card-title"><i class="fa-solid fa-clipboard"></i> เรื่องที่ติดต่อ</h3>
                             <div class="card-tools">
