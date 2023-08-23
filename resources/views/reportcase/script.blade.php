1b84cbe5721625ecb0dff0c54b3cea23367de431
meeh2029 <manon2029@gmail.com>
2023-08-23 20:57:11 +0700

    chart pdf

--- resources/views/reportcase/script.blade.php
+++ resources/views/reportcase/script.blade.php
@@ -1,3 +1,4 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.5.3/html2canvas.min.js"></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.min.js'></script>
 <script>
     $(document).ready(function() {
