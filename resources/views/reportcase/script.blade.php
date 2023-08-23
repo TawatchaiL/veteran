2a799b2c9a8d4963591d1a7106dbe20c281655eb
meeh2029 <manon2029@gmail.com>
2023-08-23 21:21:35 +0700

    a4

--- resources/views/reportcase/script.blade.php
+++ resources/views/reportcase/script.blade.php
@@ -12,21 +12,19 @@
                  pdf.addImage(imgData, 'PNG', 0, 0);
                  pdf.save("chart.pdf");
              });  */
+            var pdfWidth = 595.28; // Width of A4 in points (1 point = 1/72 inch)
+            var pdfHeight = 841.89; // Height of A4 in points
+            var pdf = new jsPDF({
+                unit: 'pt', // Use points as the unit for measurements
+                format: [pdfWidth, pdfHeight] // Set the format to A4 size
+            });
+
             var chartContainer = document.querySelector("#bar_chart_div");
 
             html2canvas(chartContainer).then(canvas => {
                 var imgData = canvas.toDataURL("image/png");
-                var imgWidth = canvas.width;
-                var imgHeight = canvas.height;
 
-                // Create a PDF with the same dimensions as the image
-                var pdf = new jsPDF({
-                    unit: 'px',
-                    format: [imgWidth,
-                        imgHeight] // Use image dimensions for PDF dimensions
-                });
-
-                pdf.addImage(imgData, 'PNG', 0, 0);
+                pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight); // Add the image
                 pdf.save("chart.pdf");
             });
 
