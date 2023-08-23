578967f2420d8822e844ae2751b19d019c262b69
meeh2029 <manon2029@gmail.com>
2023-08-23 21:30:11 +0700

    a4

--- resources/views/reportcase/script.blade.php
+++ resources/views/reportcase/script.blade.php
@@ -12,23 +12,24 @@
                  pdf.addImage(imgData, 'PNG', 0, 0);
                  pdf.save("chart.pdf");
              });  */
-            var pdf = new jsPDF();
-            var chartContainer = document.querySelector("#bar_chart_div");
-
-            // Get the actual dimensions of the image within the container
-            var imgWidth = chartContainer.querySelector("img").width;
-            var imgHeight = chartContainer.querySelector("img").height;
-
-            // Create a PDF with the same dimensions as the image
-            pdf = new jsPDF({
-                unit: 'px',
-                format: [imgWidth, imgHeight]
+            var pdfWidth = 595.28; // Width of A4 in points (1 point = 1/72 inch)
+            var pdfHeight = 841.89; // Height of A4 in points
+            var pdf = new jsPDF({
+                unit: 'pt', // Use points as the unit for measurements
+                format: [pdfWidth, pdfHeight] // Set the format to A4 size
             });
+
+            var chartContainer = document.querySelector("#bar_graph");
 
             html2canvas(chartContainer).then(canvas => {
                 var imgData = canvas.toDataURL("image/png");
 
-                pdf.addImage(imgData, 'PNG', 0, 0);
+                var imgWidth = pdfWidth; // Use the same width as PDF
+                var imgHeight = (canvas.height * imgWidth) / canvas
+                .width; // Calculate proportional height
+
+                pdf.addImage(imgData, 'PNG', 0, 0, imgWidth,
+                imgHeight); // Add the resized image
                 pdf.save("chart.pdf");
             });
         });
