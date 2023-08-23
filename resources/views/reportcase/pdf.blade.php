248a9f00a14dc8f6288e59a4d31d85d9b63a3bd1
Taitawatchai <sc_player01@hotmail.com>
2023-08-23 10:11:10 +0700

    1245

--- resources/views/reportcase/pdf.blade.php
+++ resources/views/reportcase/pdf.blade.php
@@ -1,33 +1,3 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-<style>
-    @font-face {
-        font-family: 'THSarabunNew';
-        font-style: normal;
-        font-weight: normal;
-        src: url("{{ public_path('fonts/THSarabunNew.ttf') }}") format('truetype');
-    }
-    @font-face {
-        font-family: 'THSarabunNew';
-        font-style: normal;
-        font-weight: bold;
-        src: url("{{ public_path('fonts/THSarabunNew Bold.ttf') }}") format('truetype');
-    }
-    @font-face {
-        font-family: 'THSarabunNew';
-        font-style: italic;
-        font-weight: normal;
-        src: url("{{ public_path('fonts/THSarabunNew Italic.ttf') }}") format('truetype');
-    }
-    @font-face {
-        font-family: 'THSarabunNew';
-        font-style: italic;
-        font-weight: bold;
-        src: url("{{ public_path('fonts/THSarabunNew BoldItalic.ttf') }}") format('truetype');
-    }
-    body {
-        font-family: "THSarabunNew";
-    }
-</style>
 <section class="content">
         <div class="container-fluid">
             <div class="row">
