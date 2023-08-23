482eedad4322309ee1b020e334f8b739858a4d8e
TawatchaiL <tawatchai.winit@gmail.com>
2023-08-24 00:25:31 +0700

    fgfdgdg

--- resources/views/reporttop10out/index.blade.php
+++ resources/views/reporttop10out/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('style')
-    @include('reportcase.style')
+    @include('reporttop10out.style')
 @endsection
 
 
@@ -118,50 +118,83 @@
                 </div>
 
             </div>
-                    <div class="card">
-                            <div class="card card-success card-tabs">
-                                <div class="card-header p-0 pt-1">
-                                    <ul class="nav nav-tabs" id="custom-tabs-one-tab" role="tablist">
-                                        <li class="nav-item">
-                                            <a class="nav-link active" id="custom-tabs-one-home-tab" data-toggle="pill"
-                                                href="#custom-tabs-one-home" role="tab" aria-controls="custom-tabs-one-home"
-                                                aria-selected="true">Bar Graph</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" id="custom-tabs-one-profile-tab" data-toggle="pill"
-                                                href="#custom-tabs-one-line" role="tab"
-                                                aria-controls="custom-tabs-one-profile"
-                                                aria-selected="false">Line Graph</a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" id="custom-tabs-one-profile-tab" data-toggle="pill"
-                                                href="#custom-tabs-one-pie" role="tab"
-                                                aria-controls="custom-tabs-one-profile"
-                                                aria-selected="false">Pie Graph</a>
-                                        </li>
-                                    </ul>
+            <div class="card">
+                <div class="card card-success card-tabs">
+                    <div class="card-header p-0 pt-1">
+                        <ul class="nav nav-tabs" id="custom-tabs-one-tab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="custom-tabs-one-home-tab" data-toggle="pill"
+                                    href="#custom-tabs-one-home" role="tab" aria-controls="custom-tabs-one-home"
+                                    aria-selected="true">Bar Graph</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="custom-tabs-one-profile-tab" data-toggle="pill"
+                                    href="#custom-tabs-one-line" role="tab"
+                                    aria-controls="custom-tabs-one-profile"
+                                    aria-selected="false">Line Graph</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="custom-tabs-one-profile-tab" data-toggle="pill"
+                                    href="#custom-tabs-one-pie" role="tab"
+                                    aria-controls="custom-tabs-one-profile"
+                                    aria-selected="false">Pie Graph</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-body">
+                        <div class="tab-content" id="custom-tabs-one-tabContent">
+                            <div class="tab-pane fade show active" id="custom-tabs-one-home" role="tabpanel" aria-labelledby="custom-tabs-one-home-tab">
+                                <div class="col-xs-12 col-sm-12 col-md-12 align-self-end text-right">
+                                    <button id="download_bar" class="btn btn-info">
+                                        <i class="fas fa-file-pdf"></i> บันทึกเป็น PDF
+                                    </button>
+                                    <button id="download_bar_img" class="btn btn-warning">
+                                        <i class="fas fa-file-image"></i> บันทึกเป็นรูป
+                                    </button>
+                                    <button id="print_bar" class="btn btn-secondary">
+                                        <i class="fas fa-print"></i> พิมพ์กราฟ
+                                    </button>
                                 </div>
-                                <div class="card-body">
-                                    <div class="tab-content" id="custom-tabs-one-tabContent">
-                                        <div class="tab-pane fade show active" id="custom-tabs-one-home" role="tabpanel" aria-labelledby="custom-tabs-one-home-tab">
-                                            <div class="col-sm-6">
-                                                {!! $chart1->renderHtml() !!}
-                                            </div>
-                                        </div>
-                                        <div class="tab-pane fade" id="custom-tabs-one-line" role="tabpanel" aria-labelledby="custom-tabs-one-line-tab">
-                                            <div class="col-sm-6">
-                                                    {!! $chart2->renderHtml() !!}
-                                            </div>
-                                        </div>
-                                        <div class="tab-pane fade" id="custom-tabs-one-pie" role="tabpanel" aria-labelledby="custom-tabs-one-pie-tab">
-                                            <div class="col-sm-6">
-                                                    {!! $chart3->renderHtml() !!} 
-                                            </div>
-                                        </div>
-                                    </div>
+                                <div class="col-sm-10 mx-auto text-center" id="bar_chart_div">
+                                    {!! $chart1->renderHtml() !!}
                                 </div>
                             </div>
-        </div>
+                            <div class="tab-pane fade" id="custom-tabs-one-line" role="tabpanel" aria-labelledby="custom-tabs-one-line-tab">
+                                <div class="col-xs-12 col-sm-12 col-md-12 align-self-end text-right">
+                                    <button id="download_bar" class="btn btn-info">
+                                        <i class="fas fa-file-pdf"></i> บันทึกเป็น PDF
+                                    </button>
+                                    <button id="download_bar_img" class="btn btn-warning">
+                                        <i class="fas fa-file-image"></i> บันทึกเป็นรูป
+                                    </button>
+                                    <button id="print_bar" class="btn btn-secondary">
+                                        <i class="fas fa-print"></i> พิมพ์กราฟ
+                                    </button>
+                                </div>
+                                <div class="col-sm-10 mx-auto text-center" id="bar_chart_div">
+                                        {!! $chart2->renderHtml() !!}
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="custom-tabs-one-pie" role="tabpanel" aria-labelledby="custom-tabs-one-pie-tab">
+                                <div class="col-xs-12 col-sm-12 col-md-12 align-self-end text-right">
+                                    <button id="download_bar" class="btn btn-info">
+                                        <i class="fas fa-file-pdf"></i> บันทึกเป็น PDF
+                                    </button>
+                                    <button id="download_bar_img" class="btn btn-warning">
+                                        <i class="fas fa-file-image"></i> บันทึกเป็นรูป
+                                    </button>
+                                    <button id="print_bar" class="btn btn-secondary">
+                                        <i class="fas fa-print"></i> พิมพ์กราฟ
+                                    </button>
+                                </div>
+                                <div class="col-sm-10 mx-auto text-center" id="bar_chart_div">
+                                        {!! $chart3->renderHtml() !!} 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+</div>
 
     </section>
 @endsection
