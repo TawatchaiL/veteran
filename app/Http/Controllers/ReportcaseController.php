4e32e2b2a97ee999d0a807cca7fed30fe9c40bc0
manon618 <manon2029@gmail.com>
2023-08-23 10:57:51 +0700

    chart color animate

--- app/Http/Controllers/ReportcaseController.php
+++ app/Http/Controllers/ReportcaseController.php
@@ -58,6 +58,13 @@ class ReportcaseController extends Controller
             'model' => 'App\Models\Cases',
             'group_by_field' => 'agent',
             'chart_type' => 'bar',
+            'colors' => ['#ff0000', '#00ff00', '#0000ff'],
+            'options' => [
+                'animation' => [
+                    'duration' => 1000, // Animation duration in milliseconds
+                    'easing' => 'easeOutQuart', // Easing function for animation
+                ],
+            ],
         ];
         $chart1 = new LaravelChart($chart_options);
 
@@ -67,6 +74,13 @@ class ReportcaseController extends Controller
             'model' => 'App\Models\Cases',
             'group_by_field' => 'agent',
             'chart_type' => 'line',
+            'colors' => ['#ff0000', '#00ff00', '#0000ff'],
+            'options' => [
+                'animation' => [
+                    'duration' => 1000, // Animation duration in milliseconds
+                    'easing' => 'easeOutQuart', // Easing function for animation
+                ],
+            ],
         ];
         $chart2 = new LaravelChart($chart_options);
         $chart_options = [
@@ -75,115 +89,17 @@ class ReportcaseController extends Controller
             'model' => 'App\Models\Cases',
             'group_by_field' => 'agent',
             'chart_type' => 'pie',
+            'colors' => ['#ff0000', '#00ff00', '#0000ff'],
+            'options' => [
+                'animation' => [
+                    'duration' => 1000, // Animation duration in milliseconds
+                    'easing' => 'easeOutQuart', // Easing function for animation
+                ],
+            ],
+
         ];
         $chart3 = new LaravelChart($chart_options);
 
         return view('reportcase.index', compact('chart1', 'chart2', 'chart3'));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-        $validator =  Validator::make($request->all(), [
-            //'code' => 'required|string|max:10',
-            'name' => 'required|string|max:255',
-            //'postcode' => 'int|max:10',
-            /* 'email' => 'required|string|email|max:255',
-            'address' => 'required|string|max:255',
-            'postcode' => 'required|string|max:10',
-            'telephone' => 'required|string|max:20',*/
-        ]);
-
-
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()->all()]);
-        }
-
-        $input = $request->all();
-        $contract = Case_type::create($input);
-        return response()->json(['success' => 'เพิ่ม ประเภทการติดต่อ เรียบร้อยแล้ว']);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-
-        $data = Case_type::find($id);
-        return response()->json(['data' => $data]);
-    }
-
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, $id)
-    {
-        $rules = [
-            'name' => 'required|string|max:255',
-            //'postcode' => 'integer|max:10',
-
-        ];
-
-
-        $validator =  Validator::make($request->all(), $rules);
-
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()->all()]);
-        }
-
-        $companyd = [
-            'name' => $request->get('name'),
-        ];
-
-        $company = Case_type::find($id);
-        $company->update($companyd);
-
-        return response()->json(['success' => 'แก้ไข ประเภทการติดต่อ เรียบร้อยแล้ว']);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Request $request)
-    {
-        $id = $request->get('id');
-        Case_type::find($id)->delete();
-        return ['success' => true, 'message' => 'ลบ ประเภทการติดต่อ เรียบร้อยแล้ว'];
-    }
-
-    public function destroy_all(Request $request)
-    {
-
-        $arr_del  = $request->get('table_records'); //$arr_ans is Array MacAddress
-
-        for ($xx = 0; $xx < count($arr_del); $xx++) {
-            Case_type::find($arr_del[$xx])->delete();
-        }
-
-        return redirect('casetype')->with('success', 'ลบ ประเภทการติดต่อ เรียบร้อยแล้ว');
-    } //
 }
