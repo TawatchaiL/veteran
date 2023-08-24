49471b8ea9701e6e2c78ab834febf1601b74fb7a
Taitawatchai <sc_player01@hotmail.com>
2023-08-24 14:01:07 +0700

    7564596464

---
+++ app/Models/Models/Timeslot.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Timeslot extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'timeslot'
+    ];
+}
