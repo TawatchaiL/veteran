ee22511810009a49335c79b6d2b5df415d948418
Taitawatchai <sc_player01@hotmail.com>
2023-08-24 14:27:37 +0700

    afsfs

--- app/Http/Controllers/ReportcaseinbyhourController.php
+++ app/Http/Controllers/ReportcaseinbyhourController.php
@@ -39,7 +39,7 @@ class ReportcaseinbyhourController extends Controller
         if ($request->ajax()) {
             //sleep(2);
                 $datas = DB::table('timeslot')
-                    ->selectRaw("timeslot.timeslot as timelabel, c.numberhour, 5 as sumt")
+                    ->selectRaw("timeslot.timeslot as timelabel, c.numberhour, if(c.numberhour IS NULL,0,c.total_cases) as sumt")
                     ->leftJoin(DB::raw("(SELECT
                         CASE
                             WHEN TIME(created_at) < '01:00:00' THEN '00:00-01:00'
