b0c45d495c7ac97367cdbbe99a28435992e3125f
Taitawatchai <sc_player01@hotmail.com>
2023-08-25 14:07:41 +0700

    546767

--- app/Http/Controllers/ReporttopinController.php
+++ app/Http/Controllers/ReporttopinController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use LaravelDaily\LaravelCharts\Classes\LaravelChart;
+use App\Services\GraphService;
 
 class ReporttopinController extends Controller
 {
