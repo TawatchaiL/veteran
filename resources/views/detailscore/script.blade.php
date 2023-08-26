ca18073178fb67547d1780642ffabe953cc02c2c
TawatchaiL <tawatchai.winit@gmail.com>
2023-08-26 14:53:41 +0700

    8676

--- resources/views/detailscore/script.blade.php
+++ resources/views/detailscore/script.blade.php
@@ -383,7 +383,7 @@
                             fontSize: 16
                         };
                         // กำหนดความกว้างของ header แต่ละคอลัมน์หัวข้อ
-                        doc.content[1].table.widths = [50, '*', '*', '*'];
+                        doc.content[1].table.widths = [50, '*', '*', '*', '*', '*'];
                         doc.styles.tableHeader.fontSize = 16; // กำหนดขนาด font ของ header
                         // Add cell borders
                         doc.content[1].table.layout = {
