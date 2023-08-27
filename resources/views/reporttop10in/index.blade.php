3d7bfea34fb1a8fdb5c6265d71882a10a1749b3b
manon618 <manon2029@gmail.com>
2023-08-27 13:58:48 +0700

    fix

--- resources/views/reporttop10in/index.blade.php
+++ resources/views/reporttop10in/index.blade.php
@@ -14,6 +14,11 @@
                     <div class="card card-info">
                         <div class="card-header">
                             <h3 class="card-title"><i class="fa-solid fa-magnifying-glass"></i> Filter</h3>
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                            </div>
                         </div>
                         <div class="card-body">
                             <div class="col-sm-12">
@@ -62,6 +67,9 @@
                         <div class="card-header">
                             <h3 class="card-title"><i class="fa-solid fa-print"></i> 10 อันดับเบอร์ภายใน</h3>
                             <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="maximize">
+                                    <i class="fas fa-expand"></i>
+                                </button>
                                 <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
                                     <i class="fas fa-minus"></i>
                                 </button>
