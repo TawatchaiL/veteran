1edf6e80735caef28814c0b491669dcda2269289
TawatchaiL <tawatchai.winit@gmail.com>
2023-08-27 14:09:18 +0700

    /-ภ/-ภ

---
+++ app/Http/Controllers/VoicerecordController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Cases;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use LaravelDaily\LaravelCharts\Classes\LaravelChart;
+use App\Services\GraphService;
+
+class VoicerecordController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //$this->middleware('auth');
+        $this->middleware('permission:contact-list|contact-create|contact-edit|contact-delete', ['only' => ['index', 'show']]);
+        $this->middleware('permission:contact-create', ['only' => ['create', 'store']]);
+        $this->middleware('permission:contact-edit', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:contact-delete', ['only' => ['destroy']]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $datas = DB::table('cases')
+        ->select(DB::raw('DATE(created_at) as cdate'), DB::raw('TIME(created_at) as ctime'),'telno','agent')
+        ->get();
+
+        if ($request->ajax()) {
+
+            return datatables()->of($datas)
+                ->editColumn('checkbox', function ($row) {
+                    return '<input type="checkbox" id="" class="flat" name="table_records[]" value="" >';
+                })->rawColumns(['checkbox', 'action'])
+                ->addColumn('playerbutton', 'เล่นไฟล์เสียง')
+                //->editColumn('duration', concat(rand(1,3),':',rand(0,60)))
+                ->toJson();
+        }
+
+        return view('voicerecord.index');
+    }
+
+}
