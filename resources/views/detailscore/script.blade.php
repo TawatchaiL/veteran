9ee636520726f24810766304c0b1e89613756084
Taitawatchai <sc_player01@hotmail.com>
2023-08-29 14:10:47 +0700

    gsafsaf

--- resources/views/detailscore/script.blade.php
+++ resources/views/detailscore/script.blade.php
@@ -383,7 +383,7 @@
                             fontSize: 16
                         };
                         // กำหนดความกว้างของ header แต่ละคอลัมน์หัวข้อ
-                        doc.content[1].table.widths = [50, '*', '*', '*', '*', '*'];
+                        doc.content[1].table.widths = [90, 90, 90, 90, 90, '*'];
                         doc.styles.tableHeader.fontSize = 16; // กำหนดขนาด font ของ header
                         // Add cell borders
                         doc.content[1].table.layout = {
@@ -417,6 +417,14 @@
                             doc.content[1].table.body[i][0].alignment =
                                 'center'; // Align the first column to the center
                             doc.content[1].table.body[i][1].alignment =
+                                'center'; // Align the second column to the right
+                            doc.content[1].table.body[i][2].alignment =
+                                'center'; // Align the first column to the center
+                            doc.content[1].table.body[i][3].alignment =
+                                'center'; // Align the second column to the right
+                            doc.content[1].table.body[i][4].alignment =
+                                'center'; // Align the first column to the center
+                            doc.content[1].table.body[i][5].alignment =
                                 'center'; // Align the second column to the right
                             //doc.content[1].table.body[i][2].alignment =
                             //'center'; // Align the second column to the right
