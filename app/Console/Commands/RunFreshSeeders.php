dcb2765f3aee7fa9f813688f6e51a6c663edd238
manon618 <manon2029@gmail.com>
2023-09-25 15:09:22 +0700

    command

--- app/Console/Commands/RunFreshSeeders.php
+++ app/Console/Commands/RunFreshSeeders.php
@@ -28,7 +28,6 @@ class RunFreshSeeders extends Command
             'CreateAdminUserSeeder',
             'ThcitiesSeed',
             'ThdistrictsSeed',
-            'levelSeeder',
             'ThsubdistrictsSeed',
         ];
 
