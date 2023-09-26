5f4642445ee4b071437d7a8292f76352f588297a
manon618 <manon2029@gmail.com>
2023-09-26 14:20:45 +0700

    x

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
 use App\Models\Contact;
 
@@ -9,14 +7,26 @@ class ContactSeeder extends Seeder
 {
     public function run()
     {
-        // Generate 50 fake contact records
+        // Number of fake contacts to generate
+        $numberOfContacts = 50;
 
-
-     /*    DB::table('contacts')->insert(
-            Contact::factory()->count(50)->create()
-        ); */
-
-
-        Contact::factory()->count(50)->create();
+        for ($i = 1; $i <= $numberOfContacts; $i++) {
+            Contact::create([
+                'hn' => 'HN' . random_int(1000, 9999), // Generate a random health number
+                'fname' => 'First Name ' . $i,
+                'lname' => 'Last Name ' . $i,
+                'homeno' => 'Home Address ' . $i,
+                'moo' => random_int(1, 1000), // Generate a random number for "moo"
+                'road' => 'Road ' . $i,
+                'soi' => 'Soi ' . $i,
+                'city' => 'City ' . $i,
+                'district' => 'District ' . $i,
+                'subdistrict' => 'Subdistrict ' . $i,
+                'postcode' => random_int(10000, 99999), // Generate a random postcode
+                'phoneno' => 'Phone ' . $i,
+                'telhome' => 'Home Phone ' . $i,
+                'workno' => 'Work Phone ' . $i,
+            ]);
+        }
     }
 }
