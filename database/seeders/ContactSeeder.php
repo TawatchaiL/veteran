6005ef94a4382c58c54febd55bc5d34d83b06ffb
manon618 <manon2029@gmail.com>
2023-09-26 14:54:45 +0700

    fix

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -16,40 +16,46 @@ class ContactSeeder extends Seeder
         $numberOfContacts = 200;
 
         for ($i = 1; $i <= $numberOfContacts; $i++) {
-
             $city = ThCity::inRandomOrder()->first();
-            $district = ThDistrict::where('province_id', $city->id)->inRandomOrder()->first();
-            $subDistrict = ThSubDistrict::where('district_id', $district->id)->inRandomOrder()->first();
-            // Generate random phone numbers
-            $phoneno = '0' . mt_rand(200, 999) . '-' . mt_rand(1000, 9999);
-            $telhome = '0' . mt_rand(200, 999) . '-' . mt_rand(1000, 9999);
-            $workno = '0' . mt_rand(200, 999) . '-' . mt_rand(1000, 9999);
 
-            // Generate a random date within a specific range (e.g., last 5 years)
-            $startDate = strtotime('-14 days');
-            $endDate = strtotime('now');
-            $randomTimestamp = mt_rand($startDate, $endDate);
+            if ($city) {
+                $district = ThDistrict::where('province_id', $city->id)->inRandomOrder()->first();
 
-            // Format the random timestamp as a date
-            $randomDate = date('Y-m-d', $randomTimestamp);
+                if ($district) {
+                    $subDistrict = ThSubDistrict::where('district_id', $district->id)->inRandomOrder()->first();
 
-            CrmContact::create([
-                'hn' => 'HN' . random_int(1000, 9999), // Generate a random health number
-                'adddate' => $randomDate, // Set the random date here
-                'fname' => 'First Name ' . $i,
-                'lname' => 'Last Name ' . $i,
-                'homeno' => 'Home Address ' . $i,
-                'moo' => random_int(1, 1000), // Generate a random number for "moo"
-                'road' => 'Road ' . $i,
-                'soi' => 'Soi ' . $i,
-                'city' => $city->id,
-                'district' => $district->id,
-                'subdistrict' => $subDistrict->id,
-                'postcode' => random_int(10000, 99999), // Generate a random postcode
-                'phoneno' => $phoneno, // Set the random phone numbers here
-                'telhome' => $telhome,
-                'workno' => $workno,
-            ]);
+                    // Generate random phone numbers
+                    $phoneno = '0' . mt_rand(200, 999) . '-' . mt_rand(1000, 9999);
+                    $telhome = '0' . mt_rand(200, 999) . '-' . mt_rand(1000, 9999);
+                    $workno = '0' . mt_rand(200, 999) . '-' . mt_rand(1000, 9999);
+
+                    // Generate a random date within a specific range (e.g., last 14 days)
+                    $startDate = strtotime('-14 days');
+                    $endDate = strtotime('now');
+                    $randomTimestamp = mt_rand($startDate, $endDate);
+
+                    // Format the random timestamp as a date
+                    $randomDate = date('Y-m-d', $randomTimestamp);
+
+                    CrmContact::create([
+                        'hn' => 'HN' . random_int(1000, 9999), // Generate a random health number
+                        'adddate' => $randomDate, // Set the random date here
+                        'fname' => 'First Name ' . $i,
+                        'lname' => 'Last Name ' . $i,
+                        'homeno' => 'Home Address ' . $i,
+                        'moo' => random_int(1, 1000), // Generate a random number for "moo"
+                        'road' => 'Road ' . $i,
+                        'soi' => 'Soi ' . $i,
+                        'city' => $city->id,
+                        'district' => $district->id,
+                        'subdistrict' => $subDistrict ? $subDistrict->id : null, // Handle potential null sub-district
+                        'postcode' => random_int(10000, 99999), // Generate a random postcode
+                        'phoneno' => $phoneno, // Set the random phone numbers here
+                        'telhome' => $telhome,
+                        'workno' => $workno,
+                    ]);
+                }
+            }
         }
     }
 }
