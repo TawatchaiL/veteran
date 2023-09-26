251f866547a1b408b6a69e4e40369729788a7992
manon618 <manon2029@gmail.com>
2023-09-26 14:09:49 +0700

    fake

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -1,45 +1,41 @@
 <?php
 
-/* namespace Database\Factories;
+namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory; */
+use App\Models\Contact;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
- */
-/* class ContactFactory extends Factory
-{ */
+class ContactFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Contact::class;
+
     /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-   /*  public function definition(): array
+    public function definition()
     {
         return [
-            //
+            'hn' => $this->faker->unique()->ean8,
+            'fname' => $this->faker->firstName,
+            'lname' => $this->faker->lastName,
+            'homeno' => $this->faker->buildingNumber,
+            'moo' => $this->faker->randomNumber(3),
+            'road' => $this->faker->streetName,
+            'soi' => $this->faker->streetSuffix,
+            'city' => $this->faker->city,
+            'district' => $this->faker->state,
+            'subdistrict' => $this->faker->word,
+            'postcode' => $this->faker->postcode,
+            'phoneno' => $this->faker->phoneNumber,
+            'telhome' => $this->faker->phoneNumber,
+            'workno' => $this->faker->phoneNumber,
         ];
     }
-} */
-
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Faker\Generator as Faker;
-
-$factory->define(Contact::class, function (Faker $faker) {
-    return [
-        'hn' => $faker->unique()->ean8,
-        'fname' => $faker->firstName,
-        'lname' => $faker->lastName,
-        'homeno' => $faker->buildingNumber,
-        'moo' => $faker->randomNumber(3),
-        'road' => $faker->streetName,
-        'soi' => $faker->streetSuffix,
-        'city' => $faker->city,
-        'district' => $faker->state,
-        'subdistrict' => $faker->word,
-        'postcode' => $faker->postcode,
-        'phoneno' => $faker->phoneNumber,
-        'telhome' => $faker->phoneNumber,
-        'workno' => $faker->phoneNumber,
-    ];
-});
+}
--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -10,6 +10,13 @@ class ContactSeeder extends Seeder
     public function run()
     {
         // Generate 50 fake contact records
+
+
+     /*    DB::table('contacts')->insert(
+            Contact::factory()->count(50)->create()
+        ); */
+
+
         Contact::factory()->count(50)->create();
     }
 }
