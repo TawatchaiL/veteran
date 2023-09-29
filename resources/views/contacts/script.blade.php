75d6b8ba75dab983b66cc89626e1fd304ba68e7c
Taitawatchai <sc_player01@hotmail.com>
2023-09-29 08:20:46 +0700

    sdfsf

--- resources/views/contacts/script.blade.php
+++ resources/views/contacts/script.blade.php
@@ -103,13 +103,13 @@
 
         var startDate;
         var endDate;
-
         function datesearch() {
             var currentDate = moment();
             // Set the start date to 7 days before today
-            startDate = moment(currentDate).subtract(7, 'days').format('YYYY-MM-DD');
+            startDate = moment(currentDate).subtract(15, 'days').format('YYYY-MM-DD');
             // Set the end date to the end of the current month
-            endDate = moment(currentDate).endOf('month').format('YYYY-MM-DD');
+            //endDate = moment(currentDate).endOf('month').format('YYYY-MM-DD');
+            endDate = moment().format('YYYY-MM-DD');
         }
 
         function retrieveFieldValues() {
@@ -128,6 +128,7 @@
         }
 
         let daterange = () => {
+
             $('#reservation').daterangepicker({
                 startDate: startDate,
                 endDate: endDate,
@@ -142,7 +143,7 @@
                 //storeFieldValues();
             });
         }
-
+        datesearch();
         daterange();
 
 
