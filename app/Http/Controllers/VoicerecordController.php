aa23bada1275d3e92e24e207c2ef19db77f71832
manon618 <manon2029@gmail.com>
2023-10-01 20:59:35 +0700

    fix

--- app/Http/Controllers/VoicerecordController.php
+++ app/Http/Controllers/VoicerecordController.php
@@ -54,8 +54,9 @@ class VoicerecordController extends Controller
 
         //$remoteData = DB::connection('remote_connection')->table('asteriskcdrdb.cdr')->get();
         //dd($remoteData);
-        $asterisk = $this->asterisk_ami();
-        dd($asterisk);
+        $remote = $this->asterisk_ami();
+        //dd($asterisk);
+        $remote->QueueAdd('1234', "SIP/9999", 0, "Agent1", "hint:9999@ext-local");
 
         $datas = DB::table('cases')
             ->select(DB::raw('DATE(created_at) as cdate'), DB::raw('TIME(created_at) as ctime'), 'telno', 'agent', 'id')
