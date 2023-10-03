8b4edfc896e912aa3167bb3d1f6702950cef049f
manon618 <manon2029@gmail.com>
2023-10-03 20:59:54 +0700

    fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -159,6 +159,7 @@ class LoginController extends Controller
         $phone_state_num = $this->remote->exten_state($request->get('phone'));
         if ($phone_state_num == 4 || $phone_state_num == -1) {
             //check phone status
+            dd($phone_state_num);
             $this->logoff_to_login_phone_error('หมายเลขโทรศัพท์ไม่พร้อมใช้งาน');
         }
 
