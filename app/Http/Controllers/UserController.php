ca0b1ecf3b9fd5c9fb7866d0a68e9a6828c199b6
manon618 <manon2029@gmail.com>
2023-10-03 20:40:23 +0700

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -128,8 +128,8 @@ class UserController extends Controller
             'department_id' => 'required',
             'position_id' => 'required',
             'role' => 'required',
-            'queue' => 'required|array',
-            'agent' => 'required|unique:users,agent_id',
+            //'queue' => 'required|array',
+            //'agent' => 'required|unique:users,agent_id',
         ], [
             'name.required' => 'กรุณาระบุ ชื่อ-นามสกุล ผู้ใช้งาน!',
             'email.required' => 'Username ต้องไม่เป็นค่าว่าง!',
@@ -141,8 +141,8 @@ class UserController extends Controller
             'department_id.required' => 'แผนก ต้องไม่เป็นค่าว่าง!',
             'position_id.required' => 'ตำแหน่ง ต้องไม่เป็นค่าว่าง!',
             'role.required' => 'สิทธิ์การใช้งาน ต้องไม่เป็นค่าว่าง!',
-            'queue.required' => 'กรุณาระบุ Queue ผู้ใช้งาน!',
-            'agent.required' => 'กรุณาระบุ Agent ผู้ใช้งาน!',
+            //'queue.required' => 'กรุณาระบุ Queue ผู้ใช้งาน!',
+            //'agent.required' => 'กรุณาระบุ Agent ผู้ใช้งาน!',
             'agent.unique' => 'Agent นี้ถูกใช้งานแล้ว',
         ]);
 
@@ -154,10 +154,10 @@ class UserController extends Controller
 
         $input = $request->all();
         $input['password'] = Hash::make($input['password']);
-        //$input['agent_id'] = $request->get('agent');
+        $input['agent_id'] = $request->get('agent');
 
         $user = User::create($input);
-        //$user->assignRole($request->input('role'));
+        $user->assignRole($request->input('role'));
 
         /*
         $queueData = [];
