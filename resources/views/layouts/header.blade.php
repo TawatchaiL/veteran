744ecfb2a753d37b8a44fc01ce41b8ecea32b91e
manon618 <manon2029@gmail.com>
2023-10-05 01:09:58 +0700

    fix

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,17 +11,16 @@
                 <b class="text-primary"> โรงพยาบาลพุทธชินราช พิษณุโลก</b> ]
             </a></li> --}}
 
-        <li class="nav-item d-none d-sm-inline-block">
-            <a class="nav-link" href="#" role="button">
-                <span id="phone_state_icon">{!! $temporaryPhoneStatusIcon !!}</span> [
-                <b class="text-primary">{{ $temporaryPhone }} :</b> <b class="text-primary"
-                    id="phone_state">{{ $temporaryPhoneStatus }}</b> ]
-            </a>
-        </li>
+
         <li class="nav-item d-none d-sm-inline-block">
             <a class="btn btn-success" id="ToolbarButton" {{-- data-widget="control-sidebar" data-slide="true" href="#" --}} role="button">
                 <i class="fas fa-xl fa-spin fa-gear"></i> Agent ToolBar
             </a>{{-- faa-shake animated faa-slow fa-wrench --}}
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a class="btn btn-success" data-widget="fullscreen" role="button">
+                <i class="fas fa-xl fa-expand-arrows-alt"></i> ขยาย/ย่อ หน้าจอ
+            </a>
         </li>
         @if (!empty($sidebarc))
             <li class="nav-item d-none d-sm-inline-block"><a href="/" class="nav-link"> [ <i
@@ -57,11 +56,7 @@
     </ul>
 
     <ul class="navbar-nav ml-auto">
-        <li class="nav-item d-none d-sm-inline-block">
-            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                <i class="fas fa-xl fa-expand-arrows-alt"></i> <b class="text-primary">ขยาย/ย่อ หน้าจอ</b>
-            </a>
-        </li>
+
 
         <li class="nav-item dropdown user-menu">
             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
@@ -102,6 +97,13 @@
                     </form>
                 </li>
             </ul>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a class="nav-link" href="#" role="button">
+                <span id="phone_state_icon">{!! $temporaryPhoneStatusIcon !!}</span> [
+                <b class="text-primary">{{ $temporaryPhone }} :</b> <b class="text-primary"
+                    id="phone_state">{{ $temporaryPhoneStatus }}</b> ]
+            </a>
         </li>
 
     </ul>
