3251d018ceaaf703d526bbb26f8001702faf50b0
korwlik23 <korwlik23@gmail.com>
2023-10-04 15:05:59 +0700

    check log_XII

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,6 @@
 
 var token = $('meta[name=csrf-token]').attr('content');
 
-
 function formModal(route) {
 
     $.get(route, function (res) {
@@ -9,23 +8,7 @@ function formModal(route) {
         $('#modal_form_content').html(res);
 
 
-        // $('.changeUrlButton').on('click', () => {
-        //     //const newUrl = 'wav/PinkPanther60.wav'; // Replace with the new URL
-        //     const newUrl = 'wav/2023/10/01/q-4567-8888-20231001-141026-1696169425.161.wav';
-        //     // const newUrl = 'wav/'+$('#vioc').val();
-
-        //     console.log('wav/' + $('#vioc').val());
-        //     console.log(newUrl);
-        //     initializeWaveSurfer(newUrl);
-        // });
-        // $('.vioc').on('click', function () {
-        //     const newUrl = 'wav/' + $('#vioc').val();
-        //     console.log('wav/' + $('#vioc').val());
-        //     console.log(newUrl);
-        //     initializeWaveSurfer(newUrl);
-        // });
-
-        // $("#CreateModal").modal("show");
+        $("#CreateModal").modal("show");
     });
 
 }
