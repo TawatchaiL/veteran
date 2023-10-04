2deb917b6328136d50435f09475ca00e511fd833
korwlik23 <korwlik23@gmail.com>
2023-10-04 14:51:36 +0700

    check log_XI

--- public/js/app.js
+++ public/js/app.js
@@ -9,23 +9,23 @@ function formModal(route) {
         $('#modal_form_content').html(res);
 
 
-        $('.changeUrlButton').on('click', () => {
-            //const newUrl = 'wav/PinkPanther60.wav'; // Replace with the new URL
-            const newUrl = 'wav/2023/10/01/q-4567-8888-20231001-141026-1696169425.161.wav';
-            // const newUrl = 'wav/'+$('#vioc').val();
+        // $('.changeUrlButton').on('click', () => {
+        //     //const newUrl = 'wav/PinkPanther60.wav'; // Replace with the new URL
+        //     const newUrl = 'wav/2023/10/01/q-4567-8888-20231001-141026-1696169425.161.wav';
+        //     // const newUrl = 'wav/'+$('#vioc').val();
 
-            console.log('wav/' + $('#vioc').val());
-            console.log(newUrl);
-            initializeWaveSurfer(newUrl);
-        });
-        $('.vioc').on('click', function () {
-            const newUrl = 'wav/' + $('#vioc').val();
-            console.log('wav/' + $('#vioc').val());
-            console.log(newUrl);
-            initializeWaveSurfer(newUrl);
-        });
+        //     console.log('wav/' + $('#vioc').val());
+        //     console.log(newUrl);
+        //     initializeWaveSurfer(newUrl);
+        // });
+        // $('.vioc').on('click', function () {
+        //     const newUrl = 'wav/' + $('#vioc').val();
+        //     console.log('wav/' + $('#vioc').val());
+        //     console.log(newUrl);
+        //     initializeWaveSurfer(newUrl);
+        // });
 
-        $("#CreateModal").modal("show");
+        // $("#CreateModal").modal("show");
     });
 
 }
