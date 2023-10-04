419e9cfec64bd2294c52fbe62930ab0742353d1e
korwlik23 <korwlik23@gmail.com>
2023-10-04 16:31:17 +0700

    check log_XXXII

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ function formModal(route) {
     $.get(route, function (res) {
         $("#modal_form_content").empty();
         $('#modal_form_content').html(res);
-        $("#CreateModal").modal("show");
+        $("#modal_form").modal("show");
 
 
 
