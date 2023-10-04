d8479b54c2e8f3e25c9ed4609aabdedd40ba469d
korwlik23 <korwlik23@gmail.com>
2023-10-04 11:47:54 +0700

    import form modal

---
+++ resources/views/layouts/modal-form.blade.php
@@ -0,0 +1,16 @@
+{{-- <div class="modal fade" id="modal_form">
+    <div class="modal-dialog">
+        <div class="modal-content" id="modal_form_content">
+
+
+        </div>
+    </div>
+</div> --}}
+<div class="fade modal bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+    aria-hidden="true" id="modal_form">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content" id="modal_form_content">
+            
+        </div>
+    </div>
+</div>
