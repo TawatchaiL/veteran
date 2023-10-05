c2fc49f2037b371225c14be65762224240935068
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-05 16:24:30 +0700

    sfds

--- app/Http/Controllers/CaseType6Controller.php
+++ app/Http/Controllers/CaseType6Controller.php
@@ -77,10 +77,8 @@ class CaseType6Controller extends Controller
      */
     public function store(Request $request)
     {
+        //
 
-
-        $input = $request->all();
-        $contract = CrmCaseType::create($input);
         return response()->json(['success' => 'เพิ่ม ประเภทการติดต่อ เรียบร้อยแล้ว']);
     }
 
