22c89f77e2cfa7b96850f23156461accdf1b1a4b
manon618 <manon2029@gmail.com>
2023-10-05 21:19:05 +0700

    fix

--- app/Services/IssableService.php
+++ app/Services/IssableService.php
@@ -147,8 +147,6 @@ class IssableService
 
     public function agent_break($phone, $idBreak)
     {
-
-dd($idBreak);
         $this->_agent = 'SIP/' . $phone;
         //try {
             $oECCP = $this->_obtenerConexion('ECCP');
