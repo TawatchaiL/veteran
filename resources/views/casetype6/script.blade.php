b6648b37b89da5115dad319b4c9de6e64bb77d93
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-05 16:16:29 +0700

    sfsd

--- resources/views/casetype6/script.blade.php
+++ resources/views/casetype6/script.blade.php
@@ -166,7 +166,7 @@
             }
             for (let c = 1; c < 7; c++) {
                 if (!$('#AddName' + c).prop('disabled')) {
-                    alert('OK');
+                    alert($('#AddName' + c).attr('name').substring(7, 8));
                     var crmname = $('#AddName' + c).val();
                     var p = c - 1;
                     var parent_id = $('#casetype' + p).val();
