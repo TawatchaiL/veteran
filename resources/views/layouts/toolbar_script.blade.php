30f177e8586bcc443be2be0187c3f1eefa7d8de5
manon618 <manon2029@gmail.com>
2023-10-05 15:57:10 +0700

    fix

--- resources/views/layouts/toolbar_script.blade.php
+++ resources/views/layouts/toolbar_script.blade.php
@@ -18,36 +18,53 @@
     };
 
     const get_state_color = (id) => {
-        const classMap = {
-            0: 'btn btn-secondary',
-            1: 'btn btn-success',
-            2: 'btn btn-warning',
-        };
-        return classMap[id] || '';
-    };
-
-    const toggleButtonState = (element, enabled) => {
-        element.prop('disabled', !enabled);
+        if (id === 0) {
+            return 'btn btn-secondary';
+        } else if (id === 1) {
+            return 'btn btn-success';
+        } else if (id === 2) {
+            return 'btn btn-warning';
+        }
     };
 
     const set_state_button = (id) => {
-        const buttonElements = {
-            performance_button: $('#performance_button'),
-            break_group: $('#break_group'),
-            btn_pause: $('#btn_pause'),
-            btn_unbreak: $('#btn_unbreak'),
-            btn_system_logout: $('#btn_system_logout'),
-            btn_agent_logout: $('#btn_agent_logout'),
-            btn_agent_login: $('#btn_agent_login'),
-        };
+        alert(id)
+        let dial_number = $('#dial_number');
+        let dial_button = $('#dial_button');
+        let tranfer_button = $('#dial_number');
+        let performance_button = $('#dial_number');
+        let conf_button = $('#dial_number');
+        let break_group = $('#dial_number');
+        let btn_pause = $('#dial_number');
+        let btn_unbreak = $('#dial_number');
+        let btn_system_logout = $('#dial_number');
+        let btn_agent_logout = $('#dial_number');
+        let btn_agent_login = $('#dial_number');
 
-        toggleButtonState(buttonElements.performance_button, id === 1);
-        toggleButtonState(buttonElements.break_group, id !== 0);
-        toggleButtonState(buttonElements.btn_pause, id === 0);
-        toggleButtonState(buttonElements.btn_unbreak, id === 2);
-        toggleButtonState(buttonElements.btn_system_logout, id === 0);
-        toggleButtonState(buttonElements.btn_agent_logout, id === 1);
-        toggleButtonState(buttonElements.btn_agent_login, id === 0);
+        if (id === 0) {
+            performance_button.attr('disabled', true);
+            break_group.addClass("d-none");
+            btn_pause.attr('disabled', true);
+            btn_unbreak.addClass("d-none");
+            btn_system_logout.attr('disabled', false);
+            btn_agent_logout.addClass("d-none");
+            btn_agent_login.attr('disabled', false);
+        } else if (id === 1) {
+            performance_button.attr('disabled', false);
+            break_group.removeClass("d-none");
+            btn_pause.attr('disabled', false);
+            btn_unbreak.addClass("d-none");
+            btn_system_logout.attr('disabled', true);
+            btn_agent_logout.removeClass("d-none");
+            btn_agent_login.addClass("d-none");
+        } else if (id === 2) {
+            break_group.removeClass("d-none");
+            btn_pause.attr('disabled', true);
+            btn_unbreak.removeClass("d-none");
+            btn_system_logout.attr('disabled', true);
+            btn_agent_logout.addClass("d-none");
+            btn_agent_login.addClass("d-none");
+        }
     };
 
     $(document).on('click', '#btn-agent-login', (e) => {
