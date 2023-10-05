0e47952a5be148369bc60d9acedfa47ed0258d52
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-05 11:23:17 +0700

    ddgd

--- resources/views/casetype6/script.blade.php
+++ resources/views/casetype6/script.blade.php
@@ -318,7 +318,7 @@
         //if(selectnext < 7){
             for (let i = selectno; i < 7; i++) {
                 var selectnext = i + 1;
-                    if($('#casetype' + i).val == '0'){
+                    if($('#casetype' + i).val == ''){
                         alert('OK');
                     }
 
