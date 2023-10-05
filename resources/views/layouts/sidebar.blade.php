4217b60bff6e8c791a78aed9c1875bc978b58361
manon618 <manon2029@gmail.com>
2023-10-05 22:55:34 +0700

    menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,14 +1,14 @@
 <aside class="main-sidebar main-sidebar-custom sidebar-light-primary elevation-2">
-    {{-- <div class="center-content">
+    <div class="center-content">
         <a href="{{ route('home') }}" class="brand-link">
             <img id="logo" src="{{ asset('images/logo.png') }}" alt="..." height="100"><br>
             <span class="brand-text font-weight-light">{{ config('app.subtitle') }} {{ config('app.name') }} <br>
                 {{ config('app.name2') }}</span>
         </a>
-    </div> --}}
+    </div>
 
     <div class="sidebar">
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+       {{--  <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/2048px-User_icon_2.svg.png"
                 height="150" class="user-image img-circle elevation-1" alt="User Image">
@@ -16,7 +16,7 @@
             <div class="info">
                 <a href="#" class="d-block">{{ Auth::user()->name }}</a>
             </div>
-        </div>
+        </div> --}}
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                 data-accordion="false">
