a297b53aeedbbc582e03aa11cb099c11e664b441
manon618 <manon2029@gmail.com>
2023-10-05 10:31:35 +0700

    fix

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,11 +12,11 @@
             </a></li> --}}
 
 
-        <li class="nav-item d-none d-sm-inline-block">
+        <!--<li class="nav-item d-none d-sm-inline-block">
             <a class="btn btn-success" id="ToolbarButton" {{-- data-widget="control-sidebar" data-slide="true" href="#" --}} role="button">
                 <i class="fas fa-xl fa-spin fa-gear"></i> Agent ToolBar
             </a>{{-- faa-shake animated faa-slow fa-wrench --}}
-        </li>
+        </li>-->
        {{--  <li class="nav-item d-none d-sm-inline-block">
             <a class="btn btn-success" data-widget="fullscreen" role="button">
                 <i class="fas fa-xl fa-expand-arrows-alt"></i> ขยาย/ย่อ หน้าจอ
@@ -99,7 +99,7 @@
             </ul>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a class="nav-link" href="#" role="button">
+            <a class="btn btn-success" role="button" id="ToolbarButton">
                 <span id="phone_state_icon">{!! $temporaryPhoneStatusIcon !!}</span> [
                 <b class="text-primary">{{ $temporaryPhone }} :</b> <b class="text-primary"
                     id="phone_state">{{ $temporaryPhoneStatus }}</b> ]
