1c0539a50f3e3eb6510dc46ceff1b8c67c0e496d
manon618 <manon2029@gmail.com>
2023-10-08 20:30:46 +0700

    fix

--- resources/views/layouts/toolbar_script.blade.php
+++ resources/views/layouts/toolbar_script.blade.php
@@ -85,6 +85,8 @@
             //btn_system_logout.prop('disabled', true);
             btn_agent_logout.removeClass("d-none");
             btn_agent_logout.prop('disabled', false);
+            btn_agent_logoff.removeClass("d-none");
+            btn_agent_logoff.prop('disabled', false);
             btn_agent_login.prop('disabled', true);
             btn_agent_login.addClass("d-none");
         } else if (id === 2) {
