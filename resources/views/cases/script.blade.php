25366ed5e08c7560efb5e6d3e134c5dd36aad227
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-08 22:44:39 +0700

    saf

--- resources/views/cases/script.blade.php
+++ resources/views/cases/script.blade.php
@@ -591,8 +591,8 @@
                 method: 'GET',
                 success: function(res) {
                     console.log(res);
-                    $('#hn').val(res.data.hn);
-                    $('#name').val(res.data.name);
+                    $('#Hn').val(res.data.hn);
+                    $('#Name').val(res.data.name);
                     $('#Detail').val(res.data.casedetail);
                     $('#tranferstatus').val(res.data.tranferstatus);
                     $('#casestatus').val(res.data.casestatus);
