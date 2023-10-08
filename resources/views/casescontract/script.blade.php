9970ed902350aa86a07abfd7680c3eb8924f99a0
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-08 12:38:40 +0700

    sss

--- resources/views/casescontract/script.blade.php
+++ resources/views/casescontract/script.blade.php
@@ -557,8 +557,6 @@
         let id;
         $(document).on('click', '#getEditData', function(e) {
             e.preventDefault();
-
-
             $('.alert-danger').html('');
             $('.alert-danger').hide();
             $('.alert-success').html('');
@@ -566,14 +564,38 @@
 
             id = $(this).data('id');
             $.ajax({
+                url: "casetype6/casetype/0",
+                method: 'GET',
+                success: function(res) {
+                    var provinceOb = $('#casetype1');
+                    provinceOb.html('<option value="">เลือกประเภทการติดต่อ</option>');
+                    $.each(res.data, function(index, item) {
+                        provinceOb.append(
+                            $('<option></option>').val(item.id).html(item
+                                .name)
+                        );
+                    });
+                }
+            });
+            $('#casetype2').html('<option value="">เลือกรายละเอียดเคส</option>');
+            $('#casetype3').html('<option value="">เลือกรายละเอียดเคสย่อย</option>');
+            $('#casetype4').html('<option value="">เลือกรายละเอียดเคสเพิ่มเติม 1</option>');
+            $('#casetype5').html('<option value="">เลือกรายละเอียดเคสเพิ่มเติม 2</option>');
+            $('#casetype6').html('<option value="">เลือกรายละเอียดเคสเพิ่มเติม 3</option>');
+            $('#casetype2').attr('disabled', true);
+            $('#casetype3').attr('disabled', true);
+            $('#casetype4').attr('disabled', true);
+            $('#casetype5').attr('disabled', true);
+            $('#casetype6').attr('disabled', true);
+            $.ajax({
                 url: "casescontract/edit/" + id,
                 method: 'GET',
                 success: function(res) {
                     console.log(res);
                     //$('#Editcasetype1e').val(res.data.casetype1);
                     $('#Detail').val(res.data.casedetail);
-                    $('#Edittranferstatuse').val(res.data.tranferstatus);
-                    $('#Editcasestatuse').val(res.data.casestatus);
+                    $('#tranferstatus').val(res.data.tranferstatus);
+                    $('#casestatus').val(res.data.casestatus);
                     $('#CreateModal').modal('show');
                 }
             });
