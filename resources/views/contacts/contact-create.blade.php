0e3d6872c6d2d5f752b27a4142d2e725f083dc94
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-09 22:32:50 +0700

    ss

--- resources/views/contacts/contact-create.blade.php
+++ resources/views/contacts/contact-create.blade.php
@@ -195,7 +195,7 @@
                                     <div class="col-xs-6 col-sm-6 col-md-6">
                                         <div class="form-group">
                                             <strong><i class="fas fa-phone"></i> เบอร์โทรศัพท์บ้าน:</strong>
-                                            {!! Form::text('telhomep', (strlen($telephone) == 9) ? $telephone : '', [
+                                            {!! Form::text('telhomep', null, [
                                                 'id' => 'telhomep',
                                                 'placeholder' => 'เบอร์โทรศัพท์บ้าน',
                                                 'class' => 'form-control',
@@ -206,7 +206,7 @@
                                     <div class="col-xs-6 col-sm-6 col-md-6">
                                         <div class="form-group">
                                             <strong><i class="fas fa-phone"></i> เบอร์โทรศัพท์มือถือ :</strong>
-                                            {!! Form::text('phonenop', (strlen($telephone) == 10) ? $telephone : '', [
+                                            {!! Form::text('phonenop', null, [
                                                 'id' => 'phonenop',
                                                 'placeholder' => 'เบอร์โทรศัพท์มือถือ',
                                                 'class' => 'form-control',
