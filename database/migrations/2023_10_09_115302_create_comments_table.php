fd716d56b0eded1ad63d19a8e6257a480fbd4550
korwlik23 <korwlik23@gmail.com>
2023-10-09 19:11:30 +0700

    migrate comments

---
+++ database/migrations/2023_10_09_115302_create_comments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('call_recording_id', 255)->nullable();
+            $table->string('uniqueid', 255)->nullable();
+            $table->string('comment', 255)->nullable();
+            $table->string('start', 255)->nullable();
+            $table->string('end', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
