51fec6d9209b3e18748ba6999ac2a0bebb669fa1
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-10 14:40:16 +0700

    dd

--- resources/views/cases/index.blade.php
+++ resources/views/cases/index.blade.php
@@ -43,10 +43,8 @@
             </div>
         </div>
     </section>
-
     <section class="content">
         <div class="container-fluid">
-
             <div class="row">
                 <div class="col-12">
                     <div class="card card-primary">
