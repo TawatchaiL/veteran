64bdb0167d07a1f6d270fb430e106c5673ba0067
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-10 17:28:27 +0700

    test

--- resources/views/cases/script.blade.php
+++ resources/views/cases/script.blade.php
@@ -454,7 +454,7 @@
                     method: 'GET',
                     async: false,
                     success: function(data) {
-                        alert(data);
+                        alert(data[0]);
                         var suggestions = JSON.parse(data);
                         var suggestionsList = $('#suggestions');
                         suggestionsList.empty();
