1a50d2ad37340d4dd351a1dccd1b71a8bf19528a
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-10 17:36:15 +0700

    tetetest

--- app/Http/Controllers/CasesController.php
+++ app/Http/Controllers/CasesController.php
@@ -131,9 +131,9 @@ class CasesController extends Controller
     public function seachcontact($id)
     {
         //$data = CrmContact::select('crm_contacts.hn as hn')
-        $data = DB::table('crm_contacts')->select('crm_contacts.hn as hn')
-        ->where('crm_contacts.phoneno', 'like', '%' . $id . '%')
-        ->get();
+        //$data = DB::table('crm_contacts')->select('crm_contacts.hn as hn')
+        //->where('crm_contacts.phoneno', 'like', '%' . $id . '%')
+        //->get();
        // $data = CrmContact::find($id);
        $data = ['hn' => 't', 'hn' => 'y'];
         return response()->json(['data' => $data]);
