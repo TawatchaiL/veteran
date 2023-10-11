079b89dcbafb25c058ebaa46b07b3079cdb32e7e
manon618 <manon2029@gmail.com>
2023-10-11 14:26:13 +0700

    fix

--- resources/views/layouts/popup_script.blade.php
+++ resources/views/layouts/popup_script.blade.php
@@ -109,7 +109,7 @@
             success: async function(response) {
                 //$('#' + cardId).removeClass('card-danger');
                 //$('#' + cardId).addClass('card-success');
-                await $('#pop_' + cardId).html(response.html);
+                await $('#pop_' + dataId).html(response.html);
                 //$(".card-footer").css("display", "block")
                 //$('.bclose').css('display', 'none');
                 addemerphone();
