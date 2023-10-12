4573cc796ee5d897b7ef8fc1534b77356e380be0
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-12 12:10:58 +0700

    sss

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -213,9 +213,6 @@ class ContactController extends Controller
 
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         //
