c4e79fef2c88cf05b23a36cd4367dfa586c854ba
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-12 10:53:14 +0700

    test

--- resources/views/layouts/popup_script.blade.php
+++ resources/views/layouts/popup_script.blade.php
@@ -57,7 +57,6 @@
                 removeAllTabs();
                 $('#custom-tabs-pop').prepend(response.tab_link);
                 $('#custom-tabs-pop-tabContent').prepend(response.tab_content);
-                alert(response.active_id);
                 maximizeCard(response.active_id);
                 //$('#dpopup').html(response.html);
                 // Position the cards after dynamic content is loaded
@@ -337,6 +336,7 @@
                 });
 
                 setTimeout(function() {
+                    alert(cardId);
                     $.ajax({
                         url: "{{ route('thcity.city') }}",
                         method: 'GET',
