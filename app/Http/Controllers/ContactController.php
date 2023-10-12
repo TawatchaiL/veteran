b4765664ff9384b5aa8edbc6d36f28517f6181c0
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-12 11:38:27 +0700

    test

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -210,11 +210,7 @@ class ContactController extends Controller
 
     public function create()
     {
-        //$rnumber = studentRunningNumber::pre_generate(Auth::user()->department->code);
-        //dd($rnumber);
-        /* return response()->json([
-            'running' =>  $rnumber
-        ]); */
+
     }
 
     /**
