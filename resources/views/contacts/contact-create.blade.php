ebaeae544f9f99848f5280433875afd1e33fd519
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-12 14:11:52 +0700

    tab

--- resources/views/contacts/contact-create.blade.php
+++ resources/views/contacts/contact-create.blade.php
@@ -22,7 +22,7 @@
         </div>
         <div class="card card-success card-outline card-outline-tabs">
             <div class="card-header p-0 pt-1">
-                <ul class="nav nav-tabs" id="custom-tabs-one-tabp" role="tablist">
+                <ul class="nav nav-tabs" id="custom-tabs-one-tabp{{$cardid}}" role="tablist">
                     <li class="pt-2 px-3" id="phonenosuccess{{$cardid}}"> </li>
                     {{--   <li class="pt-2 px-3">
                         <h3 class="card-title" id="contact_name"></h3>
@@ -46,7 +46,7 @@
                 </ul>
             </div>
             <div class="card-body">
-                <div class="tab-content" id="custom-tabs-one-tabContentp">
+                <div class="tab-content" id="custom-tabs-one-tabContentp{{$cardid}}">
                     <div class="tab-pane fade show active" id="custom-tabs-one-homep" role="tabpanel"
                         aria-labelledby="custom-tabs-one-home-tabp">
 
