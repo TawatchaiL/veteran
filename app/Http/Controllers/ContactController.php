5f4e1452bbcd43729310de1b0a61ea993da93650
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-12 13:58:42 +0700

    sss

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -208,11 +208,21 @@ class ContactController extends Controller
         ]);
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
     public function create()
     {
-
+        //$rnumber = studentRunningNumber::pre_generate(Auth::user()->department->code);
+        //dd($rnumber);
+        /* return response()->json([
+            'running' =>  $rnumber
+        ]); */
     }
 
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(Request $request)
     {
         //
