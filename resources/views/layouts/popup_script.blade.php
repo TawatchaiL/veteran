4bfd36d4274e062c568fe401838c1d89569f2d7c
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-13 10:47:26 +0700

    ttt

--- resources/views/layouts/popup_script.blade.php
+++ resources/views/layouts/popup_script.blade.php
@@ -729,6 +729,7 @@
                 cardId: cardId
             },
             success: async function(response) {
+                alert(response.html);
                 $('#' + cardId).removeClass('card-danger');
                 $('#' + cardId).addClass('card-success');
                 await $('#pop_' + cardId).html(response.html);
