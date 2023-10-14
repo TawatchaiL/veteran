e3322386534ad5471cf3033adcd7bdd483b30933
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-14 20:22:25 +0700

    sdfs

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -153,8 +153,8 @@ class ContactController extends Controller
             ->where('agentno', '=', $user->phone)
             ->orWhere('status', '=', "0")
             ->orWhere('status', '=', "1")
+            ->groupBy('telno')
             ->orderBy('id', 'desc')
-           // ->groupBy('telno')
             ->get();
         $html = '';
         $tab_link = '';
