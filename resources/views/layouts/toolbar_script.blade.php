1513da4eb1515187ca4ace38a73cb36ccf9f2cd2
manon618 <manon2029@gmail.com>
2023-10-14 09:48:11 +0700

    fix

--- resources/views/layouts/toolbar_script.blade.php
+++ resources/views/layouts/toolbar_script.blade.php
@@ -323,17 +323,19 @@
     });
 
 
-    function AsyncConfirmYesNo(title, msg, yesFn, noFn) {
+    function AsyncConfirmYesNo(title, msg/* , yesFn, noFn */) {
             var $confirm = $("#modalConfirmYesNo");
             $confirm.modal('show');
             $("#lblTitleConfirmYesNo").html(title);
             $("#lblMsgConfirmYesNo").html(msg);
             $("#btnYesConfirmYesNo").off('click').click(function() {
-                yesFn();
+                //yesFn();
+                return true;
                 $confirm.modal("hide");
             });
             $("#btnNoConfirmYesNo").off('click').click(function() {
-                noFn();
+                //noFn();
+                return false;
                 $confirm.modal("hide");
             });
         }
@@ -353,7 +355,7 @@
 
             }); */
         $(document).on('click', '#custom-tabs-pop a', function(e) {
-
+            e.preventDefault()
             /* var areYouSure = confirm(
                 'If you sure you wish to leave this tab?  Any data entered will NOT be saved.  To save information, use the Save buttons.'
                 );
@@ -363,16 +365,23 @@
                 // do other stuff
                 return false;
             } */
-            AsyncConfirmYesNo(
+            var areYouSure = AsyncConfirmYesNo(
                     "Yes & No Confirmation Box",
-                    "Are you hungry?",
+                    "Are you hungry?"/* ,
                     function() {
                         $(this).tab('show')
                     },
                     function() {
                         e.preventDefault()
-                    }
+                    } */
                 );
+
+                if (areYouSure === true) {
+                $(this).tab('show')
+            } else {
+                // do other stuff
+                return false;
+            }
         })
 
 
