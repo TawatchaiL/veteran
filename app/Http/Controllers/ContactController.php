dfdd6d258714557fb4a1ef70288d0bcf3d933de1
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-14 22:01:01 +0700

    aa

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -151,7 +151,7 @@ class ContactController extends Controller
             ->where('agentno', '=', $user->phone)
             ->orWhere('status', '=', "0")
             ->orWhere('status', '=', "1")
-            ->groupBy('telno')
+            //->groupBy('telno')
             ->orderBy('id', 'desc')
             ->get();
         $html = '';
