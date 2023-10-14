5e1599c816e791e5e54f16cfc0a0ed145de87153
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-14 17:16:18 +0700

    test

--- app/Http/Controllers/CasesContractController.php
+++ app/Http/Controllers/CasesContractController.php
@@ -132,9 +132,6 @@ class CasesContractController extends Controller
         
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $user = Auth::user();
