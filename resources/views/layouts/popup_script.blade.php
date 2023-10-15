347de74bc5a40fe47d0ef920aab821d167a7d1f7
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-15 20:52:25 +0700

    www

--- resources/views/layouts/popup_script.blade.php
+++ resources/views/layouts/popup_script.blade.php
@@ -46,7 +46,6 @@
         let dataId = $(this).data('id');
 
         maximizeCard(dataId);
-        //listcontact(dataId);
     });
 
     //popup card
@@ -63,7 +62,6 @@
                 $('#custom-tabs-pop').prepend(response.tab_link);
                 $('#custom-tabs-pop-tabContent').prepend(response.tab_content);
                 maximizeCard(response.active_id);
-                //listcontact(response.active_id);
                 //$('#dpopup').html(response.html);
                 // Position the cards after dynamic content is loaded
                 /*  $('.custom-bottom-right-card').each(function(index) {
