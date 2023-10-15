60a6517778ed03327f9cb96971dde8b5841da98e
manon618 <manon2029@gmail.com>
2023-10-15 10:36:51 +0700

    fix

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -131,7 +131,7 @@
                     {{ $temporaryPhone }} ]</b> : --}} <span id="phone_state_icon"
                     class="{{ $temporaryPhoneColor }}">{!! $temporaryPhoneStatusIcon !!}</span> <b id="phone_state"
                     class="{{ $temporaryPhoneColor }}">{{ $temporaryPhoneStatus }}</b> &nbsp;&nbsp;
-                <i class="fa-solid fa-xl fa-circle-chevron-down"></i>
+                <b><i class="fa-solid fa-xl fa-circle-chevron-down"></i></b>
             </a>
         </li>
 
