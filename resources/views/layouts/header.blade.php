bc192e4ebba04040773a137b92d927a90111f75f
manon618 <manon2029@gmail.com>
2023-10-15 13:53:05 +0700

    fix

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -91,6 +91,64 @@
 
             </a>
         </li> --}}
+        <li class="nav-item dropdown">
+            <a class="nav-link" data-toggle="dropdown" href="#">
+                <i class="far fa-comments"></i>
+                <span class="badge badge-danger navbar-badge">รายการผู้ติดต่อที่ยังไม่ได้บันทึก 3</span>
+            </a>
+            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <a href="#" class="dropdown-item">
+
+                    <div class="media">
+                        <img src="{{ asset('images/user.png') }}" alt="..." class="img-size-50 mr-3 img-circle">
+                        <div class="media-body">
+                            <h3 class="dropdown-item-title">
+                                0804190099
+                                <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                            </h3>
+                            <p class="text-sm">ผู้ติดต่อใหม่</p>
+                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                        </div>
+                    </div>
+
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">
+
+                    <div class="media">
+                        <img src="{{ asset('images/user.png') }}" alt="..." class="img-size-50 mr-3 img-circle">
+                        <div class="media-body">
+                            <h3 class="dropdown-item-title">
+                                0804190099
+                                <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                            </h3>
+                            <p class="text-sm">ผู้ติดต่อใหม่</p>
+                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                        </div>
+                    </div>
+
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">
+
+                    <div class="media">
+                        <img src="{{ asset('images/user.png') }}" alt="..." class="img-size-50 mr-3 img-circle">
+                        <div class="media-body">
+                            <h3 class="dropdown-item-title">
+                                0804190099
+                                <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                            </h3>
+                            <p class="text-sm">ผู้ติดต่อใหม่</p>
+                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                        </div>
+                    </div>
+
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item dropdown-footer">ดูรายการผู้ติดต่อ</a>
+            </div>
+        </li>
+
 
     </ul>
 
