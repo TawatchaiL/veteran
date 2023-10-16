95da25b2014e5dd3267d4f0c4b85a705d88948ee
manon618 <manon2029@gmail.com>
2023-10-17 01:20:03 +0700

    fix

--- resources/views/layouts/popup_script.blade.php
+++ resources/views/layouts/popup_script.blade.php
@@ -884,14 +884,12 @@
 
         $(document).on('show.bs.tab', '#custom-tabs-pop a[data-toggle="pill"]',
             function(e) {
-                // Determine which tab is being switched to
+
                 var href = $(e.target).attr("href");
                 var targetTab = href.replace("#custom-tabs-pop-", "");
 
-                // Display a confirmation dialog
                 if (!confirm("ยืนยันการเปลี่ยน Tab ไปยัง " + targetTab +
                         " ? \nกรุณาบันทึกข้อมุลก่อนเปลี่ยน Tab")) {
-                    // If the user cancels, prevent the tab switch
                     e.preventDefault();
                 }
 
