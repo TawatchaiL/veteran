31493fddc35be9077be44d5087db9c5cc3ae8212
korwlik23 <korwlik23@gmail.com>
2023-10-17 15:49:53 +0700

    filter_XXXVII

--- resources/views/voicerecord/index.blade.php
+++ resources/views/voicerecord/index.blade.php
@@ -66,13 +66,12 @@
                                             <button type="submit" class="btn btn-success" id="searchButton">
                                                 <i class="fas fa-search"></i> ค้นหา </button>
                                         </div>
-                                    </div>
-                                    <div class="col-xs-2 col-sm-2 col-md-2 align-self-end">
                                         <div class="form-group">
                                             <button type="button" class="btn bg-gradient-warning" id="resetSearchButton"> <i class="fa-solid fa-rotate"></i> reset </button>
                                             {{-- <button type="button" class="btn bg-gradient-warning" id="resetSearchButton"><i class="fa-solid fa-rotate"></i></button> --}}
                                         </div>
                                     </div>
+                                    
 
                                 </ol>
                             </div>
