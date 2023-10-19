8fe15a354b7c4d6b9f10b2ebeb52ba0eb4ef78e1
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-19 13:53:27 +0700

    fgdg

--- resources/views/layouts/popup_script.blade.php
+++ resources/views/layouts/popup_script.blade.php
@@ -9,9 +9,10 @@
 
     $('#custom-tabs-pop').on('click', '.nav-link', function() {
         let dataId = $(this).data('id');
-
+alert('OK');
         if ($('#pop_' + dataId).text() == '(ผู้ติดต่อที่เคยบันทึกข้อมูลไว้แล้ว)' || $('#pop_' + dataId)
             .text() == '(ผู้ติดต่อใหม่)') {
+                alert('OK1');
             maximizeCard(dataId);
         }
     });
