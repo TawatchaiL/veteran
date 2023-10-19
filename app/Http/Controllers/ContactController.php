07a91026afc1220bd77fa3ab556d49367bfbfe20
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-19 21:35:17 +0700

    test

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -179,7 +179,6 @@ class ContactController extends Controller
     public function popupeditphone($telnop)
     {
         $datac = DB::table('crm_contacts')
-        //->where('id', '=', $telnop)
             ->where('phoneno', '=', $telnop)
             ->orWhere('telhome', '=', $telnop)
             ->orWhere('workno', '=', $telnop)
