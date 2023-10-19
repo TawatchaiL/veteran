ef4d34256b36a09932d7a5d3524f8df04f52baf9
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-19 14:26:30 +0700

    ss

--- resources/views/layouts/popup_script.blade.php
+++ resources/views/layouts/popup_script.blade.php
@@ -9,7 +9,7 @@
 
     $('#custom-tabs-pop').on('click', '.nav-link', function() {
         var dataId = $(this).data('id');
-        alert(dataId);
+        alert($('#pop_' + dataId).text());
         if ($('#pop_' + dataId).text() == '(ผู้ติดต่อที่เคยบันทึกข้อมูลไว้แล้ว)' || $('#pop_' + dataId).text() == '(ผู้ติดต่อใหม่)') {
             maximizeCard(dataId);
         }
