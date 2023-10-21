14e860ef397a6a2bc5203906fd7bfb5ef73736de
manon618 <manon2029@gmail.com>
2023-10-21 14:30:29 +0700

    fix

--- resources/views/layouts/header_dashboard.blade.php
+++ resources/views/layouts/header_dashboard.blade.php
@@ -5,32 +5,31 @@
             <a class="nav-link sidebar-toggle-btn" data-widget="pushmenu" href="#" role="button"><i
                     class="fas fa-bars"></i></a>
         </li>
+        <li class="nav-item d-none d-sm-inline-block"><a href="/" class="nav-link"> [ <i
+                    class="fa-solid fa-lg fa-home"></i>
+                <b class="text-primary"> หน้าหลัก </b> ]
+            </a></li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a class="nav-link" data-widget="fullscreen">
+                <i class="fas fa-xl fa-expand-arrows-alt"></i> <b class="text-primary">ขยาย/ย่อ หน้าจอ</b>
+            </a>
+        </li>
     </ul>
 
     <ul class="navbar-nav ml-auto">
-        @if (!empty($sidebarc))
-            <li class="nav-item d-none d-sm-inline-block"><a href="/" class="nav-link"> [ <i
-                        class="fa-solid fa-lg fa-home"></i>
-                    <b class="text-primary"> หน้าหลัก </b> ]
-                </a></li>
-            <li class="nav-item d-none d-sm-inline-block">
-                <a class="nav-link" data-widget="fullscreen">
-                    <i class="fas fa-xl fa-expand-arrows-alt"></i> <b class="text-primary">ขยาย/ย่อ หน้าจอ</b>
-                </a>
-            </li>
-            <li>
-                <div class="form-group row">
-                    <label for="redirectSelect" class="col-sm-4 col-form-label text-sm-end"> <b
-                            class="text-primary">Queue:</b></label>
-                    <div class="col-sm-8">
-                        <select id="redirectSelect" class="custom-select form-control-border">
-                            @foreach ($queue as $queueItem)
-                                <option value="{{ $queueItem->extension }}">{{ $queueItem->descr }}</option>
-                            @endforeach
-                        </select>
-                    </div>
+
+        <li>
+            <div class="form-group row">
+                <label for="redirectSelect" class="col-sm-4 col-form-label text-sm-end"> <b
+                        class="text-primary">Queue:</b></label>
+                <div class="col-sm-8">
+                    <select id="redirectSelect" class="custom-select form-control-border">
+                        @foreach ($queue as $queueItem)
+                            <option value="{{ $queueItem->extension }}">{{ $queueItem->descr }}</option>
+                        @endforeach
+                    </select>
                 </div>
-            </li>
-        @endif
+            </div>
+        </li>
     </ul>
 </nav>
