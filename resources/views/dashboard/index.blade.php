311c003a8e5d5a6c6456090a21a6eb342a50d537
manon618 <manon2029@gmail.com>
2023-10-21 13:11:53 +0700

    fix

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -35,8 +35,8 @@
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item">
                         <div class="form-group row">
-                            <label for="redirectSelect" class="col-sm-2 col-form-label">Queue:</label>
-                            <div class="col-sm-10">
+                            <label for="redirectSelect" class="col-sm-4 col-form-label">Queue:</label>
+                            <div class="col-sm-6">
                                 <select id="redirectSelect" class="custom-select form-control-border">
                                     @foreach ($queue as $queueItem)
                                         <option value="{{ $queueItem->extension }}">{{ $queueItem->descr }}</option>
