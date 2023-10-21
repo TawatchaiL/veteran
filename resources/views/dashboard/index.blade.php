e4a465e1c98a61e759cc7cca16607c0036b4b677
manon618 <manon2029@gmail.com>
2023-10-21 13:43:59 +0700

    fix

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -31,19 +31,15 @@
 
     <section class="content-header">
         <div class="container-fluid">
-            <div class="row mb-2">
-
-                <div class="form-group row">
-                    <label for="redirectSelect" class="col-sm-4 col-form-label">Queue:</label>
-                    <div class="col-sm-6">
-                        <select id="redirectSelect" class="custom-select form-control-border">
-                            @foreach ($queue as $queueItem)
-                                <option value="{{ $queueItem->extension }}">{{ $queueItem->descr }}</option>
-                            @endforeach
-                        </select>
-                    </div>
+            <div class="form-group row">
+                <label for="redirectSelect" class="col-sm-4 col-form-label">Queue:</label>
+                <div class="col-sm-6">
+                    <select id="redirectSelect" class="custom-select form-control-border">
+                        @foreach ($queue as $queueItem)
+                            <option value="{{ $queueItem->extension }}">{{ $queueItem->descr }}</option>
+                        @endforeach
+                    </select>
                 </div>
-
             </div>
         </div>
     </section>
