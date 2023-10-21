e84394a79347cfc6d484655353169f4fd9832931
manon618 <manon2029@gmail.com>
2023-10-21 14:51:29 +0700

    fix

--- resources/views/layouts/header_dashboard.blade.php
+++ resources/views/layouts/header_dashboard.blade.php
@@ -21,7 +21,7 @@
         <li>
             <div class="form-group row">
                 <label for="redirectSelect" class="col-sm-4 col-form-label text-sm-end"> <i
-                        class="fa-solid fa-arrow-up-1-9"></i> <b class="text-primary">Queue:</b></label>
+                        class="fa-solid fa-xl fa-arrow-up-1-9"></i> <b class="text-primary">Queue:</b></label>
                 <div class="col-sm-8">
                     <select id="redirectSelect" class="custom-select form-control-border">
                         @foreach ($queue as $queueItem)
