58a03d9d9390cf8587b0abbc2ae7d6b32ecf679b
manon618 <manon2029@gmail.com>
2023-10-21 13:03:29 +0700

    fix

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -32,10 +32,7 @@
     <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h2>Dashboard</h2>
-                </div>
-                <div class="col-sm-6">
+                <div class="col-sm-12">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"> <label for="redirectSelect">Queue:</label></li>
                         <li class="breadcrumb-item active"><select id="redirectSelect"
