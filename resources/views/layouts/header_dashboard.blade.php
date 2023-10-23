0212b0fd8217a98d8715ffb35799160b0b2e2132
manon618 <manon2029@gmail.com>
2023-10-23 12:34:41 +0700

    fix

--- resources/views/layouts/header_dashboard.blade.php
+++ resources/views/layouts/header_dashboard.blade.php
@@ -17,8 +17,8 @@
     </ul>
 
     <ul class="navbar-nav ml-auto">
-        <li>
-            <div class="nav-item form-group">
+        <li class="nav-item d-sm-inline-block">
+            <div class="form-group row">
                 <label for="redirectSelect" class="col-sm-4 col-form-label text-sm-end"> <i
                         class="fa-solid fa-xl fa-arrow-up-1-9"></i> <b class="text-primary">Queue </b></label>
                 <div class="col-sm-8">
