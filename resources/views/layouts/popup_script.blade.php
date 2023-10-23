467ff9726425b3a5f0144c5d9ff242378996de5b
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-23 15:15:57 +0700

    test

--- resources/views/layouts/popup_script.blade.php
+++ resources/views/layouts/popup_script.blade.php
@@ -92,12 +92,12 @@
                         $('#phonenosuccess' + cardId).html(
                             '<h3 class="card-title" style="color: #1a16eb"> <i class="fa-solid fa-user-tie"></i> ผู้ติดต่อใหม่</h3>'
                         );
-                        
                         $.ajax({
                             url: "contacts/popupeditphone/" + telnop,
                             method: 'GET',
                             async: false,
-                            success: function(res) {alert('OK');
+                            success: function(res) {
+                                alert(res.datax.length);
                                 if (res.datax.length > 0) {
                                     $('#phonenosuccess' + cardId).html(
                                         '<h3 class="card-title" style="color: #1a16eb"> <i class="fa-solid fa-user-tie"></i> ' +
