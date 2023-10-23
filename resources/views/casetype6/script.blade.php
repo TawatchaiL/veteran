a001fab67f74cc7b9749c5b7a7d916d538e3c8d8
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-23 23:40:31 +0700

    ssf

--- resources/views/casetype6/script.blade.php
+++ resources/views/casetype6/script.blade.php
@@ -378,9 +378,9 @@
                 $('#casetype' + nextcase).attr('disabled', false);
                 $('#AddName' + nextcase).attr('disabled', false);
                 $('#typelev' + nextcase).show();
-                if(levcase > 1){
+                //if(levcase > 1){
                     $('#typelev' + levcase).hide();
-                }
+                //}
                 
                 for (let i = discase; i < 7; i++) {
                     $('#casetype' + i).attr('disabled', true);
