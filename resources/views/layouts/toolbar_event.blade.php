42096ffc7e605e827578ebaf070ed0257d085507
manon618 <manon2029@gmail.com>
2023-10-25 00:03:12 +0700

    fix

--- resources/views/layouts/toolbar_event.blade.php
+++ resources/views/layouts/toolbar_event.blade.php
@@ -7,8 +7,8 @@
 
     socket.on('queuecallerjoin', async (response) => {
         alert_danger('Alert', 'มีสายรอในคิว ' + response.data.count + ' สาย', '');
-        alert_danger('Alert สายเข้าจาก ' + response.data.calleridnum, 'มีสายเข้าคิวจากหมายเลข ' + response
-            .data.calleridnum + ' /nในลำดับ: ' + response
+        alert_danger('Alert สายเข้าจาก ' + response.data.calleridnum, 'มีสายเข้าคิวจากหมายเลข: ' + response
+            .data.calleridnum + '<br>ในลำดับ: ' + response
             .data.position, '');
     });
 
