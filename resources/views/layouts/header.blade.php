0c13a3f343e04fdfee65764267a91ec70383f100
manon618 <manon2029@gmail.com>
2023-10-25 11:22:25 +0700

    fix

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -128,7 +128,7 @@
                 <a class="btn" data-toggle="dropdown" href="#" id="queue_wait_button"><span class="icon-gray"><i
                             class="fa-solid fa-xl fa-arrow-up-1-9"></i> สายรอคิว
                     </span><span id="queuediv"><b class="icon-blue" id="queue_wait">( 0 )</b></span></a>
-                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right" id="queue_wait_list">
+                <div class="dropdown-menu dropdown-menu-lg dropdown-menu-left" id="queue_wait_list">
 
 
                 </div>
--- resources/views/layouts/toolbar_event.blade.php
+++ resources/views/layouts/toolbar_event.blade.php
@@ -6,12 +6,16 @@
     });
 
     socket.on('queuecallerjoin', async (response) => {
-        const dropdownButton = $('#queue_wait_list');
-        dropdownMenu.classList.add('show');
+        const dropdownButton = $('#queue_wait_button');
+        let isDropdownClicked = false;
+        if (dropdownButton && !isDropdownClicked) {
+            dropdownButton.click();
+            isDropdownClicked = true;
+        }
         //alert_danger('Alert', 'มีสายรอในคิว ' + response.data.count + ' สาย', '');
-       /*  alert_danger('Alert สายเข้าจาก ' + response.data.calleridnum, 'มีสายเข้าคิวจากหมายเลข: ' + response
-            .data.calleridnum + '<br>ในลำดับ: ' + response
-            .data.position, ''); */
+        /*  alert_danger('Alert สายเข้าจาก ' + response.data.calleridnum, 'มีสายเข้าคิวจากหมายเลข: ' + response
+             .data.calleridnum + '<br>ในลำดับ: ' + response
+             .data.position, ''); */
     });
 
 
