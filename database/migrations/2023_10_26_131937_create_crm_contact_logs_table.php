57c42c9577038462df54f1802540c376dbb542c9
Taitawatchai <sc_player01@hotmail.com>
2023-10-26 13:48:56 +0700

    test

--- database/migrations/2023_10_26_131937_create_crm_contact_logs_table.php
+++ database/migrations/2023_10_26_131937_create_crm_contact_logs_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -36,7 +37,7 @@ return new class extends Migration
             $table->dateTime('updated_at');
             $table->string('modifyaction',10)->nullable();
             $table->string('modifyagent',10)->nullable();
-            $table->timestamp('modifydate');
+            $table->timestamp('modifydate')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
     
