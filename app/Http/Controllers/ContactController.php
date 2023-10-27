3a9d64f9242f4172edbfd5a6afadcabb4a1bd411
Taitawatchai <sc_player01@hotmail.com>
2023-10-27 12:26:15 +0700

    หดหกด

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -498,7 +498,7 @@ class ContactController extends Controller
         //DB::table('crm_contact_logs')->insert($contact);
         //$bindings = $contact->getBindings();
 
-        CrmContactLog::create($contactlog);
+        CrmContactLog::create($contact);
 
         $contact->update($contactd);
         if (!empty($request->eemergencyData)) {
