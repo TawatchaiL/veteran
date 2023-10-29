2bd70bd71c9e4ee3804d80a4f71769a1886fa436
manon618 <manon2029@gmail.com>
2023-10-29 13:11:46 +0700

    fix

--- resources/views/dashboard/script.blade.php
+++ resources/views/dashboard/script.blade.php
@@ -531,7 +531,7 @@
         let phone_number = exts[1];
         const div_src = $('#' + phone_number + '_src');
 
-        if (res.queue == storedOption) {
+        //if (res.queue == storedOption) {
             if (res.status == 5 || res.status == 0) {
                 phone_status = `<span style="font-size: 1em; color: red;">
                     <i class="fa-solid fa-triangle-exclamation"></i></span> ` + phone_number
@@ -628,7 +628,7 @@
             active_div.html(num_active);
             div_agent_status_chart.setOption(agent_status_chart(num_offline, num_ready, num_pause, num_warp,
                 num_busy));
-        }
+        //}
 
     });
 
