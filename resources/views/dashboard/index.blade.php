4e6c90f5444e2dcd359b6bbf9787d09e3f2d9a7e
manon618 <manon2029@gmail.com>
2023-10-29 15:49:38 +0700

    fix

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -282,7 +282,7 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-8">
+                        <div class="col-md-7">
 
                             <div class="card card-primary" style="min-height: 405px">
                                 <div class="card-header">
@@ -347,7 +347,7 @@
 
                             </div>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-5">
 
                             <div class="card card-primary" style="min-height: 405px">
                                 <div class="card-header">
