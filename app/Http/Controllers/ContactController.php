d0d0b13bcd85aa511053033ba8a4b1db17b11417
TawatchaiL <tawatchai.winit@gmail.com>
2023-10-29 22:24:22 +0700

    tswtw

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -804,7 +804,36 @@ class ContactController extends Controller
             }
         }
 
-
+        $Crmcsae = new CrmCase();
+        $Crmcsae->contact_id = $id;
+        $Crmcsae->telno = $request->get('telno');
+        $Crmcsae->casetype1 = $request->input('casetype1');
+        $Crmcsae->caseid1 = $request->input('caseid1');
+        if ($request->input('casetype2') != "") {
+            $Crmcsae->casetype2 = $request->input('casetype2');
+            $Crmcsae->caseid2 = $request->input('caseid2');
+        }
+        if ($request->input('casetype3') != "") {
+            $Crmcsae->casetype3 = $request->input('casetype3');
+            $Crmcsae->caseid3 = $request->input('caseid3');
+        }
+        if ($request->input('casetype4') != "") {
+            $Crmcsae->casetype4 = $request->input('casetype4');
+            $Crmcsae->caseid4 = $request->input('caseid4');
+        }
+        if ($request->input('casetype5') != "") {
+            $Crmcsae->casetype5 = $request->input('casetype5');
+            $Crmcsae->caseid5 = $request->input('caseid5');
+        }
+        if ($request->input('casetype6') != "") {
+            $Crmcsae->casetype6 = $request->input('casetype6');
+            $Crmcsae->caseid6 = $request->input('caseid6');
+        }
+        $Crmcsae->tranferstatus = $request->get('tranferstatus');
+        $Crmcsae->casedetail = $request->get('casedetail');
+        $Crmcsae->casestatus = $request->get('casestatus');
+        $Crmcsae->agent = $user->id;
+        $Crmcsae->save();
 
         //CrmIncoming::where('telno', $request->input('telno'))->update('status' => '2');
         DB::table('crm_incoming')->where('telno', $request->input('telno'))->update(['status' => '2']);
