5cafd9ac2c9464b87d2327b4586058e4625299ce
manon618 <manon2029@gmail.com>
2023-10-29 13:14:38 +0700

    fix

--- resources/views/dashboard/script.blade.php
+++ resources/views/dashboard/script.blade.php
@@ -499,7 +499,7 @@
 
             let formattedTime =
                 `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-            if (item.queue == storedOption) {
+            //if (item.queue == storedOption) {
                 html += `
                 <tr>
                     <td>${item.position}</td>
@@ -508,7 +508,7 @@
                     <td>${item.connectedlinenum === 'unknown' ? '-' : item.connectedlinenum}</td>
                 </tr>`;
                 waiting_total++;
-            }
+            //}
         });
 
         tableBody.html(html);
