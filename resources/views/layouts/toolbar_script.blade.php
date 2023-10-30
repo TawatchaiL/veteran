083137976e64724a2ae7cc555a92721b0c969104
manon618 <manon2029@gmail.com>
2023-10-30 19:23:19 +0700

    fix

--- resources/views/layouts/toolbar_script.blade.php
+++ resources/views/layouts/toolbar_script.blade.php
@@ -783,6 +783,7 @@
 
 
         $.get(`${api_serv}/chans/` + exten, async (data, status) => {
+            console.log(data)
 
             await data.forEach((item, index) => {
                 let strArray = item.split("!");
