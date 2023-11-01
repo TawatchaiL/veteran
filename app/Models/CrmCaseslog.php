abb1553ff66434e8cb27e22a822425f1ae8ad879
TawatchaiL <tawatchai.winit@gmail.com>
2023-11-01 21:20:58 +0700

    test

---
+++ app/Models/CrmCaseslog.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CrmCaseslog extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'id',
+        'contact_id',
+        'telno',
+        'casetype1',
+        'caseid1',
+        'casetype2',
+        'caseid2',
+        'casetype3',
+        'caseid3',
+        'casetype4',
+        'caseid4',
+        'casetype5',
+        'caseid5',
+        'casetype6',
+        'caseid6',
+        'tranferstatus',
+        'casedetail',
+        'casestatus',
+        'agent',
+        'created_at',
+        'updated_at',
+        'modifyaction',
+        'modifyagent',
+        'modifydate',
+    ];
+}
