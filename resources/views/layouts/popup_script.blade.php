5ae0c158ce9167c13268ba0634918b9718fc95db
Taitawatchai <sc_player01@hotmail.com>
2023-11-01 12:44:28 +0700

    tswtst

--- resources/views/layouts/popup_script.blade.php
+++ resources/views/layouts/popup_script.blade.php
@@ -906,7 +906,7 @@
         //Click Tab
         $(document).on('click', '.tablistcaseP', function() {
             const tabText = $(this).text();
-            alert(tabText);
+            //alert(tabText);
         });
         $(document).on('click', '.custom-bottom-right-card .card-tools [data-card-widget="maximize"]',
             function() {
