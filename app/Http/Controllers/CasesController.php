3a2fbd56890180a4983f289b41feff878d3dffd9
Taitawatchai <sc_player01@hotmail.com>
2023-11-02 08:42:33 +0700

    xvxv

--- app/Http/Controllers/CasesController.php
+++ app/Http/Controllers/CasesController.php
@@ -350,7 +350,7 @@ class CasesController extends Controller
     public function destroy(Request $request)
     {
         $id = $request->get('id');
-        CrmCase::find($id)->delete();
+        //CrmCase::find($id)->delete();
         return ['success' => true, 'message' => 'ลบ เรื่องที่ติดต่อ เรียบร้อยแล้ว'];
     }
 
