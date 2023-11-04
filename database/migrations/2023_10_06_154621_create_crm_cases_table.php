64a7ad181d74f84fcbffc4887f4207a30f58eab0
TawatchaiL <tawatchai.winit@gmail.com>
2023-11-04 16:46:48 +0700

    etes

--- database/migrations/2023_10_06_154621_create_crm_cases_table.php
+++ database/migrations/2023_10_06_154621_create_crm_cases_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('crm_cases', function (Blueprint $table) {
             $table->id();
             $table->integer('contact_id')->nullable();
+            $table->date('adddate')->nullable();
             $table->string('uniqid')->nullable();
             $table->string('telno')->nullable();
             $table->string('casetype1')->nullable();
