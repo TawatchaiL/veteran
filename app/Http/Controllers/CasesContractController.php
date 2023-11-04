104f83db0536efcbbf027e1dcabe3e46f5170b5c
TawatchaiL <tawatchai.winit@gmail.com>
2023-11-04 21:43:03 +0700

    test

--- app/Http/Controllers/CasesContractController.php
+++ app/Http/Controllers/CasesContractController.php
@@ -217,7 +217,35 @@ class CasesContractController extends Controller
 
         $company = CrmCase::find($id);
 
-
+        $caseslog = [
+         'id' => $company->id,
+         'contact_id' => $company->contact_id,
+         //'adddate' => $company->adddate,
+         'uniqid' => $company->uniqid,
+         'telno' => $company->telno,
+         'casetype1' => $company->casetype1,
+         'caseid1' => $company->caseid1,
+         'casetype2' => $company->casetype2,
+         'caseid2' => $company->caseid2,
+         'casetype3' => $company->casetype3,
+         'caseid3' => $company->caseid3,
+         'casetype4' => $company->casetype4,
+         'caseid4' => $company->caseid4,
+         'casetype5' => $company->casetype5,
+         'caseid5' => $company->caseid5,
+         'casetype6' => $company->casetype6,
+         'caseid6' => $company->caseid6,
+         'tranferstatus' => $company->tranferstatus,
+         'casedetail' => $company->casedetail,
+         'casestatus' => $company->casestatus,
+         'agent' => $company->agent,
+         'created_at' => $company->created_at,
+         'updated_at' => $company->updated_at,
+         'modifyaction' => 'edit',
+         'modifyagent' => $user->id,
+     ];
+ 
+         CrmCaseslog::create($caseslog);
         $company->update($companyd);
 
         return response()->json(['success' => 'แก้ไข เรื่องที่ติดต่อ เรียบร้อยแล้ว']);
