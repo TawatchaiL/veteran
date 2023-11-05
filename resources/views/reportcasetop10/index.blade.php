d372f113b34b431a3e968749bbc5fee62b2d2033
TawatchaiL <tawatchai.winit@gmail.com>
2023-11-05 21:53:09 +0700

    test

--- resources/views/reportcasetop10/index.blade.php
+++ resources/views/reportcasetop10/index.blade.php
@@ -212,4 +212,10 @@
 
 @section('script')
     @include('reportcasetop10.script')
+    {!! $chart1->renderJs() !!}
+
+    {!! $chart2->renderJs() !!}
+
+
+    {!! $chart3->renderJs() !!}
 @endsection
