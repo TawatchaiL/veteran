24566b44d3638be21c285cdbaf1d64d86931e4ec
Taitawatchai <sc_player01@hotmail.com>
2023-11-07 14:15:09 +0700

    dgsdg

--- resources/views/reportcasetop10/script.blade.php
+++ resources/views/reportcasetop10/script.blade.php
@@ -376,7 +376,7 @@
                     d.sdate = $('#reservation').val();
                 },
                 complete: function (data) {
-                    alert(JSON.stringify(data.recordsFiltered));
+                    alert(JSON.stringify(data));
                     Loadchart();
                 }  
             },
