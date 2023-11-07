e5288ffdbdb12de4ce4a18de57ded1116db137bb
Taitawatchai <sc_player01@hotmail.com>
2023-11-07 10:40:02 +0700

    test

--- resources/views/reportcasetop10/script.blade.php
+++ resources/views/reportcasetop10/script.blade.php
@@ -725,13 +725,14 @@
                             });
                             options.series.data =  res.datag;
                         var chart3 = new ApexCharts(document.querySelector("#pie_graph"), options);
+                            chart3.render();
                             chart3.updateOptions({
                                                 chart: {
                                                 type: "donut",
                                                 animate: true
                                                 }
                                             });
-                            chart3.render();
+                            
                 }
             });
     });
