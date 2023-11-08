b29bfa05d653709d8e2f74b05bd74410c7f6fbb7
manon618 <manon2029@gmail.com>
2023-11-08 15:25:49 +0700

    fix

--- app/Http/Controllers/HolidaysController.php
+++ app/Http/Controllers/HolidaysController.php
@@ -120,8 +120,8 @@ class HolidaysController extends Controller
             'thankyou_sound' => $request->get('thankyou_sound'),
             'start_datetime' =>  $startutcFormattedDate . " " . $start_array[1] . ":00",
             'end_datetime' => $endutcFormattedDate . " " . $end_array[1] . ":00",
-            'start_datetime_th' =>  $request->get('start_date') . ":00",
-            'end_datetime_th' => $request->get('end_date') . ":00",
+            'start_datetime_th' =>  $request->get('start_date'),
+            'end_datetime_th' => $request->get('end_date'),
             'status' => $request->get('status'),
         ];
 
