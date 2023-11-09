a5b07a07d259e845559f53e9aae796938e32fc5c
manon618 <manon2029@gmail.com>
2023-11-09 16:27:14 +0700

    fix

--- resources/views/billing/edit.blade.php
+++ resources/views/billing/edit.blade.php
@@ -4,7 +4,8 @@
         <form id="editdata" class="form" action="" method="POST">
             <div class="modal-content">
                 <div class="modal-header bg-primary">
-                    <h4 class="modal-title" id="exampleModalLongTitle"><i class="fas fa-list-ol"></i> แก้ไข วันหยุดประจำปี
+                    <h4 class="modal-title" id="exampleModalLongTitle"><i class="fas fa-list-ol"></i> แก้ไข
+                        วันหยุดประจำปี
                     </h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
@@ -36,84 +37,24 @@
                         <div class="row">
                             <div class="col-xs-8 col-sm-8 col-md-8">
                                 <div class="form-group">
-                                    <strong><i class="fas fa-calendar"></i> จากวันที่:</strong>
-                                    @php
-                                        $datethai = date('m/d/') . date('Y') + 543 . ' ' . date('H:i');
-                                    @endphp
-                                    {!! Form::text('start_date', $datethai, [
-                                        'id' => 'EditSDate',
-                                        'placeholder' => '',
-                                        'class' => 'datepick form-control',
-                                    ]) !!}
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-8 col-sm-8 col-md-8">
-                                <div class="form-group">
-                                    <strong><i class="fas fa-calendar"></i> ถึงวันที่:</strong>
-                                    @php
-                                        $datethai = date('m/d/') . date('Y') + 543 . ' ' . date('H:i');
-                                    @endphp
-                                    {!! Form::text('end_date', $datethai, [
-                                        'id' => 'EditEDate',
-                                        'placeholder' => '',
-                                        'class' => 'datepick form-control',
-                                    ]) !!}
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-8 col-sm-8 col-md-8">
-                                <div class="form-group">
                                     <strong><i class="fas fa-list-ol"></i> เสียง ประกาศวันหยุด:</strong>
                                     <select style="width: 100%;" class="select2 select2_single form-control"
                                         id="EditGreeting" name="egreeting" multiple="multiple">
-                                        @foreach ($sound as $key2)
-                                            <option value="{{ $key2->displayname }}">{{ $key2->displayname }}
-                                            </option>
+                                        @foreach ($trunk as $key2)
+                                            <option value="{{ strtoupper($key2->tech) }}/{{ $key2->channelid }}">
+                                                {{ $key2->name }}</option>
                                         @endforeach
                                     </select>
                                 </div>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-xs-8 col-sm-8 col-md-8">
-                                <div class="form-group">
-                                    <strong><i class="fas fa-list-ol"></i> เสียง ขอบคุณ:</strong>
-                                    <select style="width: 100%;" class="select2 select2_single form-control"
-                                        id="EditThankyou" name="ethankyou" multiple="multiple">
-                                        @foreach ($sound as $key2)
-                                            <option value="{{ $key2->displayname }}">{{ $key2->displayname }}
-                                        @endforeach
-
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-8 col-sm-8 col-md-8">
-                                <div class="form-group">
-                                    <strong><i class="fas fa-eye"></i> เปิดใช้งาน:</strong>
-                                    <br />
-                                    <div class="custom-control custom-switch">
-                                        {{ Form::checkbox('status', '1', false, ['id' => 'ecustomCheckbox1', 'class' => 'custom-control-input name']) }}
-                                        <label for="ecustomCheckbox1" class="custom-control-label">
-                                            เปิดใช้งาน</label>
-                                    </div>
-
-                                </div>
-                            </div>
-
-                        </div>
-
                         {!! Form::close() !!}
                     </div>
                 </div>
                 <!-- Modal footer -->
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-success" id="SubmitEditForm"><i
-                            class="fas fa-download"></i> บันทึกข้อมูล</button>
+                    <button type="button" class="btn btn-success" id="SubmitEditForm"><i class="fas fa-download"></i>
+                        บันทึกข้อมูล</button>
                     <button type="button" class="btn btn-danger modelClose" data-dismiss="modal"><i
                             class="fas fa-door-closed"></i> ปิดหน้าต่าง</button>
                 </div>
