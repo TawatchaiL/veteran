afe38610b85472ae3405959f71df06b7fbc4e022
manon618 <manon2029@gmail.com>
2023-11-10 23:56:05 +0700

    fix

--- resources/views/layouts/style.blade.php
+++ resources/views/layouts/style.blade.php
@@ -248,7 +248,7 @@
     .dig {
         float: left;
         padding: 10px 30px;
-        width: 90px;
+        width: 70px;
         cursor: pointer;
         border: 2px solid #d3d3d3;
         /* Set the border color */
@@ -256,13 +256,13 @@
     }
 
     .sub {
-        font-size: 0.8rem;
+        font-size: 0.5rem;
         color: grey;
     }
 
     .containerbutton {
         background-color: white;
-        width: 320px;
+        width: 300px;
         padding: 25px;
         margin: 20px auto;
         height: 350px;
@@ -305,66 +305,11 @@
         margin: 10px;
         width: 70px;
         cursor: pointer;
-        font-size: 1rem;
+        font-size: 0.8rem;
     }
 
     .popover {
         max-width: 100%;
     }
 
-
-    /***
- Material Design Switches for Bootstrap 4 and Material Design Bootstrap (MDB)
- by djibe
- JSFiddle : https://jsfiddle.net/djibe89/9deak9dh/
-***/
-
-    .material-switch>input[type="checkbox"] {
-        display: none;
-    }
-
-    .material-switch>label {
-        cursor: pointer;
-        height: 0px;
-        position: relative;
-        top: 2px;
-        width: 40px;
-    }
-
-    .material-switch>label::before {
-        background: rgb(0, 0, 0);
-        box-shadow: inset 0px 0px 10px rgba(0, 0, 0, 0.5);
-        border-radius: 8px;
-        content: '';
-        height: 16px;
-        margin-top: -8px;
-        position: absolute;
-        opacity: 0.3;
-        transition: all 0.4s ease-in-out;
-        width: 40px;
-    }
-
-    .material-switch>label::after {
-        background: rgb(255, 255, 255);
-        border-radius: 16px;
-        box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
-        content: '';
-        height: 24px;
-        left: -4px;
-        margin-top: -8px;
-        position: absolute;
-        top: -4px;
-        transition: all 0.3s ease-in-out;
-        width: 24px;
-    }
-
-    .material-switch>input[type="checkbox"]:checked+label::before {
-        background: inherit;
-        opacity: 0.5;
-    }
-
-    .material-switch>input[type="checkbox"]:checked+label::after {
-        background: inherit;
-        left: 20px;
-    }
 </style>
