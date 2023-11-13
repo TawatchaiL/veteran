5e42f15414497a01ed06e18f1b3e8504a7b69632
TawatchaiL <tawatchai.winit@gmail.com>
2023-11-13 22:25:17 +0700

    sfd

--- resources/views/cases/script.blade.php
+++ resources/views/cases/script.blade.php
@@ -287,7 +287,7 @@
             dateFormat: 'yy-mm-dd',
             changeMonth: true,
             changeYear: true,
-            yearRange: '2480:2593',
+            yearRange: 'c-60:c+1',
             yearSuffix: " พ.ศ.",
             beforeShow: function (input, inst) {
                 var currentDate = $(this).datepicker("getDate");
