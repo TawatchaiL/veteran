d981dc9d5597b551e8139f9613991f1a9515ead6
Taitawatchai <sc_player01@hotmail.com>
2023-11-14 13:51:08 +0700

    test

--- resources/views/cases/script.blade.php
+++ resources/views/cases/script.blade.php
@@ -557,6 +557,7 @@
             if(actions == 'edit'){
                 if (!confirm("ยืนยันการทำรายการ ?")) return;
             }
+            alert($( "Addadddate" )datepicker("getDate"));
             e.preventDefault();
             $('.alert-danger').html('');
             $('.alert-danger').hide();
