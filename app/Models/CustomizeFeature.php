32c64523b80ee0ab1d1a5a31b0e21cefc3fcfd49
manon618 <manon2029@gmail.com>
2023-11-14 12:07:50 +0700

    fix

---
+++ app/Models/CustomizeFeature.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CustomizeFeature extends Model
+{
+    use HasFactory;
+    protected $connection = 'remote_connection';
+
+    protected $table = 'customize_features';
+
+    protected $fillable = [
+        'name', 'value', 'status'
+    ];
+}
---
+++ database/migrations/2023_11_14_114936_create_customize_features_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::connection('remote_connection')->create('customize_features', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('value')->nullable();
+            $table->integer('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::connection('remote_connection')->dropIfExists('customize_features');
+    }
+};
