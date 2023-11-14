f573a665dcd49c30ad210c7d90707f41feba5fbf
Taitawatchai <sc_player01@hotmail.com>
2023-11-14 14:19:18 +0700

    test

--- resources/views/cases/script.blade.php
+++ resources/views/cases/script.blade.php
@@ -318,15 +318,14 @@
 		onClose:function(){
 			if($(this).val()!="" && $(this).val()==dateBefore){			
 				var arrayDate=dateBefore.split("-");
-                $('#temp'+$(this).attr('id')).val(dateBefore);
+                $('#temp'+$(this).attr('id')).html(dateBefore);
 				arrayDate[0]=parseInt(arrayDate[0])+543;
 				$(this).val(arrayDate[0]+"-"+arrayDate[1]+"-"+arrayDate[2]);	
 			}		
 		},
 		onSelect: function(dateText, inst){ 
 			dateBefore=$(this).val();
-            alert($(this).attr('id'));
-            $('#temp'+$(this).attr('id')).val(dateBefore);
+            $('#temp'+$(this).attr('id')).html(dateBefore);
 			var arrayDate=dateText.split("-");
 			arrayDate[0]=parseInt(arrayDate[0])+543;
 			$(this).val(arrayDate[0]+"-"+arrayDate[1]+"-"+arrayDate[2]);
