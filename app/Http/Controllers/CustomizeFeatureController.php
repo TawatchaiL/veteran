1a0c576ca26030ea2e2b1f7fdc530c1530ba844d
manon618 <manon2029@gmail.com>
2023-11-14 13:02:57 +0700

    customize

--- app/Http/Controllers/CustomizeFeatureController.php
+++ app/Http/Controllers/CustomizeFeatureController.php
@@ -3,17 +3,58 @@
 namespace App\Http\Controllers;
 
 use App\Models\CustomizeFeature;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CustomizeFeatureController extends Controller
 {
+      /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //$this->middleware('auth');
+        $this->middleware('permission:customize-list|customize-create|customize-edit|customize-delete', ['only' => ['index', 'show']]);
+        $this->middleware('permission:customize-create', ['only' => ['create', 'store']]);
+        $this->middleware('permission:customize-edit', ['only' => ['edit', 'update']]);
+        $this->middleware('permission:customize-delete', ['only' => ['destroy']]);
+    }
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        if ($request->ajax()) {
+            //sleep(2);
+
+            $datas = CustomizeFeature::orderBy("id", "desc")->get();
+            $state_text = array('ไม่เปิดใช้งาน', 'เปิดใช้งาน');
+            return datatables()->of($datas)
+                ->editColumn('checkbox', function ($row) {
+                    return '<input type="checkbox" id="' . $row->id . '" class="flat" name="table_records[]" value="' . $row->id . '" >';
+                })
+                ->editColumn('status', function ($row) use ($state_text) {
+                    $state = $state_text[$row->status];
+                    return $state;
+                })
+                ->addColumn('action', function ($row) {
+                    if (Gate::allows('holiday-edit')) {
+                        $html = '<button type="button" class="btn btn-sm btn-warning btn-edit" id="getEditData" data-id="' . $row->id . '"><i class="fa fa-edit"></i> แก้ไข</button> ';
+                    } else {
+                        $html = '<button type="button" class="btn btn-sm btn-warning disabled" data-toggle="tooltip" data-placement="bottom" title="คุณไม่มีสิทธิ์ในส่วนนี้"><i class="fa fa-edit"></i> แก้ไข</button> ';
+                    }
+                    return $html;
+                })->rawColumns(['checkbox', 'action'])->toJson();
+        }
+
+        return view('holiday.index');
     }
+
 
     /**
      * Show the form for creating a new resource.
