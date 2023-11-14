6ca1e8e0407a0ce78f988d201f9a01a6670ba46f
Taitawatchai <sc_player01@hotmail.com>
2023-11-14 14:14:58 +0700

    test

--- resources/views/cases/script.blade.php
+++ resources/views/cases/script.blade.php
@@ -325,6 +325,7 @@
 		},
 		onSelect: function(dateText, inst){ 
 			dateBefore=$(this).val();
+            alert(dateBefore);
             $('#temp'+$(this).attr('id')).val(dateBefore);
 			var arrayDate=dateText.split("-");
 			arrayDate[0]=parseInt(arrayDate[0])+543;
