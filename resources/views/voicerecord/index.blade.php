6b428c34d5dcfe1027b8731ead47956ba7fc7b85
manon618 <manon2029@gmail.com>
2023-11-15 13:39:36 +0700

    fix

--- resources/views/voicerecord/index.blade.php
+++ resources/views/voicerecord/index.blade.php
@@ -62,7 +62,7 @@
                                             </select>
                                         </div>
                                     </div>
-                                    <div class="col-xs-2 col-sm-2 col-md-2">
+                                    <div class="col-xs-2 col-sm-2 col-md-2" style="align-items: flex-end;">
                                         <div class="form-group">
                                             <a class="btn btn-success" id="searchButton"> <i class="fas fa-search"></i>
                                                 Search </a>
@@ -70,13 +70,6 @@
                                                     class="fa-solid fa-rotate"></i> Reset </a>
                                         </div>
                                     </div>
-                                    <div class="col-xs-2 col-sm-2 col-md-2">
-                                        <div class="form-group">
-                                            <strong>&nbsp;</strong>
-
-                                        </div>
-                                    </div>
-
                                 </ol>
                             </div>
                         </div>
