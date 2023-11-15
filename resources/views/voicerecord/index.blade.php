8a9746a6bcd70302bbdb0af1f10567b68e188ed2
manon618 <manon2029@gmail.com>
2023-11-15 13:36:47 +0700

    fix

--- resources/views/voicerecord/index.blade.php
+++ resources/views/voicerecord/index.blade.php
@@ -62,18 +62,18 @@
                                             </select>
                                         </div>
                                     </div>
-                                    <div class="col-xs-1 col-sm-1 col-md-1">
+                                    <div class="col-xs-2 col-sm-2 col-md-2">
                                         <div class="form-group">
                                             <strong>&nbsp;</strong>
                                             <button class="btn btn-success" id="searchButton"> <i class="fas fa-search"></i>
-                                               </button>
+                                                Search </button>
                                         </div>
                                     </div>
-                                    <div class="col-xs-1 col-sm-1 col-md-1">
+                                    <div class="col-xs-2 col-sm-2 col-md-2">
                                         <div class="form-group">
                                             <strong>&nbsp;</strong>
                                             <button class="btn bg-warning" id="resetSearchButton"> <i
-                                                    class="fa-solid fa-rotate"></i> </button>
+                                                    class="fa-solid fa-rotate"></i> Reset </button>
                                         </div>
                                     </div>
 
