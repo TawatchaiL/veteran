c7945cbb4f1961bd31afefba4985a1cfc3bd00ca
manon618 <manon2029@gmail.com>
2023-11-15 13:40:42 +0700

    fix

--- resources/views/voicerecord/index.blade.php
+++ resources/views/voicerecord/index.blade.php
@@ -62,7 +62,7 @@
                                             </select>
                                         </div>
                                     </div>
-                                    <div class="col-xs-2 col-sm-2 col-md-2" style="align-items: flex-end;">
+                                    <div class="col-xs-2 col-sm-2 col-md-2 align-self-end text-right">
                                         <div class="form-group">
                                             <a class="btn btn-success" id="searchButton"> <i class="fas fa-search"></i>
                                                 Search </a>
