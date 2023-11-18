0ae4760c90cc7a511a3615ef84c43e05691f9b3d
TawatchaiL <tawatchai.winit@gmail.com>
2023-11-18 22:31:07 +0700

    sdfg

--- app/Http/Controllers/DetailscoreagentController.php
+++ app/Http/Controllers/DetailscoreagentController.php
@@ -49,7 +49,7 @@ class DetailscoreagentController extends Controller
         }
         $datas = DB::connection('remote_connection')
             ->table('call_center.agent_score')
-            ->select('call_center.agent_score.score as lscore',  DB::raw('count(call_center.agent_score.score) as sumscore'))
+            ->select('call_center.agent_score.score as score',  DB::raw('count(call_center.agent_score.score) as sumscore'))
             ->whereRaw('call_center.agent_score.datetime between "' . $startDate . ' 00:00:00" and "' . $endDate . ' 23:59:59"');
             if(!empty($request->get('agent')) && $request->get('agent') != "0"){
                 $datas->whereRaw('crm_id = "'. $request->input('agent') .'"');  
@@ -63,7 +63,7 @@ class DetailscoreagentController extends Controller
                 $chart_label = array();
                 foreach ($datas as $data) {
                     $chart_data[] = $data->sumscore;
-                    $chart_label[] = $data->lscore;
+                    $chart_label[] = $data->score;
                 }
                 return response()->json(['datag' => $chart_data,'datal' => $chart_label]);
             }
