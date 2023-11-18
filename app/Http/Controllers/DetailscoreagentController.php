4dba4caa94a697c8c3ec6e6d05226a939696cccb
TawatchaiL <tawatchai.winit@gmail.com>
2023-11-18 22:55:12 +0700

    tes

--- app/Http/Controllers/DetailscoreagentController.php
+++ app/Http/Controllers/DetailscoreagentController.php
@@ -49,7 +49,7 @@ class DetailscoreagentController extends Controller
         }
         $datas = DB::connection('remote_connection')
             ->table('call_center.agent_score')
-            ->select('call_center.agent_score.score as score',  DB::raw('count(call_center.agent_score.score) as sumscore'))
+            ->select('call_center.agent_score.score as score',  DB::raw('count(call_center.agent_score.id) as sumscore'))
             ->whereRaw('call_center.agent_score.datetime between "' . $startDate . ' 00:00:00" and "' . $endDate . ' 23:59:59"');
  
             $datas->groupBy('call_center.agent_score.score')
