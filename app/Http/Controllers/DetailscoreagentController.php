b83f07da6d1f754c69dbf8417bcb27357681d656
TawatchaiL <tawatchai.winit@gmail.com>
2023-11-18 15:01:24 +0700

    test

--- app/Http/Controllers/DetailscoreagentController.php
+++ app/Http/Controllers/DetailscoreagentController.php
@@ -62,7 +62,7 @@ class DetailscoreagentController extends Controller
                 $chart_data = array();
                 $chart_label = array();
                 foreach ($datas as $data) {
-                    $chart_data[] = $data->sumcases;
+                    $chart_data[] = $data->sumscore;
                     $chart_label[] = $data->score;
                 }
                 return response()->json(['datag' => $chart_data,'datal' => $chart_label]);
