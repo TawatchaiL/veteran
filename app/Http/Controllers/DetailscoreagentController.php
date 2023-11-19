903cd91314e9617fc78b013c0f7d4440f51a43d6
TawatchaiL <tawatchai.winit@gmail.com>
2023-11-19 12:57:05 +0700

    test

--- app/Http/Controllers/DetailscoreagentController.php
+++ app/Http/Controllers/DetailscoreagentController.php
@@ -50,8 +50,7 @@ class DetailscoreagentController extends Controller
         $datas = DB::connection('remote_connection')
             ->table('call_center.agent_score')
             //->select('call_center.agent_score.score as score',  DB::raw('count(call_center.agent_score.score) as sumscore'))
-            ->select('score')
-            ->sum('score')
+            ->select('score',  DB::raw('count(score) as sumscore'))
             ->whereRaw('call_center.agent_score.datetime between "' . $startDate . ' 00:00:00" and "' . $endDate . ' 23:59:59"');
             if(!empty($request->get('agent')) && $request->get('agent') != "0"){
                 $datas->whereRaw('crm_id = "'. $request->input('agent') .'"');  
