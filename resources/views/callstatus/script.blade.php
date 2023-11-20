a91a7933c83bce4714dc4a619e226ab9af71ca86
Taitawatchai <sc_player01@hotmail.com>
2023-11-20 13:20:18 +0700

    test

--- resources/views/callstatus/script.blade.php
+++ resources/views/callstatus/script.blade.php
@@ -472,7 +472,7 @@
                             fontSize: 16
                         };
                         // กำหนดความกว้างของ header แต่ละคอลัมน์หัวข้อ
-                        doc.content[1].table.widths = ['*', 100, 100, 100, 100];
+                        doc.content[1].table.widths = ['*', 80, 80, 80, 80];
                         doc.styles.tableHeader.fontSize = 16; // กำหนดขนาด font ของ header
                         // Add cell borders
                         doc.content[1].table.layout = {
