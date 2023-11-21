9e07e42e930f6339081381d861271e5f5af504a8
meeh2029 <manon2029@gmail.com>
2023-11-21 20:29:04 +0700

    fix

--- resources/views/cases/caseslist.blade.php
+++ resources/views/cases/caseslist.blade.php
@@ -7,22 +7,21 @@
 </div>
 <div class="card">
     <div class="card-body"> --}}
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <table id="ListCaseview"
-                class="display nowrap table table-bordered table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>ประเภทเคส</th>
-                        <th>รายละเอียดเคส</th>
-                        <th>สถานะเคส</th>
-                        <th>Agent</th>
-                        <th>วันที่ทำรายการ</th>
-                        <th width="140px"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($caselist as $caselists)
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <table id="ListCaseview" class="display nowrap table table-bordered table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>ประเภทเคส</th>
+                    <th>รายละเอียดเคส</th>
+                    <th>สถานะเคส</th>
+                    <th>Agent</th>
+                    <th>วันที่ทำรายการ</th>
+                    <th width="140px"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($caselist as $caselists)
                     <tr>
                         <td>{{ $caselists->casetype1 }}</td>
                         <td>{{ $caselists->casedetail }}</td>
@@ -30,12 +29,14 @@
                         <td>{{ $caselists->agent }}</td>
                         <td>{{ $caselists->created_at }}</td>
                         <td width="140px">
-                            <button type="button" data-cases_id="{{ $caselists->id }}" data-tabid="{{$cardid}}" class="form-control btn btn-success casedetailP-button">รายละเอียด</button></td>
+                            <button type="button" data-cases_id="{{ $caselists->id }}" data-tabid="{{ $cardid }}"
+                                class="form-control btn btn-success casedetailP-button">รายละเอียด</button>
+                        </td>
                     </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            </div>
-        </div>  
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 {{--     </div>
 </div> --}}
