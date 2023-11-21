0c0348e051a4fddb5dd42cc3b6d2393bfa2d63f2
Taitawatchai <sc_player01@hotmail.com>
2023-11-21 12:33:14 +0700

    test

--- resources/views/detailcaseinternalnumber/script.blade.php
+++ resources/views/detailcaseinternalnumber/script.blade.php
@@ -469,7 +469,7 @@
                             fontSize: 16
                         };
                         // กำหนดความกว้างของ header แต่ละคอลัมน์หัวข้อ
-                        doc.content[1].table.widths = [120, 100, 80, 80, 80, '*'];
+                        doc.content[1].table.widths = ['*', 100, 80, 80, 80, 80];
                         doc.styles.tableHeader.fontSize = 16; // กำหนดขนาด font ของ header
                         // Add cell borders
                         doc.content[1].table.layout = {
