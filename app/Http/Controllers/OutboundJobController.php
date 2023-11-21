be1ced07b473c5b366265f483379ef5e8fb9c860
meeh2029 <manon2029@gmail.com>
2023-11-21 10:23:51 +0700

    Outbound

---
+++ app/Http/Controllers/OutboundJobController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Outbound_job;
+use Illuminate\Http\Request;
+
+class OutboundJobController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Outbound_job $outbound_job)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Outbound_job $outbound_job)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Outbound_job $outbound_job)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Outbound_job $outbound_job)
+    {
+        //
+    }
+}
