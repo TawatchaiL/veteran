9abe4a3551d12cc2d14edc8ea9fa98a17491d51f
meeh2029 <manon2029@gmail.com>
2023-11-22 16:33:12 +0700

    fix

--- resources/views/agentoutbound/index.blade.php
+++ resources/views/agentoutbound/index.blade.php
@@ -57,7 +57,7 @@
                             <div class="row ">
                                 <div class="col-xs-12 col-sm-12 col-md-12">
                                     <div class="row float-lg-left">
-                                        <div class="col-xs-4 col-sm-4 col-md-4">
+                                        <div class="col-xs-3 col-sm-3 col-md-3">
                                             <div class="form-group">
                                                 <strong><i class="fa-solid fa-id-card"></i>
                                                     ประเภทการค้นหา:</strong>
@@ -69,7 +69,7 @@
                                                 </select>
                                             </div>
                                         </div>
-                                        <div class="col-xs-8 col-sm-8 col-md-8">
+                                        <div class="col-xs-9 col-sm-9 col-md-9">
                                             <div class="form-group">
                                                 <strong>
                                                     วันที่:</strong>
