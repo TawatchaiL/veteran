4f347bfcaabfb658acf17b371cd49a99aac801bc
meeh2029 <manon2029@gmail.com>
2023-11-22 10:24:06 +0700

    ดรป

--- resources/views/agentoutbound/index.blade.php
+++ resources/views/agentoutbound/index.blade.php
@@ -14,27 +14,6 @@
                     <ol class="breadcrumb float-sm-right">
                         {{-- <li class="breadcrumb-item"><a href="#">Home</a></li>
                         <li class="breadcrumb-item active">Users Management</li> --}}
-
-                        @can('voice-export-create')
-                            <button type="button" class="btn btn-success" id="CreateButton">
-                                <i class="fas fa-list-ol"></i> เพิ่มรายการ Export ไฟล์บันทึกเสียง </a> </button>
-                        @else
-                            <span class="d-inline-block" tabindex="0" data-toggle="tooltip" data-placement="bottom"
-                                title="คุณไม่มีสิทธิ์ในส่วนนี้">
-                                <button type="button" class="btn btn-success disabled">
-                                    <i class="fas fa-list-ol"></i> เพิ่มรายการ Export ไฟล์บันทึกเสียง </a></button>
-                            </span>
-                        @endcan &nbsp;
-
-                        @can('voice-export-delete')
-                            <button type="button" class="btn btn-danger delete_all_button"><i class="fa fa-trash"></i> ลบทั้งหมด</button>
-                        @else
-                            <span class="d-inline-block" tabindex="0" data-toggle="tooltip" data-placement="bottom"
-                                title="คุณไม่มีสิทธิ์ในส่วนนี้">
-                                <button type="button" class="btn btn-danger disabled"><i class="fa fa-trash"></i>
-                                    ลบทั้งหมด</button>
-                            </span>
-                        @endcan
                     </ol>
 
                 </div>
@@ -48,7 +27,7 @@
                 <div class="col-12">
                     <div class="card card-primary">
                         <div class="card-header">
-                            <h3 class="card-title"><i class="fas fa-list-ol"></i> Export ไฟล์บันทึกเสียงสนทนา </h3>
+                            <h3 class="card-title"><i class="fas fa-list-ol"></i> รายการโทรออก </h3>
                             <div class="card-tools">
                                 <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
                                     <i class="fas fa-minus"></i>
@@ -70,30 +49,20 @@
                                     });
                                 </script>
                             @endif
-                            <form method="post" action="{{ route('voicebackup.destroy_all') }}" name="delete_all"
-                                id="delete_all">
-                                @csrf
-                                @method('POST')
-                                <table id="Listview" class="display nowrap table table-bordered table-striped table-hover">
-                                    <thead>
-                                        <tr>
-                                            <th width="50px"><input type="checkbox" id="check-all" class="flat"></th>
-                                            <th>Name</th>
-                                            <th>วันที่เวลา</th>
-                                            <th>เบอร์ที่ติดต่อ</th>
-                                            <th>Agent</th>
-                                            <th>ประเภท</th>
-                                            <th>สถานะ</th>
-                                            <th width="150px">Progress</th>
-                                            <th width="180px"></th>
-                                            <th>More</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
+                            <table id="Listview" class="display nowrap table table-bordered table-striped table-hover">
+                                <thead>
+                                    <tr>
+                                        <th width="50px"><input type="checkbox" id="check-all" class="flat"></th>
+                                        <th>วันที่เวลา</th>
+                                        <th>เบอร์ที่ติดต่อ</th>
+                                        <th width="180px"></th>
+                                        <th>More</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
 
-                                    </tbody>
-                                </table>
-                            </form>
+                                </tbody>
+                            </table>
                         </div>
 
                     </div>
@@ -105,11 +74,8 @@
         </div>
 
     </section>
-
-
-    @include('voicebackup.create')
 @endsection
 
 @section('script')
-    @include('voicebackup.script')
+    @include('agentoutbound.script')
 @endsection
