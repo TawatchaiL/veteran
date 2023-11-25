2f9dd4d25a8c681dcea9a24b061c24015238a264
meeh2029 <manon2029@gmail.com>
2023-11-26 00:21:50 +0700

    fix

--- resources/views/report_menu.blade.php
+++ resources/views/report_menu.blade.php
@@ -17,114 +17,110 @@
                 </div>
             </div>
             <div class="row">
-               
 
-                    <div class="row">
-                        <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
-                            <div class="info-box">
-                                <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
-                                <div class="info-box-content">
-                                    <span class="info-box-text"> 10 อันดับเรื่องที่ติดต่อมากที่สุด</span>
-                                    <span class="info-box-number text-right"><button
-                                            onclick="window.location.href='{{ route('reportcasetop10') }}'"
-                                            class="btn btn-success btn-sm"><i class="fas fa-search"></i>
-                                            View</button></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
-                            <div class="info-box">
-                                <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
-                                <div class="info-box-content">
-                                    <span class="info-box-text"> ผลรวมแยกตามประเภทที่ติดต่อ</span>
-                                    <span class="info-box-number text-right"><button
-                                            onclick="window.location.href='{{ route('reportsumbytype') }}'"
-                                            class="btn btn-success btn-sm"><i class="fas fa-search"></i>
-                                            View</button></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
-                            <div class="info-box">
-                                <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
-                                <div class="info-box-content">
-                                    <span class="info-box-text"> ผลรวมแยกตามสถานะ</span>
-                                    <span class="info-box-number text-right"><button
-                                            onclick="window.location.href='{{ route('reportsumcasebystatus') }}'"
-                                            class="btn btn-success btn-sm"><i class="fas fa-search"></i>
-                                            View</button></span>
-                                </div>
-                            </div>
-                        </div>
-
-
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
-                            <div class="info-box">
-                                <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
-                                <div class="info-box-content">
-                                    <span class="info-box-text"> ผลรวมแยกตามสถานะการโอนสาย</span>
-                                    <span class="info-box-number text-right"><button
-                                            onclick="window.location.href='{{ route('reportsumcasebytranferstatus') }}'"
-                                            class="btn btn-success btn-sm"><i class="fas fa-search"></i>
-                                            View</button></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
-                            <div class="info-box">
-                                <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
-                                <div class="info-box-content">
-                                    <span class="info-box-text"> ผลรวมเรื่องที่ติดต่อแยกตาม HN</span>
-                                    <span class="info-box-number text-right"><button
-                                            onclick="window.location.href='{{ route('sumcasebyhn') }}'"
-                                            class="btn btn-success btn-sm"><i class="fas fa-search"></i>
-                                            View</button></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
-                            <div class="info-box">
-                                <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
-                                <div class="info-box-content">
-                                    <span class="info-box-text"> รายการเรื่องที่ติดต่อ</span>
-                                    <span class="info-box-number text-right"><button
-                                            onclick="window.location.href='{{ route('detailcases') }}'"
-                                            class="btn btn-success btn-sm"><i class="fas fa-search"></i>
-                                            View</button></span>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
-                            <div class="info-box">
-                                <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
-                                <div class="info-box-content">
-                                    <span class="info-box-text"> รายการเรื่องที่ติดต่อแสดงตามสถานะ</span>
-                                    <span class="info-box-number text-right"><button
-                                            onclick="window.location.href='{{ route('detailcasesstatus') }}'"
-                                            class="btn btn-success btn-sm"><i class="fas fa-search"></i>
-                                            View</button></span>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
-                            <div class="info-box">
-                                <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
-                                <div class="info-box-content">
-                                    <span class="info-box-text">รายการแก้ไข/คอมเม้น เรื่องที่ติดต่อ</span>
-                                    <span class="info-box-number text-right"><button
-                                            onclick="window.location.href='{{ route('detailcaselogbyhn') }}'"
-                                            class="btn btn-success btn-sm"><i class="fas fa-search"></i>
-                                            View</button></span>
-                                </div>
-                            </div>
+                <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text"> 10 อันดับเรื่องที่ติดต่อมากที่สุด</span>
+                            <span class="info-box-number text-right"><button
+                                    onclick="window.location.href='{{ route('reportcasetop10') }}'"
+                                    class="btn btn-success btn-sm"><i class="fas fa-search"></i>
+                                    View</button></span>
                         </div>
                     </div>
+                </div>
+                <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text"> ผลรวมแยกตามประเภทที่ติดต่อ</span>
+                            <span class="info-box-number text-right"><button
+                                    onclick="window.location.href='{{ route('reportsumbytype') }}'"
+                                    class="btn btn-success btn-sm"><i class="fas fa-search"></i>
+                                    View</button></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text"> ผลรวมแยกตามสถานะ</span>
+                            <span class="info-box-number text-right"><button
+                                    onclick="window.location.href='{{ route('reportsumcasebystatus') }}'"
+                                    class="btn btn-success btn-sm"><i class="fas fa-search"></i>
+                                    View</button></span>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text"> ผลรวมแยกตามสถานะการโอนสาย</span>
+                            <span class="info-box-number text-right"><button
+                                    onclick="window.location.href='{{ route('reportsumcasebytranferstatus') }}'"
+                                    class="btn btn-success btn-sm"><i class="fas fa-search"></i>
+                                    View</button></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text"> ผลรวมเรื่องที่ติดต่อแยกตาม HN</span>
+                            <span class="info-box-number text-right"><button
+                                    onclick="window.location.href='{{ route('sumcasebyhn') }}'"
+                                    class="btn btn-success btn-sm"><i class="fas fa-search"></i>
+                                    View</button></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text"> รายการเรื่องที่ติดต่อ</span>
+                            <span class="info-box-number text-right"><button
+                                    onclick="window.location.href='{{ route('detailcases') }}'"
+                                    class="btn btn-success btn-sm"><i class="fas fa-search"></i>
+                                    View</button></span>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text"> รายการเรื่องที่ติดต่อแสดงตามสถานะ</span>
+                            <span class="info-box-number text-right"><button
+                                    onclick="window.location.href='{{ route('detailcasesstatus') }}'"
+                                    class="btn btn-success btn-sm"><i class="fas fa-search"></i>
+                                    View</button></span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-sm-6 col-lg-6  col-xl-3 col-12">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-primary"><i class="fa-solid fa-print"></i></span>
+                        <div class="info-box-content">
+                            <span class="info-box-text">รายการแก้ไข/คอมเม้น เรื่องที่ติดต่อ</span>
+                            <span class="info-box-number text-right"><button
+                                    onclick="window.location.href='{{ route('detailcaselogbyhn') }}'"
+                                    class="btn btn-success btn-sm"><i class="fas fa-search"></i>
+                                    View</button></span>
+                        </div>
+                    </div>
+                </div>
+
 
             </div>
 
