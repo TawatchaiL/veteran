18cf2ad62d4177e8ca6d6f79d2579f878fcfb2a7
meeh2029 <manon2029@gmail.com>
2023-11-25 17:13:20 +0700

    ดรป

--- resources/views/layouts/popup_script.blade.php
+++ resources/views/layouts/popup_script.blade.php
@@ -1075,6 +1075,7 @@
                                     });
 
                                     $('.alert-success-pop' + cardId).hide();
+                                    $('#ToolbarModal').modal('hide');
                                 }
                             }
                         });
