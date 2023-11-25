65bccca05707db0cc72db18e837960f37d20842a
meeh2029 <manon2029@gmail.com>
2023-11-25 23:43:52 +0700

    fix

--- resources/views/report_menu.blade.php
+++ resources/views/report_menu.blade.php
@@ -26,7 +26,7 @@
                                         class="fa-solid fa-arrow-right-to-bracket"></i></span>
                                 <div class="info-box-content">
                                     <span class="info-box-text"> 10 อันดับเรื่องที่ติดต่อมากที่สุด</span>
-                                    <span class="info-box-number text-right"><button class="btn btn-info"> View</button></span>
+                                    <span class="info-box-number text-right"><button class="btn btn-info btn-sm"><i class="fas fa-search"></i> View</button></span>
                                 </div>
 
                             </div>
