169799c3f7bab2a34fab02d8d9c8f7815a7ef97c
TawatchaiL <tawatchai.winit@gmail.com>
2023-11-25 22:09:00 +0700

    test

--- resources/views/billing/script.blade.php
+++ resources/views/billing/script.blade.php
@@ -663,6 +663,7 @@
                 'csv',
                 { // กำหนดพิเศษเฉพาะปุ่ม pdf
                     "extend": 'pdf', // ปุ่มสร้าง pdf ไฟล์
+                    "footer": true,
                     "text": 'PDF', // ข้อความที่แสดง
                     "pageSize": 'A4', // ขนาดหน้ากระดาษเป็น A4
                     "orientation": 'landscape',
