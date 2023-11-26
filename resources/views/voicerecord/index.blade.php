188a32c06acdc3694cf1c652c502c65f0781a50a
meeh2029 <manon2029@gmail.com>
2023-11-26 22:15:15 +0700

    fix

--- resources/views/voicerecord/index.blade.php
+++ resources/views/voicerecord/index.blade.php
@@ -8,7 +8,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-                    <div class="card card-info">
+                    <div class="card card-primary">
                         <div class="card-header">
                             <h3 class="card-title"><i class="fa-solid fa-magnifying-glass"></i> Filter</h3>
                             <div class="card-tools">
