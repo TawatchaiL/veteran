f0d37e8550a58a15a3a5d8b068a05c81c1c69c03
TawatchaiL <tawatchai.winit@gmail.com>
2023-11-27 00:26:53 +0700

    sss

--- app/Http/Controllers/ReportreportsumcasebystatusController.php
+++ app/Http/Controllers/ReportreportsumcasebystatusController.php
@@ -44,8 +44,8 @@ class ReportreportsumcasebystatusController extends Controller
                 }
             }
         }else{
-                    $startDate = date("Y-m-d");
-                    $endDate = date("Y-m-t", strtotime($startDate));  
+            $startDate = date("Y-m-d H:i:s");
+            $endDate = date("Y-m-t H:i:s", strtotime($startDate));  
         }
 
             $datas = DB::table('crm_cases')
