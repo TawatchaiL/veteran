ac14c5a904c4664bbf663ecfb2f5e2011149deb4
TawatchaiL <tawatchai.winit@gmail.com>
2023-11-29 23:17:55 +0700

    ะำหะ

--- resources/views/cases/commentdetail.blade.php
+++ resources/views/cases/commentdetail.blade.php
@@ -1,75 +1,151 @@
-<div class="card">
-    <div class="card-body">
-        <div class="row">
-            <div class="col-xs-6 col-sm-6 col-md-6">
-                    <strong><i class="fas fa-code"></i> HN : {{ $datact->hn }}                                       
-            </div>
-            <div class="col-xs-6 col-sm-6 col-md-6">
-                    <strong><i class="fas fa-user-tie"></i> ชื่อ-สกุล : </strong>{{ $datact->tname }}{{ $datact->fname }} {{ $datact->lname }}                                 
-            </div>
-        </div>
+<div class="row">
+    <div class="col-md-6">
+        <aside class="sidebarr">
+            <div class="single contact-info">
+                <h4 class="side-title">รายละเอียด เรื่องที่ติดต่อ</h4>
+                <ul class="list-unstyled">
+                    <li>
+                        <div class="icon"><i class="fas fa-code"></i></div>
+                        <div class="info">
+                            <p><strong>HN</strong> <br>&nbsp;{{ $datact->hn !== '' ? $datact->hn : '-' }}</p>
+                        </div>
+                    </li>
 
-        <div class="row">
-            <div class="col-xs-6 col-sm-6 col-md-6">
-                    <strong><i class="fa-regular fa-message"></i> ประเภทเคส : </strong>{{ $cases->casetype1 }}  
+                    <li>
+                        <div class="icon"><i class="fa-solid fa-list-ul"></i></div>
+                        <div class="info">
+                            <p><strong>ประเภทเคส</strong> <br>&nbsp;{{ $cases->casetype1 !== '' ? $cases->casetype1 : '-'  }}</p>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="icon"><i class="fa-solid fa-list-ul"></i></div>
+                        <div class="info">
+                            <p><strong>รายละเอียดเคสย่อย</strong><br>&nbsp;{{ $cases->casetype3 !== '' ? $cases->casetype3 : '-'  }}</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="icon"><i class="fa-solid fa-list-ul"></i></div>
+                        <div class="info">
+                            <p><strong>รายละเอียดเคส เพิ่มเติม 2</strong><br>&nbsp;{{ $cases->casetype5 !== '' ? $cases->casetype5 : '-'  }}</p>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <div class="col-xs-6 col-sm-6 col-md-6">
-                <strong><i class="fa-regular fa-message"></i> รายละเอียดเคส : </strong>{{ $cases->casetype2 }} 
+        </aside>
+    </div>
+    <div class="col-md-6">
+        <aside class="sidebarr">
+            <div class="single contact-info">
+                <h4 class="side-title">&nbsp;</h4>
+                <ul class="list-unstyled">
+                    <li>
+                        <div class="icon"><i class="fas fa-user-tie"></i></div>
+                        <div class="info">
+                            <p><strong>ชื่อ-สกุล</strong> <br>&nbsp;{{ $datact->tname }}{{ $datact->fname }}
+                                {{ $datact->lname }}</p>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="icon"><i class="fa-solid fa-list-ul"></i></div>
+                        <div class="info">
+                            <p><strong>รายละเอียดเคส</strong><br>&nbsp;{{ $cases->casetype2 !== '' ? $cases->casetype2 : '-'  }}</p>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="icon">
+                            <i class="fa-solid fa-list-ul"></i>
+                        </div>
+                        <div class="info">
+                            <p><strong>รายละเอียดเคส เพิ่มเติม 1</strong><br>&nbsp;{{ $cases->casetype4 !== '' ? $cases->casetype4 : '-'  }}</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="icon"><i class="fa-solid fa-list-ul"></i></div>
+                        <div class="info">
+                            <p><strong>รายละเอียดเคส เพิ่มเติม 3</strong><br>&nbsp;{{ $cases->casetype6 !== '' ? $cases->casetype6 : '-'  }}</p>
+                        </div>
+                    </li>
+                </ul>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-6 col-sm-6 col-md-6">
-                    <strong><i class="fa-regular fa-message"></i> รายละเอียดเคสย่อย : </strong>{{ $cases->casetype3 }} 
+        </aside>
+    </div>
+    <div class="col-md-12">
+        <aside class="sidebarr">
+            <div class="single contact-info">
+                <h4 class="side-title"></h4>
+                <ul class="list-unstyled">
+                    <li>
+                        <div class="icon"><i class="fa-regular fa-comment-dots"></i></div>
+                        <div class="info">
+                            <p><strong>รายละเอียด</strong> <br>&nbsp;{{ $cases->casedetail !== '' ? $cases->casedetail : '-'  }}</p>
+                        </div>
+                    </li>
+
+
+                </ul>
             </div>
-            <div class="col-xs-6 col-sm-6 col-md-6">
-                <strong><i class="fa-regular fa-message"></i> รายละเอียดเคส เพิ่มเติม 1 : </strong>{{ $cases->casetype4 }} 
+        </aside>
+    </div>
+    <div class="col-md-6">
+        <aside class="sidebarr">
+            <div class="single contact-info">
+                <h4 class="side-title">สถานะ เรื่องที่ติดต่อ</h4>
+                <ul class="list-unstyled">
+    
+                    <li>
+                        <div class="icon"><i class="fas fa-shuffle"></i></div>
+                        <div class="info">
+                            <p><strong>สถานะการโอนสาย</strong> <br>&nbsp;{{ $cases->tranferstatus }}</p>
+                        </div>
+                    </li>
+    
+    
+                </ul>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-6 col-sm-6 col-md-6">
-                    <strong><i class="fa-regular fa-message"></i> รายละเอียดเคส เพิ่มเติม 2 : </strong>{{ $cases->casetype5 }} 
+        </aside>
+    </div>
+    <div class="col-md-6">
+        <aside class="sidebarr">
+            <div class="single contact-info">
+                <h4 class="side-title">&nbsp;</h4>
+                <ul class="list-unstyled">
+    
+                    <li>
+                        <div class="icon"><i class="fas fa-arrows-rotate"></i></div>
+                        <div class="info">
+                            <p><strong>สถานะการเคส</strong> <br>&nbsp;{{ $cases->casestatus }}</p>
+                        </div>
+                    </li>
+    
+    
+                </ul>
             </div>
-            <div class="col-xs-6 col-sm-6 col-md-6">
-                <strong><i class="fa-regular fa-message"></i> รายละเอียดเคส เพิ่มเติม 3 : </strong>{{ $cases->casetype6 }} 
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                    <strong><i class="fa-regular fa-comment-dots"></i>
-                        รายละเอียด : </strong>{{ $cases->casedetail }} 
-            </div>
-        </div>
+        </aside>
     </div>
 </div>
-<div class="card">
-    <div class="card-body">
-        <div class="row">
-            <div class="col-xs-6 col-sm-6 col-md-6">
-                    <strong><i class="fas fa-shuffle"></i> สถานะการโอนสาย : </strong>{{ $cases->tranferstatus }} 
-            </div>
-            <div class="col-xs-6 col-sm-6 col-md-6">
-                    <strong><i class="fas fa-arrows-rotate"></i> สถานะการเคส : </strong>{{ $cases->casestatus }} 
-            </div>
+<div class="col-md-12">
+    <aside class="sidebarr">
+        <div class="single contact-info">
+            <h4 class="side-title"></h4>
+            <ul class="list-unstyled">
+                <li>
+                    <div class="icon"><i class="fa-regular fa-comment-dots"></i></div>
+                    <div class="info">
+                        <p><strong>การแสดงความคิดเห็น</strong> <br>&nbsp;{{ $casecomment->comment !== '' ? $casecomment->comment : '-'  }}</p>
+                    </div>
+                </li>
+
+
+            </ul>
         </div>
-    </div>
+    </aside>
 </div>
-<div class="card">
-    <div class="card-body">
-        <div class="row">
-            <div class="col-xs-6 col-sm-6 col-md-6">
-                    <strong><i class="fas fa-shuffle"></i> แสดงความคิดเห็น : </strong>
-                    {{ $casecomment->comment }} 
-            </div>
-        </div>
-    </div>
-</div>
-<div class="card">
-    <div class="card-body">
-        <div class="row justify-content-end">
-            <button type="button" data-case_id="{{ $cases->id }}" class="btn btn-success listcomment-button" id="ListcommentButton"><i class="fas fa-download"></i>
-                ย้อนกลับ</button>&nbsp;
-            <button type="button" class="btn btn-danger modelClose" data-dismiss="modal"><i
-                    class="fas fa-door-closed"></i> ปิดหน้าต่าง</button>
-        </div>
-    </div>
+<div class="row justify-content-end">
+    <button type="button" data-case_id="{{ $cases->id }}" class="btn btn-success listcomment-button" id="ListcommentButton"><i class="fa-solid fa-backward"></i>
+        ย้อนกลับ</button>&nbsp;
+    <button type="button" class="btn btn-danger modelClose" data-dismiss="modal"><i
+            class="fas fa-door-closed"></i> ปิดหน้าต่าง</button>
 </div>
