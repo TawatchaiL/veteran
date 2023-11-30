8e9502df150542cc1b4e490665eea9f035f6f3a3
meeh2029 <manon2029@gmail.com>
2023-11-30 11:45:01 +0700

    fix

--- public/ldap3.php
+++ public/ldap3.php
@@ -1,10 +1,10 @@
 <?php
 $ldaphost = "192.168.1.103"; // Replace with the actual IP address of your Synology NAS
 $ldapport = "389"; // Default LDAP port
-$ldapdn = "cn=users,dc=manon2029"; // Replace with the admin DN of your Synology LDAP
+$ldapdn = "uid=root,cn=users,dc=synology"; // Replace with the admin DN of your Synology LDAP
 $ldappass = "Hackbychu@2022"; // Replace with the admin password
 
-$base = "ou=users,dc=manon2029"; // Replace with the base DN of your LDAP directory
+$base = "ou=users,dc=synology"; // Replace with the base DN of your LDAP directory
 $filter = "(&(objectClass=person)(cn=*))"; // Adjust the filter based on your requirements
 
 $ds = ldap_connect($ldaphost, $ldapport);
