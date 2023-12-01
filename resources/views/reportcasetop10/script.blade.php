49665bad19f2b0dff9ec910d4fa135554d5e6cca
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-01 23:18:15 +0700

    sfds

--- resources/views/reportcasetop10/script.blade.php
+++ resources/views/reportcasetop10/script.blade.php
@@ -596,8 +596,6 @@
             sPaginationType: "full_numbers",
             dom: 'T<"clear">lfrtip',
             columns: [{
-                    data: 'rowno',
-                    name: 'rowno'
                 },{
                     data: 'casetype1',
                     name: 'casetype1'
