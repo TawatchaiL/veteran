f19057b6e336a117b2fc05bafb522afff45e509d
Taitawatchai <sc_player01@hotmail.com>
2023-12-04 12:53:41 +0700

    open pdf

--- resources/views/reportcasetop10/script.blade.php
+++ resources/views/reportcasetop10/script.blade.php
@@ -500,6 +500,7 @@
                     "text": 'PDF', // ข้อความที่แสดง
                     "pageSize": 'A4', // ขนาดหน้ากระดาษเป็น A4
                     "title": '10 อันดับเรื่องที่ติดต่อมากที่สุด',
+                    "download": 'open',
                     exportOptions: {
                         columns: ':visible:not(.no-print)',
                     },
