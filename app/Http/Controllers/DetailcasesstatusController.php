40d3f5d9811f7358a69de61358f34e010265af79
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-05 22:11:48 +0700

    test

--- app/Http/Controllers/DetailcasesstatusController.php
+++ app/Http/Controllers/DetailcasesstatusController.php
@@ -49,7 +49,7 @@ class DetailcasesstatusController extends Controller
         }
 
         $datas = DB::table('crm_cases')
-            ->select(DB::raw(DB::raw('ROW_NUMBER() OVER (ORDER BY crm_cases.created_at ASC) as row_number'),'DATE(crm_cases.created_at) as cdate'), DB::raw('TIME(crm_cases.created_at) as ctime'), 'telno', 'casetype1', 'casedetail', 'casestatus', 'tranferstatus', 'users.name as agent')
+            ->select(DB::raw(DB::raw('ROW_NUMBER() OVER (ORDER BY cdate ASC) as row_number'),'DATE(crm_cases.created_at) as cdate'), DB::raw('TIME(crm_cases.created_at) as ctime'), 'telno', 'casetype1', 'casedetail', 'casestatus', 'tranferstatus', 'users.name as agent')
             ->join('users', 'crm_cases.agent', '=', 'users.id')
             ->whereRaw('adddate between "' . $startDate . '" and "' . $endDate . '" and casestatus = "' . $request->input('casesstatus') . '"')
             ->get();
