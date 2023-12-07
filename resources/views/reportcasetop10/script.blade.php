64068f30eb19ff25062a9a5103dd10ce2abb1333
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-07 23:59:38 +0700

    test

--- resources/views/reportcasetop10/script.blade.php
+++ resources/views/reportcasetop10/script.blade.php
@@ -586,14 +586,18 @@
                         // Customize the print layout
                         $(win.document.body).prepend('<img style="position:absolute; top:0; left:465;width:100" src='+logobase64+'>')
                         $(win.document.body).find('h1').css('text-align', 'center').css('font-size','16px').css('margin-top','105px');
-                        $(win.document.body).find('table').addClass('display').css('font-size','12px').removeClass('dataTable').css('margin-top','5px').css('margin-bottom','60px');
+                        $(win.document.body).find('table').addClass('display').css('font-size','12px')
+                                            .removeClass('dataTable').css('margin-top','5px').css('margin-bottom','60px');
                         $(win.document.body).find('table.dataTable th, table.dataTable td').css(
-                            'border', '1px solid #ddd');
+                            'border', '1px solid black');
                         $(win.document.body).find('table.dataTable th').css('background-color',
                             '#f2f2f2');
                         $(win.document.body).find('table.dataTable td:nth-child(0)').css(
                             'width', '50px');
                     }
+                },
+                exportOptions: {
+                    columns: [40, 400, '*'],
                 }
             ],
             layout: {
