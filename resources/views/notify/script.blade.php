236ce274d6f18fed4f2e53f3566bd0e7d2cee80e
meeh2029 <manon2029@gmail.com>
2023-12-07 11:57:28 +0700

    fix

--- resources/views/notify/script.blade.php
+++ resources/views/notify/script.blade.php
@@ -53,13 +53,13 @@
         var currentDate = new Date();
         var currentYear = currentDate.getFullYear();
         var maxYear = currentYear + 1;
-       
 
-        $(".datepick").datepicker_thai({
+
+        $(".datepick").datepicker({
             changeMonth: true,
             changeYear: true,
-            langTh: true,
-            yearTh: true,
+            /* langTh: true,
+            yearTh: true, */
             yearRange: '2023' + ':' + maxYear,
             dateFormat: 'dd/mm/yy',
             /* isBE: true,
