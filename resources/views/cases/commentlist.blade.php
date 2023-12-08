96470407cfd78f46236b31d3593a4d6269e8a4b2
meeh2029 <manon2029@gmail.com>
2023-12-08 13:34:57 +0700

    fix

--- resources/views/cases/commentlist.blade.php
+++ resources/views/cases/commentlist.blade.php
@@ -1,46 +1,29 @@
-<div class="card card-primary">
-    <div class="card-header">
-        <h3 class="card-title"><i class="fas fa-address-book"></i> ประวัติการแสดงความคิดเห็นเรื่องที่ติดต่อ</h3>
-        <div class="card-tools">
-        </div>
-    </div>
-</div>
-<div class="card">
-    <div class="card-body">
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <table id="Listview"
-                class="display nowrap table table-bordered table-striped table-hover">
-                <thead>
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <table id="Listview" class="display nowrap table table-bordered table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>วันที่ทำรายการ</th>
+                    <th>ผู้ทำรายการ</th>
+                    <th width="60%">การแสดงความคิดเห็น</th>
+                    <th width="140px"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($casecomment as $casecomments)
                     <tr>
-                        <th>การแสดงความคิดเห็น</th>
-                        <th>Agent</th>
-                        <th>วันที่ทำรายการ</th>
-                        <th width="140px"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($casecomment as $casecomments)
-                    <tr>
-                        <td>{{ $casecomments->comment }}</td>
-                        <td>{{ $casecomments->agentname }}</td>
                         <td>{{ $casecomments->created_at }}</td>
+                        <td>{{ $casecomments->agentname }}</td>
+                        <td>{{ $casecomments->comment }}</td>
                         <td width="140px">
-                            <button type="button" data-comment_id="{{ $casecomments->id }}" class="form-control btn btn-success selectcomment-button">รายละเอียด
-                            </button></td>
+                            <button type="button" data-comment_id="{{ $casecomments->id }}"
+                                class="form-control btn btn-warning selectcomment-button"><i
+                                    class="fa-solid fa-comment-dots"></i> รายละเอียด
+                            </button>
+                        </td>
                     </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            </div>
-        </div>  
-    </div>
-</div>
-<div class="card">
-    <div class="card-body">
-        <div class="row justify-content-end">
-            <button type="button" class="btn btn-danger modelClose" data-dismiss="modal"><i
-                    class="fas fa-door-closed"></i> ปิดหน้าต่าง</button>
-        </div>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </div>
