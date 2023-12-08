62f621128f69b12020df1a957d9b4cd2b517b402
meeh2029 <manon2029@gmail.com>
2023-12-08 14:50:11 +0700

    fix

--- resources/views/cases/caseslist.blade.php
+++ resources/views/cases/caseslist.blade.php
@@ -8,35 +8,43 @@
 <div class="card">
     <div class="card-body"> --}}
 <div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <table id="ListCaseview" class="display nowrap table table-bordered table-striped table-hover">
-            <thead>
-                <tr>
-                    <th>วันที่ทำรายการ</th>
-                    <th>ประเภทเคส</th>
-                    <th>รายละเอียดเคส</th>
-                    <th>สถานะเคส</th>
-                    <th>Agent ที่รับ</th>
-                    <th width="120px"></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($caselist as $caselists)
+    <div class="card col-xs-12 col-sm-12 col-md-12">
+        <div class="card-header">
+            <h3 class="card-title"></h3>
+        </div>
+
+        <div class="card-body p-0" style="height: 300px;">
+            <table id="Listview"
+                class="table table-sm table-head-fixed text-nowrap table-bordered table-striped table-hover">
+                <thead>
                     <tr>
-                        <td>{{ $caselists->created_at }}</td>
-                        <td>{{ $caselists->casetype1 }}</td>
-                        <td>{{ $caselists->casedetail }}</td>
-                        <td>{{ $caselists->casestatus }}</td>
-                        <td>{{ $agent[$caselists->agent]['name'] }}</td>
-                        <td width="140px">
-                            <button type="button" data-cases_id="{{ $caselists->id }}" data-tabid="{{ $cardid }}"
-                                class="form-control btn btn-warning btn-sm casedetailP-button"><i
-                                    class="fas fa-search"></i> รายละเอียด</button>
-                        </td>
+                        <th>วันที่ทำรายการ</th>
+                        <th>ประเภทเคส</th>
+                        <th>รายละเอียดเคส</th>
+                        <th>สถานะเคส</th>
+                        <th>Agent ที่บันทึก</th>
+                        <th width="120px"></th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($caselist as $caselists)
+                        <tr>
+                            <td>{{ $caselists->created_at }}</td>
+                            <td>{{ $caselists->casetype1 }}</td>
+                            <td>{{ $caselists->casedetail }}</td>
+                            <td>{{ $caselists->casestatus }}</td>
+                            <td>{{ $agent[$caselists->agent]['name'] }}</td>
+                            <td width="140px">
+                                <button type="button" data-cases_id="{{ $caselists->id }}"
+                                    data-tabid="{{ $cardid }}"
+                                    class="form-control btn btn-warning btn-sm casedetailP-button"><i
+                                        class="fas fa-search"></i> รายละเอียด</button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 {{--     </div>
