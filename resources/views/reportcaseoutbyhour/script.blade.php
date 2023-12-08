3a3ad8204921122820295329053cb42a196a7c14
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-08 23:19:26 +0700

    dd

--- resources/views/reportcaseoutbyhour/script.blade.php
+++ resources/views/reportcaseoutbyhour/script.blade.php
@@ -475,17 +475,20 @@
                     "text": 'PDF', // ข้อความที่แสดง
                     "pageSize": 'A4', // ขนาดหน้ากระดาษเป็น A4
                     "title": 'ผลรวมสายเข้าภายนอกแยกตามช่วงเวลา',
+                    "download": 'open',
                     exportOptions: {
                         columns: ':visible:not(.no-print)',
                     },
-                    customize: function ( doc ) {
-                    doc.defaultStyle = {
+                    customize: function(doc) {
+                        doc.defaultStyle = {
                             font: 'THSarabun',
                             fontSize: 16
                         };
-                        doc.content.splice(0,1);
-                        doc.pageMargins = [20,100,20,30];
-						doc.styles.tableHeader.fontSize = 16;
+                        doc.content.splice(0, 1);
+                        doc.pageMargins = [20, 100, 20, 30];
+                        doc.styles.tableHeader.fontSize = 16;
+                        doc.styles.tableBodyOdd.alignment = 'center';
+                        doc.styles.tableBodyEven.alignment = 'center';
                         doc.styles.tableFooter.fontSize = 16;
                         doc['header']=(function() {
 							return {
@@ -541,11 +544,12 @@
                     },
                     customize: function(win) {
                         // Customize the print layout
-                        $(win.document.body).find('h1').css('text-align', 'center');
-                        $(win.document.body).find('table').addClass('display').css('font-size',
-                            '12px');
+                        $(win.document.body).prepend('<img style="position:absolute; top:0; left:470;width:100" src='+logobase64+'>')
+                        $(win.document.body).find('h1').css('text-align', 'center').css('font-size','16px').css('margin-top','105px');
+                        $(win.document.body).find('table').addClass('display').css('font-size','12px')
+                                            .removeClass('dataTable').css('margin-top','5px').css('margin-bottom','60px');
                         $(win.document.body).find('table.dataTable th, table.dataTable td').css(
-                            'border', '1px solid #ddd');
+                            'border', '1px solid black');
                         $(win.document.body).find('table.dataTable th').css('background-color',
                             '#f2f2f2');
                         $(win.document.body).find('table.dataTable td:nth-child(0)').css(
@@ -572,13 +576,7 @@
             responsive: true,
             sPaginationType: "full_numbers",
             dom: 'T<"clear">lfrtip',
-            columns: [{
-                    data: 'checkbox',
-                    name: 'checkbox',
-                    orderable: false,
-                    searchable: false,
-                    className: 'no-print'
-                },
+            columns: [
                 {
                     data: 'timelabel',
                     name: 'timelabel'
