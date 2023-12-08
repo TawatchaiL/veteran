4d650244b530cfa5a2bf26b0cfca7db9bc191f25
meeh2029 <manon2029@gmail.com>
2023-12-08 20:09:08 +0700

    fix

--- app/Http/Controllers/CasesController.php
+++ app/Http/Controllers/CasesController.php
@@ -109,7 +109,8 @@ class CasesController extends Controller
                     return '<input type="checkbox" id="' . $row->id . '" class="flat" name="table_records[]" value="' . $row->id . '" >';
                 })
                 ->editColumn('created_at', function ($row) {
-                    $adddate = Carbon::parse($row->created_at)->addYears(543)->format('d/m/Y H:i:s');
+                    //$adddate = Carbon::parse($row->created_at)->addYears(543)->format('d/m/Y H:i:s');
+                    $adddate = Carbon::parse($row->created_at)->format('Y-m-d H:i:s');
                     return $adddate;
                 })
                 ->addColumn('action', function ($row) {
