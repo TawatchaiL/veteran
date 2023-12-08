0509301c793fdc9bd59d2c6bf163741a3a1283e0
meeh2029 <manon2029@gmail.com>
2023-12-08 23:55:03 +0700

    fix

--- resources/views/cases/script.blade.php
+++ resources/views/cases/script.blade.php
@@ -94,6 +94,7 @@
             ];
             var last7DaysRange = [moment().subtract(6, 'days').startOf('day'), moment().endOf('day')];
             var last30DaysRange = [moment().subtract(29, 'days').startOf('day'), moment().endOf('day')];
+            var currentYear = moment().year();
             var maxYear = moment().year(currentYear).add(1, 'year').format('YYYY-MM-DD');
             var minYear = moment().year(currentYear).subtract(2, 'years').format('YYYY-MM-DD');
 
