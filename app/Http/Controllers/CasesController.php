0054c2425d302ce5de9cfa8d8074cadad4198a9f
meeh2029 <manon2029@gmail.com>
2023-12-09 09:40:38 +0700

    fix

--- app/Http/Controllers/CasesController.php
+++ app/Http/Controllers/CasesController.php
@@ -31,6 +31,7 @@ class CasesController extends Controller
         
         if ($request->ajax()) {
             dd($request->input('seachtext'));
+            dd($request->input('seachtype'));
             if (!empty($request->get('sdate'))) {
                 $dateRange = $request->input('sdate');
                 if ($dateRange) {
