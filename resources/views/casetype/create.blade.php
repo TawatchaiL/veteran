225a876dc334b8f3cb38e3707ae93a5ace54ab93
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-10 11:14:20 +0700

    ss

--- resources/views/casetype/create.blade.php
+++ resources/views/casetype/create.blade.php
@@ -39,28 +39,6 @@
                         </div>
                     </div>
                     <div class="col-xs-6 col-sm-6 col-md-6" id="targettext">
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-12 col-md-12">
-                                <div class="form-group" id="typelev5">
-
-                                    {!! Form::text('name5', null, ['id' => 'AddName5', 'placeholder' => 'Name', 'class' => 'form-control']) !!}
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-12 col-md-12">
-                                <div class="form-group" id="typelev5">
-                                    {!! Form::text('name5', null, ['id' => 'AddName5', 'placeholder' => 'Name', 'class' => 'form-control']) !!}
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xs-12 col-sm-12 col-md-12">
-                                <div class="form-group" id="typelev5">
-                                    {!! Form::text('name5', null, ['id' => 'AddName5', 'placeholder' => 'Name', 'class' => 'form-control']) !!}
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
