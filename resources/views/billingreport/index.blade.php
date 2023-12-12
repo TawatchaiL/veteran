77d660d845806c5f9dd9b6970086d5b867391a3d
manon618 <manon2029@gmail.com>
2023-12-12 21:39:02 +0700

    dci

--- resources/views/billingreport/index.blade.php
+++ resources/views/billingreport/index.blade.php
@@ -21,7 +21,7 @@
                             </div>
                         </div>
                         <div class="card-body">
-                            <div class="row col-sm-12">
+                            <div class="row">
                                 <ol class="breadcrumb float-sm-center">
 
                                     <div class="col-xs-4 col-sm-4 col-md-4">
@@ -105,7 +105,7 @@
 
                                 </ol>
                             </div>
-                            <div class="row col-sm-12">
+                            <div class="row ">
                                 <div class="col-xs-2 col-sm-2 col-md-2 align-self-end">
                                     <div class="form-group">
                                         <a class="btn btn-success" id="searchButton"> <i class="fas fa-search"></i>
