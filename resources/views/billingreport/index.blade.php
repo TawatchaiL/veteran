4e70af3e448183a7af8df5d4b3c88d2045fd0c66
manon618 <manon2029@gmail.com>
2023-12-12 21:40:22 +0700

    fix

--- resources/views/billingreport/index.blade.php
+++ resources/views/billingreport/index.blade.php
@@ -102,18 +102,18 @@
                                             </select>
                                         </div>
                                     </div>
-
+                                    <div class="col-xs-2 col-sm-2 col-md-2 align-self-end">
+                                        <div class="form-group">
+                                            <a class="btn btn-success" id="searchButton"> <i class="fas fa-search"></i>
+                                            </a>
+                                            <a class="btn bg-warning" id="resetSearchButton"> <i class="fa-solid fa-rotate"></i>
+                                            </a>
+                                        </div>
+                                    </div>
                                 </ol>
                             </div>
                             <div class="row ">
-                                <div class="col-xs-2 col-sm-2 col-md-2 align-self-end">
-                                    <div class="form-group">
-                                        <a class="btn btn-success" id="searchButton"> <i class="fas fa-search"></i>
-                                        </a>
-                                        <a class="btn bg-warning" id="resetSearchButton"> <i class="fa-solid fa-rotate"></i>
-                                        </a>
-                                    </div>
-                                </div>
+
                             </div>
                         </div>
                     </div>
