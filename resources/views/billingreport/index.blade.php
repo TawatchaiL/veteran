2790f95319176dab46f2e8d1b978c0a3019248e3
manon618 <manon2029@gmail.com>
2023-12-12 21:44:44 +0700

    fix

--- resources/views/billingreport/index.blade.php
+++ resources/views/billingreport/index.blade.php
@@ -24,7 +24,7 @@
                             <div class="row">
                                 <ol class="breadcrumb float-sm-center">
 
-                                    <div class="col-xs-4 col-sm-4 col-md-4">
+                                    <div class="col-xs-5 col-sm-5 col-md-5">
                                         <div class="form-group">
                                             <strong>
                                                 วันที่โทรเข้า:</strong>
