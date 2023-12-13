2940512b2651cd9a3b596ce06dd1e4c5bc0670d1
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-13 22:03:07 +0700

    tetwst

--- resources/views/casetype/script.blade.php
+++ resources/views/casetype/script.blade.php
@@ -1,22 +1,5 @@
 <script>
     $(document).ready(function() {
-        $(".delete_all_button").click(function() {
-            var len = $('input[name="table_records[]"]:checked').length;
-            if (len > 0) {
-
-                if (confirm("ยืนยันการลบข้อมูล ?")) {
-                    $('form#delete_all').submit();
-                }
-            } else {
-                alert("กรุณาเลือกรายการที่จะลบ");
-            }
-
-        });
-
-        $('#check-all').click(function() {
-            $(':checkbox.flat').prop('checked', this.checked);
-        });
-
         //$.noConflict();
         var token = ''
         $.ajaxSetup({
@@ -24,93 +7,6 @@
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
         });
-
-
-        var table = $('#Listview').DataTable({
-            /*"aoColumnDefs": [
-            {
-            'bSortable': true,
-            'aTargets': [0]
-            } //disables sorting for column one
-            ],
-            "searching": false,
-            "lengthChange": false,
-            "paging": false,
-            'iDisplayLength': 10,
-            "sPaginationType": "full_numbers",
-            "dom": 'T<"clear">lfrtip',
-                */
-            ajax: '',
-            serverSide: true,
-            processing: true,
-            language: {
-                loadingRecords: '&nbsp;',
-                processing: `<div class="spinner-border text-primary"></div>`,
-                "sProcessing": "กำลังดำเนินการ...",
-                "sLengthMenu": "แสดง_MENU_ แถว",
-                "sZeroRecords": "ไม่พบข้อมูล",
-                "sInfo": "แสดง _START_ ถึง _END_ จาก _TOTAL_ รายการ",
-                "sInfoEmpty": "แสดง 0 ถึง 0 จาก 0 รายการ",
-                "sInfoFiltered": "(กรองข้อมูล _MAX_ ทุกแถว)",
-                "sInfoPostFix": "",
-                "sSearch": "ค้นหา:",
-                "sUrl": "",
-                "oPaginate": {
-                    "sFirst": "เริ่มต้น",
-                    "sPrevious": "ก่อนหน้า",
-                    "sNext": "ถัดไป",
-                    "sLast": "สุดท้าย"
-                }
-            },
-            aaSorting: [
-                [0, "desc"]
-            ],
-            iDisplayLength: 5,
-            lengthMenu: [5, 10, 25, 50, 75, 100],
-            stateSave: true,
-            autoWidth: false,
-            responsive: true,
-            sPaginationType: "full_numbers",
-            dom: 'T<"clear">lfrtip',
-            columns: [{
-                    data: 'checkbox',
-                    name: 'checkbox',
-                    orderable: false,
-                    searchable: false
-                },
-                {
-                    data: 'name',
-                    name: 'name'
-                },
-                {
-                    data: 'status',
-                    name: 'status'
-                },
-                {
-                    data: 'action',
-                    name: 'action'
-                },
-            ]
-        });
-
-
-        $("#example1").DataTable({
-            "responsive": true,
-            "lengthChange": false,
-            "autoWidth": false,
-            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-        $('#example2').DataTable({
-            "paging": true,
-            "lengthChange": false,
-            "searching": false,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false,
-            "responsive": true,
-        });
-
-
 
         $(document).on('click', '#CreateButton', function(e) {
             e.preventDefault();
@@ -179,181 +75,6 @@
             $('#typelev6').hide();
             $('#CreateModal').modal('show');
         });
-
-
-        // Create product Ajax request.
-        $('#SubmitCreateForm').click(function(e) {
-            e.preventDefault();
-            $('.alert-danger').html('');
-            $('.alert-danger').hide();
-            $('.alert-success').html('');
-            $('.alert-success').hide();
-
-            if ($('#customCheckbox1').is(":checked")) {
-                sstatus = 1;
-            } else {
-                sstatus = 0;
-            }
-            for (let c = 1; c < 7; c++) {
-                if (!$('#AddName' + c).prop('disabled')) {
-                    crmname = $('#AddName' + c).val();
-                    l = c;
-                    p = c - 1;
-                    if(c == 1){
-                        parent_id = 0;
-                    }else{
-                        parent_id = $('#casetype' + p).val();
-                    }
-                    break;
-                }
-            }
-            var additionalData = {
-                parent_id: parent_id,
-                name: crmname,
-                crmlev: l,
-                status: sstatus,
-            };
-            $.ajax({
-                url: "{{ route('casetype.store') }}",
-                method: 'post',
-                data: additionalData,
-                success: function(result) {
-                    if (result.errors) {
-                        $('.alert-danger').html('');
-                        $.each(result.errors, function(key, value) {
-                            $('.alert-danger').show();
-                            $('.alert-danger').append('<strong><li>' + value +
-                                '</li></strong>');
-                        });
-                    } else {
-                        $('.alert-danger').hide();
-                        $('.alert-success').show();
-                        $('.alert-success').append('<strong><li>' + result.success +
-                            '</li></strong>');
-                        toastr.success(result.success, {
-                            timeOut: 5000
-                        });
-                        $('#Listview').DataTable().ajax.reload();
-                        $('.form').trigger('reset');
-                        $('#CreateModal').modal('hide');
-                    }
-                },
-                error: function(result) {
-                    alert('error; ' + result.responseText);
-
-                }
-            });
-        });
-
-        let id;
-        $(document).on('click', '#getEditData', function(e) {
-            e.preventDefault();
-
-
-            $('.alert-danger').html('');
-            $('.alert-danger').hide();
-            $('.alert-success').html('');
-            $('.alert-success').hide();
-
-            id = $(this).data('id');
-            $.ajax({
-                url: "casetype/edit/" + id,
-                method: 'GET',
-                success: function(res) {
-                    console.log(res);
-                    $('#EditName').val(res.data.name);
-                    $('#EditModalBody').html(res.html);
-                    if (res.data.status == 1) {
-                        $('#ecustomCheckbox1').prop('checked', true);
-                    } else {
-                        $('#ecustomCheckbox1').prop('checked', false);
-                    }
-                    $('#EditModal').modal('show');
-                }
-            });
-
-        })
-
-        $('#SubmitEditForm').click(function(e) {
-            if (!confirm("ยืนยันการทำรายการ ?")) return;
-            e.preventDefault();
-
-            $('.alert-danger').html('');
-            $('.alert-danger').hide();
-            $('.alert-success').html('');
-            $('.alert-success').hide();
-
-            if ($('#ecustomCheckbox1').is(":checked")) {
-                esstatus = 1;
-            } else {
-                esstatus = 0;
-            }
-
-            $.ajax({
-                url: "casetype/save/" + id,
-                method: 'PUT',
-                data: {
-                    name: $('#EditName').val(),
-                    status: esstatus,
-                },
-
-                success: function(result) {
-                    //console.log(result);
-                    if (result.errors) {
-                        $('.alert-danger').html('');
-                        $.each(result.errors, function(key, value) {
-                            $('.alert-danger').show();
-                            $('.alert-danger').append('<strong><li>' + value +
-                                '</li></strong>');
-                        });
-                    } else {
-                        $('.alert-danger').hide();
-                        $('.alert-success').show();
-                        $('.alert-success').append('<strong><li>' + result.success +
-                            '</li></strong>');
-                        $('#EditModal').modal('hide');
-                        toastr.success(result.success, {
-                            timeOut: 5000
-                        });
-                        $('#Listview').DataTable().ajax.reload();
-                        //setTimeout(function() {
-                        //$('.alert-success').hide();
-
-                        //}, 10000);
-
-                    }
-                }
-            });
-        });
-
-        $(document).on('click', '.btn-delete', function() {
-            if (!confirm("ยืนยันการทำรายการ ?")) return;
-
-            var rowid = $(this).data('rowid')
-            var el = $(this)
-            if (!rowid) return;
-            $.ajax({
-                type: "POST",
-                dataType: 'JSON',
-                url: "casetype/destroy/",
-                data: {
-                    id: rowid,
-                    _method: 'delete',
-                    _token: token
-                },
-                success: function(data) {
-                    console.log(data);
-                    if (data.success) {
-                        toastr.success(data.message, {
-                            timeOut: 5000
-                        });
-                        table.row(el.parents('tr'))
-                            .remove()
-                            .draw();
-                    }
-                }
-            });
-        })
 
         $(document).on("change", ".casetypechang", function() {
             let levcase = $(this).data("lev");
@@ -675,6 +396,7 @@
                 }
             });
         });
+        $(".casetypechang").trigger("change");
     });
     function loadcrm(pid, crmlev){
         $.ajax({
@@ -738,4 +460,5 @@
             }
         });
     }
+    
 </script>
