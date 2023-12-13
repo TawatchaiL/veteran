510c365214aba71f43c0613e28ba39600d9d2989
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-13 21:55:20 +0700

    test

--- resources/views/casetype/index.blade.php
+++ resources/views/casetype/index.blade.php
@@ -4,6 +4,11 @@
 @endsection
 
 @section('content')
+    <section class="content-header">
+        <div class="container-fluid">
+        </div>
+    </section>
+
     <section class="content">
         <div class="container-fluid">
             <div class="row">
@@ -18,52 +23,73 @@
                                 {{-- <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
                                     <i class="fas fa-times"></i>
                                 </button> --}}
+                                <div class="alert alert-danger alert-dismissible fade show" role="alert" style="display: none;">
+                                    <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="alert alert-success alert-dismissible fade show" role="alert" style="display: none;">
+                
+                                    <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true"></span>
+                                    </button>
+                                </div>
+                                <div class="row">
+                                    <div class="col-xs-5 col-sm-5 col-md-5" id="targetselect">
+                                        <div class="row mb-3 case1">
+                                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                                <select style="width: 100%;"
+                                                    class="select2 form-control casetypechang" id="casetype1" data-lev="1" data-parent="0" name="casetype1">
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3 case2">
+                                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                                <select style="width: 100%;"
+                                                    class="select2 form-control casetypechang" id="casetype2" data-lev="2" name="casetype2">
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3 case3">
+                                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                                <select style="width: 100%;"
+                                                    class="select2 form-control casetypechang" id="casetype3" data-lev="3" name="casetype3">
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3 case4">
+                                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                                <select style="width: 100%;"
+                                                    class="select2 form-control casetypechang" id="casetype4" data-lev="4" name="casetype4">
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3 case5">
+                                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                                <select style="width: 100%;"
+                                                    class="select2 form-control casetypechang" id="casetype5" data-lev="5" name="casetype5">
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="row mb-3 case6">
+                                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                                <select style="width: 100%;"
+                                                    class="select2 form-control casetypechang" id="casetype6" data-lev="6" name="casetype6">
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-7 col-sm-7 col-md-7" id="targettext">
+                                        <div class="row mb-3"><div class="col-xs-12 col-sm-12 col-md-12"><div class="input-group"><input type="text" id="newcasetype" name="newcasetype[]" class="form-control has-feedback-left" value="" required="required">&nbsp;<button type="button" class="btn btn-success" id="SubmitNewCasetype"><i class="fa-solid fa-plus"></i>เพิ่ม</button></div></div></div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-
-                        <div class="card-body">
-                            @if ($message = Session::get('success'))
-                                {{--  <div class="alert alert-success">
-                                    <p>{{ $message }}</p>
-                                </div> --}}
-                                <script>
-                                    toastr.success('{{ $message }}', {
-                                        timeOut: 5000
-                                    });
-                                </script>
-                            @endif
-                            <form method="post" action="{{ route('casetype6.destroy_all') }}" name="delete_all"
-                                id="delete_all">
-                                @csrf
-                                @method('POST')
-                                <table id="Listview" class="table table-bordered table-striped table-hover">
-                                    <thead>
-                                        <tr>
-                                            <th width="80px"><input type="checkbox" id="check-all" class="flat"></th>
-                                            <th>ประเภทการติดต่อ</th>
-                                            <th>สถานะ</th>
-                                            <th width="120px"></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-
-                                    </tbody>
-                                </table>
-                            </form>
-                        </div>
-
                     </div>
-
-
-
                 </div>
-
             </div>
-
         </div>
-
     </section>
-
     @include('casetype.create')
 
     @include('casetype.edit')
