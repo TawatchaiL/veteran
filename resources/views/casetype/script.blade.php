b1fd70b6e8cc2b066b526584d1e7cf4449b3b8a5
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-13 16:01:16 +0700

    etst

--- resources/views/casetype/script.blade.php
+++ resources/views/casetype/script.blade.php
@@ -537,7 +537,6 @@
             var id = $(this).data("id");
             var pid = $(this).data("pid");
             var crmlev = $(this).data("crmlev");
-            alert(crmlev);
             $.ajax({
                 url: "{{ route('casetype.store') }}",
                 method: 'post',
@@ -559,8 +558,36 @@
                         $('.alert-success').html('');
                         $('.alert-danger').hide();
                         $('.alert-success').show();
-                        $('.alert-success').append('<strong><li>' + result.success +
-                            '</li></strong>');
+                        $('.alert-success').append('<strong><li>' + result.success + '</li></strong>');
+                        $.ajax({
+                            url: "casetype/casetype/" + pid,
+                            method: 'GET',
+                            async: false,
+                            success: function(res) {
+                                var caseOb = $('#casetype' + nextcase);
+                                    caseOb.attr('disabled', false);
+                                var countres = res.data.length - 1;
+                                    $('#targettext').html('');
+                                $.each(res.data, function(index, item) {
+                                    htmltargettext = '<div class="row mb-3"><div class="col-xs-12 col-sm-12 col-md-12"><div class="input-group">'
+                                        + '<input type="text" class="form-control has-feedback-left text-editcasetype" id="editcasetype' + item.id + '" value="' + item.name + '" required="required" disabled>' 
+                                        + '&nbsp;<button type="button" class="btn btn-success btn-editcasetype" id="btneditcasetype' + item.id + '" data-id="' + item.id + '"><i class="fa-regular fa-pen-to-square"></i>แก้ไข</button>' 
+                                        + '&nbsp;<button type="button" class="btn btn-danger btn-editcancel" id="btneditcancel' + item.id + '" data-id="' + item.id + '" data-oldvalue="' + item.name + '" disabled><i class="fa-regular fa-rectangle-xmark"></i>ยกเลิก</button>'; 
+                                        if(index > 0){
+                                            htmltargettext += '&nbsp;<button type="button" class="btn btn-primary btn-editup" data-id="' + item.id + '" data-crmlist="' + item.crmlist + '" data-upid="' + res.data[index-1].id + '" data-upcrmlist="' + res.data[index-1].crmlist + '"><i class="fa-solid fa-angle-up"></i></button>'; 
+                                        }else{
+                                            htmltargettext += '&nbsp;<button type="button" class="btn btn-primary" data-id="' + item.id + '" disabled><i class="fa-solid fa-angle-up"></i></button>';     
+                                        }
+                                        if(index < countres){
+                                            htmltargettext += '&nbsp;<button type="button" class="btn btn-primary btn-editdown" data-id="' + item.id + '" data-crmlist="' + item.crmlist + '" data-downid="' + res.data[index+1].id + '" data-downcrmlist="' + res.data[index+1].crmlist + '"><i class="fa-solid fa-angle-down"></i></button>'; 
+                                        }else{
+                                            htmltargettext += '&nbsp;<button type="button" class="btn btn-primary" data-id="' + item.id + '" disabled><i class="fa-solid fa-angle-down"></i></button>';     
+                                        }
+                                        htmltargettext += '</div></div></div>';
+                                    $('#targettext').append(htmltargettext);
+                                });
+                            }
+                        });
                     }
                 }
             });
