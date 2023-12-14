5f7658b1ce91104fa01fea616ef00b3dbd2967cf
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-14 20:35:46 +0700

    test

--- resources/views/reportcasetop10/script.blade.php
+++ resources/views/reportcasetop10/script.blade.php
@@ -24,7 +24,7 @@
 
             var chartContainer = document.querySelector("#bar_graph");
             var logoImage = new Image();
-            logoImage.src = 'images/logo.png'; 
+            logoImage.src = 'public/images/logo.png'; 
 
             html2canvas(chartContainer).then(canvas => {
                 var imgData = canvas.toDataURL("image/png");
