fb1382c1cb0fa8804d2da45bf0c891c06f61deeb
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-15 14:29:13 +0700

    test

--- resources/views/reportcasetop10/script.blade.php
+++ resources/views/reportcasetop10/script.blade.php
@@ -553,11 +553,12 @@
                             return {
                                 columns: [
                                     {
-                                        
-                                        text: [
+                                        text: [  
+                                                { text: 'REPORT', alignment: 'right', fontSize: 42, margin: [0, 50, 70, 0] },
+                                                '\n',
                                                 { text: 'Top 10 (10 อันดับเรื่องที่ติดต่อมากที่สุด)', alignment: 'left', fontSize: 18, margin: [0, 50, 70, 0] },
                                                 '\n',
-                                                { text: 'Report By : {{ Auth::user()->name }}', alignment: 'center', fontSize: 18, margin: [0, 0, 70, 0] }
+                                                { text: 'Report By : {{ Auth::user()->name }}', alignment: 'left', fontSize: 18, margin: [0, 0, 70, 0] }
                                             ]
                                     }
                                 ],
