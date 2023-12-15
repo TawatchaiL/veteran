3cd05a03aa003d090474348892c23d6ecd7faf59
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-15 12:27:58 +0700

    test

--- resources/views/layouts/popup_script.blade.php
+++ resources/views/layouts/popup_script.blade.php
@@ -281,13 +281,13 @@
                                                                             );
                                                                         }); 
                                                                         */
-                                    
+
                                     $('#custom-tabs-one-casedetail-tabp' +
                                         cardId).click();
                                 }
                             }
                         });
-
+                        $('#cityp' + cardId).val('65').change();
                         setTimeout(function() {
                             $.ajax({
                                 url: "casetype6/casetype/0",
@@ -890,13 +890,15 @@
                                 */
                                 }
 
+                               
+
                                 if (res.datax.cases.length != 0) {
                                     $('#custom-tabs-one-casedetail-tabp' + cardId)
                                         .click();
                                 }
                             }
                         });
-
+                        $('#cityp' + cardId).val('65').change();
                         $.ajax({
                             url: "casetype6/casetype/0",
                             method: 'GET',
