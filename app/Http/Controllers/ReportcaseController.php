4af3a46e8b7bc1f0794ecdaacfc2f19a88f98800
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-15 21:05:57 +0700

    test

--- app/Http/Controllers/ReportcaseController.php
+++ app/Http/Controllers/ReportcaseController.php
@@ -56,35 +56,11 @@ class ReportcaseController extends Controller
             ->having('sumcases', '>', 0)
             ->orderBy("crm_id", "asc")
             ->get();
-
-        $datas = DB::connection('remote_connection')
-            ->table(DB::raw('SELECT @rownumber:=@rownumber + 1 AS rownumber, t.* FROM (SELECT crm_id, COUNT(crm_id) AS sumcases FROM call_center.call_entry WHERE datetime_init BETWEEN "' . $startDate . '" AND "' . $endDate . '" GROUP BY crm_id HAVING sumcases > 0 ORDER BY crm_id ASC) t, (SELECT @rownumber:=0) r'))
-            //->select('rownumber', 'crm_id', 'sumcases')
-            ->get();
 */
-$datas = DB::connection('remote_connection')
-    ->select(DB::raw('
-        SELECT
-            @rownum := @rownum + 1 AS rownumber,
-            t.*
-        FROM (
-            SELECT
-                crm_id,
-                COUNT(crm_id) AS sumcases
-            FROM
-                call_center.call_entry
-            WHERE
-                datetime_init BETWEEN "' . $startDate . '" AND "' . $endDate . '"
-            GROUP BY
-                crm_id
-            HAVING
-                sumcases > 0
-            ORDER BY
-                crm_id ASC
-        ) AS t,
-        (SELECT @rownum := 0) r;
-    '))
-    ->get();
+        $datas = DB::connection('remote_connection')
+            ->table(DB::raw('(SELECT @rownumber:=@rownumber + 1 AS rownumber, t.* FROM (SELECT crm_id, COUNT(crm_id) AS sumcases FROM call_center.call_entry WHERE datetime_init BETWEEN "' . $startDate . '" AND "' . $endDate . '" GROUP BY crm_id HAVING sumcases > 0 ORDER BY crm_id ASC) t, (SELECT @rownumber:=0) r) AS temp'))
+            ->select('rownumber', 'crm_id', 'sumcases')
+            ->get();
 
             $agents = User::orderBy("id", "asc")->get();
             $agent_data = array();
