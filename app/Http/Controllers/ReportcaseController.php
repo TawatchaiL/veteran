89e30be70de94d7d3421e098dcab13075cafe802
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-15 21:01:49 +0700

    test

--- app/Http/Controllers/ReportcaseController.php
+++ app/Http/Controllers/ReportcaseController.php
@@ -62,18 +62,29 @@ class ReportcaseController extends Controller
             //->select('rownumber', 'crm_id', 'sumcases')
             ->get();
 */
-            $datas = DB::connection('remote_connection')
-            ->table('call_center.call_entry')
-            ->select(
-                DB::raw('(@rownumber := @rownumber + 1) AS rownumber'),
-                'crm_id',
-                DB::raw('count(crm_id) as sumcases')
-            )
-            ->whereRaw('datetime_init BETWEEN "' . $startDate . '" AND "' . $endDate . '"')
-            ->groupBy('crm_id')
-            ->having('sumcases', '>', 0)
-            ->orderBy('crm_id', 'asc')
-            ->get();
+$datas = DB::connection('remote_connection')
+    ->select(DB::raw('
+        SELECT
+            @rownum := @rownum + 1 AS rownumber,
+            t.*
+        FROM (
+            SELECT
+                crm_id,
+                COUNT(crm_id) AS sumcases
+            FROM
+                call_center.call_entry
+            WHERE
+                datetime_init BETWEEN "' . $startDate . '" AND "' . $endDate . '"
+            GROUP BY
+                crm_id
+            HAVING
+                sumcases > 0
+            ORDER BY
+                crm_id ASC
+        ) AS t,
+        (SELECT @rownum := 0) r;
+    '))
+    ->get();
 
             $agents = User::orderBy("id", "asc")->get();
             $agent_data = array();
