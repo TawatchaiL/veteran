f0972bf354df40875c921607e846522fae368638
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-15 20:40:51 +0700

    test

--- app/Http/Controllers/SumtelController.php
+++ app/Http/Controllers/SumtelController.php
@@ -51,7 +51,7 @@ class SumtelController extends Controller
 
         $datas = DB::connection('remote_connection')
             ->table(DB::raw('(SELECT @rownumber:=@rownumber + 1 AS rownumber, t.* FROM (SELECT DATE(datetime_init) as cdate, SUM(if(status = "terminada",1,0)) as terminada, SUM(if(status = "abandonada",1,0)) as abandonada FROM call_center.call_entry WHERE datetime_init BETWEEN "' . $startDate . '" AND "' . $endDate . '" GROUP BY cdate ORDER BY cdate ASC) t, (SELECT @rownumber:=0) r) AS temp'))
-            ->select('rownumber', 'crm_id', 'sumcases')
+            ->select('rownumber', 'cdate', 'terminada', 'abandonada')
             ->get();
 /*
         $datas = DB::connection('remote_connection')
