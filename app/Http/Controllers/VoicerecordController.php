b45a8da6c9c4f880f9bd0dac7b16591bb2e1d669
manon618 <manon2029@gmail.com>
2023-12-16 17:30:28 +0700

    fix

--- app/Http/Controllers/VoicerecordController.php
+++ app/Http/Controllers/VoicerecordController.php
@@ -99,9 +99,9 @@ class VoicerecordController extends Controller
                         //->where('asteriskcdrdb.cdr.dst_userfield', '=', NULL);
                 } else if ($ctype == 3) {
                     //$datass->where('asteriskcdrdb.cdr.accountcode', '!=', '')
-                    $datass->where('asteriskcdrdb.cdr.recordingfile', 'like', 'exten-%');
+                    $datass->where('asteriskcdrdb.cdr.recordingfile', 'like', 'exten-%')
                         //->where('asteriskcdrdb.cdr.userfield', '!=', '')
-                        //->where('asteriskcdrdb.cdr.dst_userfield', '!=', NULL);
+                        ->where('asteriskcdrdb.cdr.dst_userfield', '!=', NULL);
                 }
             }
 
