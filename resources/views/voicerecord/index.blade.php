e428fb03434829bf5e0c80f7ffd6c4f91490aaff
manon618 <manon2029@gmail.com>
2023-12-17 16:32:30 +0700

    fix

--- resources/views/voicerecord/index.blade.php
+++ resources/views/voicerecord/index.blade.php
@@ -91,7 +91,7 @@
                                     <div class="col-xs-2 col-sm-2 col-md-2 align-self-end ">
                                         <div class="form-group">
                                             <a class="btn btn-success" id="searchButton"> <i class="fas fa-search"></i>
-                                                </a>
+                                            </a>
                                             <a class="btn bg-warning" id="resetSearchButton"> <i
                                                     class="fa-solid fa-rotate"></i></a>
                                         </div>
@@ -142,8 +142,10 @@
                             {{-- <button class="changeUrlButton">Change URL</button> --}}
                             <div class="col-xs-12 col-sm-12 col-md-12 align-self-end text-right">
                                 <div class="form-group">
-                                    <a class="btn btn-warning" id="exportVoiceButton" {{-- href="{{ route('reportcase.pdf') }}" --}}>
-                                        <i class="fa-solid fa-file-export"></i> Export </a>
+                                    @can('voice-record-supervisor')
+                                        <a class="btn btn-warning" id="exportVoiceButton" {{-- href="{{ route('reportcase.pdf') }}" --}}>
+                                            <i class="fa-solid fa-file-export"></i> Export </a>
+                                    @endcan
                                     <a class="btn btn-danger" id="exportPDFButton" {{-- href="{{ route('reportcase.pdf') }}" --}}>
                                         <i class="fa-regular fa-file-pdf"></i> PDF </a>
                                     <a class="btn btn-success" id="exportXLSButton" {{-- href="{{ route('reportcase.pdf') }}" --}}>
