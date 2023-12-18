5f115bb0227bf4488707a7b906bfdcc5dec655f7
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-18 13:01:04 +0700

    test

--- resources/views/reportcasetop10/script.blade.php
+++ resources/views/reportcasetop10/script.blade.php
@@ -11,31 +11,41 @@
             bolditalics: '{{ asset('fonts/THSarabunNew BoldItalic.ttf') }}'
         }
     }
-    $(document).ready(function() {
-        $('#download_bar').click(function(event) {
+    
+    $('#download_bar').click(function(event) {
+    var pdfWidth = 595.28;
+    var pdfHeight = 841.89;
+    var pdf = new jsPDF({
+        unit: 'pt',
+        format: [pdfWidth, pdfHeight]
+    });
 
-            var pdfWidth = 595.28;
-            var pdfHeight = 841.89;
-            var pdf = new jsPDF({
-                unit: 'pt',
-                format: [pdfWidth, pdfHeight]
-            });
+    // Custom header
+    var headerText = "Custom Header Text";
+    pdf.setProperties({
+        title: headerText,
+        subject: headerText,
+        author: "Your Name",
+        keywords: "PDF, Custom Header, Bar Chart",
+    });
 
-            var chartContainer = document.querySelector("#bar_graph");
+    // Set custom header content (you can also use addHTML method for more complex content)
+    pdf.text(headerText, pdfWidth / 2, 30, { align: 'center' });
 
-            html2canvas(chartContainer).then(canvas => {
-                var imgData = canvas.toDataURL("image/png");
+    var chartContainer = document.querySelector("#bar_graph");
 
-                var imgWidth = pdfWidth;
-                var imgHeight = (canvas.height * imgWidth) / canvas
-                    .width;
+    html2canvas(chartContainer).then(canvas => {
+        var imgData = canvas.toDataURL("image/png");
 
-                pdf.addImage(imgData, 'PNG', 0, 60, imgWidth,
-                    imgHeight);
-                pdf.save("bar_chart.pdf");
-            });
+        var imgWidth = pdfWidth;
+        var imgHeight = (canvas.height * imgWidth) / canvas.width;
 
-        });
+        // Adjust the vertical position based on the header height
+        pdf.addImage(imgData, 'PNG', 0, 90, imgWidth, imgHeight);
+
+        pdf.save("bar_chart.pdf");
+    });
+});
 
         $('#download_bar_img').click(function(event) {
             var chartContainer = document.querySelector("#bar_graph");
