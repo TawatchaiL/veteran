e11d51dcaf6146b2e5352a535089bcf38e73916e
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-18 21:18:18 +0700

    tss

--- app/Http/Controllers/DetailcaseexternalnumberController.php
+++ app/Http/Controllers/DetailcaseexternalnumberController.php
@@ -47,6 +47,11 @@ class DetailcaseexternalnumberController extends Controller
             $startDate = date("Y-m-d H:i:s");
             $endDate = date("Y-m-t H:i:s", strtotime($startDate));  
         }
+        if(!empty($request->get('agent')) && $request->get('agent') != "0"){
+            $sqlagent = " and crm_id = '".$request->input('agent')."'";
+        }else{
+            $sqlagent = "";
+        }    
         $datas = DB::connection('remote_connection')
         ->table(DB::raw('(SELECT @rownumber:=@rownumber + 1 AS rownumber, t.* FROM (SELECT DATE(datetime_init) as cdate, TIME(datetime_init) as ctime, callerid as telno, crm_id as agentid, SEC_TO_TIME(duration) as duration, SEC_TO_TIME(duration_wait) as duration_wait FROM call_center.call_entry WHERE LENGTH(callerid) > 5 AND datetime_init BETWEEN "' . $startDate . '" AND "' . $endDate . '"' .$sqlagent. ' ORDER BY datetime_init) t, (SELECT @rownumber:=0) r) AS temp'))
         ->select('rownumber', 'cdate', 'ctime', 'telno', 'agentid', 'duration', 'duration_wait');
