4cb66ecc68a6660b1d7f9ef62bb24108aa2a66a3
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-19 15:11:49 +0700

    ะำหะ

--- app/Http/Controllers/DetailscoreagentController.php
+++ app/Http/Controllers/DetailscoreagentController.php
@@ -83,7 +83,7 @@ class DetailscoreagentController extends Controller
                 }
                 $chart_data[1]=[10,15,23,5,9];
                 $chart_data[2]=[9,5,8,4,1];
-                $chart_label=[1,2,3,4,5];
+                $chart_label=[1,5];
                 return response()->json(['datag' => $chart_data,'datal' => $chart_label]);
             }
 
