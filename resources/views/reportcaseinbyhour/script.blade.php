9bb5f2eb490175a26e931c9b3536bffbac3d9c78
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-19 08:56:27 +0700

    test

--- resources/views/reportcaseinbyhour/script.blade.php
+++ resources/views/reportcaseinbyhour/script.blade.php
@@ -589,7 +589,7 @@
                             }
                         });
 
-                        doc.content[0].table.widths = [40,400, '*'];
+                        doc.content[0].table.widths = [400, '*'];
                         var objLayout = {};
 						objLayout['hLineWidth'] = function(i) { return .5; };
 						objLayout['vLineWidth'] = function(i) { return .5; };
