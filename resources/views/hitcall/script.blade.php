2b8b272e2ecec3639b2631c365aba234ff580a9f
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-21 22:26:04 +0700

    dsfsf

--- resources/views/hitcall/script.blade.php
+++ resources/views/hitcall/script.blade.php
@@ -395,7 +395,7 @@
 						doc.content[0].layout = objLayout;
 
                         for (var i = 1; i < doc.content[0].table.body.length; i++) {
-                            doc.content[0].table.body[i][0].alignment = 'left';
+                            doc.content[0].table.body[i][0].alignment = 'center';
                             doc.content[0].table.body[i][1].alignment = 'center';
                             doc.content[0].table.body[i][2].alignment = 'center';
                             doc.content[0].table.body[i][3].alignment = 'center';
