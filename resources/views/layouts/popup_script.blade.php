756ab9792695568233c80e95bb350b30e7809038
manon618 <manon2029@gmail.com>
2023-12-21 18:29:33 +0700

    fix

--- resources/views/layouts/popup_script.blade.php
+++ resources/views/layouts/popup_script.blade.php
@@ -65,6 +65,11 @@
                 cardId: cardId
             },
             success: async function(response) {
+                $(".select2_single").select2({
+                    maximumSelectionLength: 1,
+                    placeholder: 'กรุณาเลือก'
+                });
+
                 $('#' + cardId).removeClass('card-danger');
                 $('#' + cardId).addClass('card-success');
                 await $('#pop_' + cardId).html(response.html);
@@ -1110,7 +1115,7 @@
                     url: "{{ route('contacts.casescontract') }}",
                     method: 'post',
                     data: additionalData,
-                    async : false,
+                    async: false,
                     success: function(result) {
                         if (result.errors) {
                             $('.alert-danger-pop' + cardId).html('');
@@ -1428,19 +1433,19 @@
                 $confirm.modal("hide");
             });
         }
-/*
-        $(".select2_single").select2({
-            maximumSelectionLength: 1,
-            //allowClear: true,
-            //theme: 'bootstrap4'
-            placeholder: 'กรุณาเลือก'
-        });
+        /*
+                $(".select2_single").select2({
+                    maximumSelectionLength: 1,
+                    //allowClear: true,
+                    //theme: 'bootstrap4'
+                    placeholder: 'กรุณาเลือก'
+                });
 
-        $(".select2_single").on("select2:unselect", function(e) {
-            //log("select2:unselect", e);
-            //$('.products').html('');
-        });
- */
+                $(".select2_single").on("select2:unselect", function(e) {
+                    //log("select2:unselect", e);
+                    //$('.products').html('');
+                });
+         */
         /* $(document).on('show.bs.tab', '#custom-tabs-pop a[data-toggle="pill"]',
             function(e) {
 
