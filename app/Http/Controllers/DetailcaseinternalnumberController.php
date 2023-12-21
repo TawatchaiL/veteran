602fb7c2bea6d7c62f371f317a658b31492742b5
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-21 23:07:00 +0700

    sfsf

--- app/Http/Controllers/DetailcaseinternalnumberController.php
+++ app/Http/Controllers/DetailcaseinternalnumberController.php
@@ -57,34 +57,8 @@ class DetailcaseinternalnumberController extends Controller
         $datas = DB::connection('remote_connection')
             ->table(DB::raw('(SELECT @rownumber:=0) AS temp, call_center.call_entry'))
             ->select(DB::raw('(@rownumber:=@rownumber + 1) AS rownumber'), DB::raw('DATE(datetime_init) as cdate'), DB::raw('TIME(datetime_init) as ctime'),'callerid as telno','crm_id as agentid', DB::raw('SEC_TO_TIME(duration) as duration'), DB::raw('SEC_TO_TIME(duration_wait) as duration_wait'))
-            ->whereRaw('LENGTH(callerid) < 5 and datetime_init between "' . $startDate . '" and "' . $endDate . '"'.$sqlagent.' ORDER BY datetime_init DESC')
-        //if(!empty($request->get('agent')) && $request->get('agent') != "0"){
-        //    $datasql->whereRaw('crm_id = "'. $request->input('agent') .'"');  
-        //}    
-        //$datasql->orderBy(DB::raw('ORDER BY datetime_init'))
-        ->get();
-/* 
-        $datas = DB::connection('remote_connection')
-                ->table(DB::raw("(SELECT @rownumber:=@rownumber + 1 AS rownumber, t.* FROM ($datasql) t, (SELECT @rownumber:=0) r) AS temp"))
-                ->select('rownumber', 'cdate', 'ctime', 'telno', 'agentid', 'duration', 'duration_wait')
-                ->get();
-         
-
-        if(!empty($request->get('agent')) && $request->get('agent') != "0"){
-            $sqlagent = " and crm_id = '".$request->input('agent')."'";
-        }else{
-            $sqlagent = "";
-        }      
-        $datas = DB::connection('remote_connection')
-                ->table(DB::raw('(SELECT @rownumber:=@rownumber + 1 AS rownumber, t.* FROM (SELECT DATE(datetime_init) as cdate, TIME(datetime_init) as ctime, callerid as telno, crm_id as agentid, SEC_TO_TIME(duration) as duration, SEC_TO_TIME(duration_wait) as duration_wait FROM call_center.call_entry WHERE LENGTH(callerid) < 6 AND datetime_init BETWEEN "' . $startDate . '" AND "' . $endDate . '"' .$sqlagent. ' ORDER BY datetime_init) t, (SELECT @rownumber:=0) r) AS temp'))
-                ->select('rownumber', 'cdate', 'ctime', 'telno', 'agentid', 'duration', 'duration_wait');
-            //    ->get();
-            //if(!empty($request->get('agent')) && $request->get('agent') != "0"){
-            //    $datas->whereRaw('crm_id = "'. $request->input('agent') .'"');  
-            //}    
-            //->limit(10)
-            $datas->get();
-*/
+            ->whereRaw('LENGTH(callerid) < 6 and datetime_init between "' . $startDate . '" and "' . $endDate . '"'.$sqlagent.' ORDER BY datetime_init DESC')
+            ->get();
 
             $agents = User::orderBy("id", "asc")->get();
 
