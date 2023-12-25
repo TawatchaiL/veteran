ca6961b881cc25bd45fc20b1b7bb09140efaa074
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-25 23:14:53 +0700

    test

--- app/Http/Controllers/MisscallController.php
+++ app/Http/Controllers/MisscallController.php
@@ -69,7 +69,7 @@ class MisscallController extends Controller
         $datas = DB::connection('remote_connection')
             ->table(DB::raw('(SELECT @rownumber:=0) AS temp, asteriskcdrdb.cdr'))
             ->select(DB::raw('(@rownumber:=@rownumber + 1) AS rownumber'), 'dst_userfield as crm_id', DB::raw('calldate as cdate'), DB::raw('TIME(calldate) as ctime'), 'src as telno', DB::raw('SEC_TO_TIME(duration) as durationwait'))
-            ->whereRaw('calldate between "' . $startDate . '" and "' . $endDate . '" AND disposition = "NO ANSWER" AND  dst_userfield is not null'.$sqlagent.' ORDER BY calldate desc');
+            ->whereRaw('calldate between "' . $startDate . '" and "' . $endDate . '" AND disposition = "NO ANSWER" AND  dst_userfield is not null'.$sqlagent.'');
         //if (!empty($request->get('agent'))) {
         //    $datas->whereRaw('dst_userfield = "' . $request->input('agent') . '"');
         //}
