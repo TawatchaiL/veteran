9fff954d920659137c1aad23abff98ab64630919
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-25 21:18:37 +0700

    tes

--- resources/views/ivrreport/script.blade.php
+++ resources/views/ivrreport/script.blade.php
@@ -484,7 +484,7 @@
             },
 
             aaSorting: [
-                [0, "asc"]
+                [1, "asc"]
             ],
             iDisplayLength: 10,
             lengthMenu: [5, 10, 25, 50, 75, 100],
