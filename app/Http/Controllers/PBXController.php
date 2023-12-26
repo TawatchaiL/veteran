56598f58b75788a438ad28dd195e866180c789ae
manon618 <manon2029@gmail.com>
2023-12-26 22:27:07 +0700

    fix

--- app/Http/Controllers/PBXController.php
+++ app/Http/Controllers/PBXController.php
@@ -501,7 +501,7 @@ class PBXController extends Controller
                     ->where('call_status', 0)
                     ->orderBy('job_number_id', 'asc')
                     ->first();
-                //dd($request->input('telno'));
+                dd($outbound);
                 if (!empty($outbound)) {
                     DB::table('crm_incoming')
                         ->where('telno', $request->input('telno'))
