8976e4ca4c17bf88a3013052b458a89ac8deda7c
TawatchaiL <tawatchai.winit@gmail.com>
2023-12-26 22:14:11 +0700

    test

--- app/Http/Controllers/DetailscoreagentController.php
+++ app/Http/Controllers/DetailscoreagentController.php
@@ -93,11 +93,11 @@ class DetailscoreagentController extends Controller
                 $a = 0;
                 foreach ($datatt as $keys => $values) {
                         $datat[$a]['name'] = 'test'.$a;
-                        $datat[$a]['data'][0] = 1;
-                        $datat[$a]['data'][1] = 2;
-                        $datat[$a]['data'][2] = 3;
-                        $datat[$a]['data'][3] = 4;
-                        $datat[$a]['data'][4] = 5;
+                        $datat[$a]['data'] = array(10, 15, 23, 5, 9);
+                        //$datat[$a]['data'][1] = 2;
+                        //$datat[$a]['data'][2] = 3;
+                        //$datat[$a]['data'][3] = 4;
+                        //$datat[$a]['data'][4] = 5;
 
                         $a++;
                 }
