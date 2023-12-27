50c5de75b4242dc8f6af129c20623e0c365eaa39
Taitawatchai <sc_player01@hotmail.com>
2023-12-27 10:30:55 +0700

    ss

--- resources/views/detailscoreagent/script.blade.php
+++ resources/views/detailscoreagent/script.blade.php
@@ -814,7 +814,7 @@ html2canvas(chartContainer).then(canvas => {
                     //offsetX: 0
                     position: 'right',
                     offsetY: 20,
-                    offsetX: -200,
+                    offsetX: 500,
                     height: 230,
                 }
                 };
