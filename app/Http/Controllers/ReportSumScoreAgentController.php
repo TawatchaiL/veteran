c0e2ce9b1d0ae21eafa996d509872081703dc905
Taitawatchai <sc_player01@hotmail.com>
2023-12-28 12:06:45 +0700

    test

--- app/Http/Controllers/ReportSumScoreAgentController.php
+++ app/Http/Controllers/ReportSumScoreAgentController.php
@@ -74,11 +74,11 @@ class ReportSumScoreAgentController extends Controller
                 foreach ($datas as $data) {
                     if (array_key_exists($data->crm_id, $agent_data)) {
                         $chart_data[] = $data->sumscore;
-                        $chart_label[] = $data->sumscore;
+                        $chart_label[] = "x".$data->sumscore."X";
                         //$chart_label[] = $agent_data[$data->crm_id];
                     }else{
                         $chart_data[] = $data->sumscore;
-                        $chart_label[] = $data->sumscore;
+                        $chart_label[] = "x".$data->sumscore."X";
                         //$chart_label[] = 'Agent not found';
                     }
                 }
