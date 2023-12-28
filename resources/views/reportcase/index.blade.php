ca5e3d73e31a178fd2529f37ceaba1362bcd1a99
Taitawatchai <sc_player01@hotmail.com>
2023-12-28 10:36:36 +0700

    ะำหะ

--- resources/views/reportcase/index.blade.php
+++ resources/views/reportcase/index.blade.php
@@ -13,15 +13,15 @@
               <div class="row mb-2">
                 <div class="col-sm-6">
                   <h1 class="m-0"></h1>
-                </div><!-- /.col -->
+                </div>
                 <div class="col-sm-6">
                   <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="{{ route('report') }}">Report</a></li>
                     <li class="breadcrumb-item active">Callcenter Report</li>
                   </ol>
-                </div><!-- /.col -->
-              </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
+                </div>
+              </div>
+            </div>
         </div>
         <div class="container-fluid">
             <div class="row">
--- resources/views/reportcasebyagent/index.blade.php
+++ resources/views/reportcasebyagent/index.blade.php
@@ -14,6 +14,21 @@
                 <div class="col-sm-6">
                   <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="{{ route('report') }}">Report</a></li>
+                    <li class="breadcrumb-item active">IVR & Call Survey Report</li>
+                  </ol>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="content-header">
+            <div class="container-fluid">
+              <div class="row mb-2">
+                <div class="col-sm-6">
+                  <h1 class="m-0"></h1>
+                </div>
+                <div class="col-sm-6">
+                  <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="{{ route('report') }}">Report</a></li>
                     <li class="breadcrumb-item active">Crm Report</li>
                   </ol>
                 </div>
