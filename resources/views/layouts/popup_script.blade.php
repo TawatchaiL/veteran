b9609bcd691fd7f6510caece6bc95b1cba33a2d0
Taitawatchai <sc_player01@hotmail.com>
2023-12-28 13:33:25 +0700

    test

--- resources/views/layouts/popup_script.blade.php
+++ resources/views/layouts/popup_script.blade.php
@@ -31,6 +31,7 @@
                 $('#hold_tab').html(response.hold_tab);
                 $('#hold_tab_content').html(response.hold_tab_content);
                 $('#hcomment_tab').html(response.chold_tab);
+                alert(response.chold_tab_content);
                 $('#hcomment_tab_content').html(response.chold_tab_content);
                 setTimeout(function() {
                     state_overlay.addClass("d-none");
