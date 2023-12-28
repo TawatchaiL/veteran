fd0302f05b9a6368e56bd3ae5f60f0957bbd6f96
Taitawatchai <sc_player01@hotmail.com>
2023-12-28 13:51:48 +0700

    ะำหะ

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -381,9 +381,9 @@ class ContactController extends Controller
             'tab_content' => $tab_content,
             'active_id' => $active_id,
             'hold_tab' => $i - 1,
-            'hold_tab_content' => $tab_hold
-            //'chold_tab' => $c - 1,
-            //'chold_tab_content' => $ctab_hold
+            'hold_tab_content' => $tab_holdม
+            'chold_tab' => $c - 1,
+            'chold_tab_content' => $ctab_hold
         ]);
     }
 
