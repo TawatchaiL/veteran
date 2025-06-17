deac1f7e015f7c1bbfb02b53b73255fc3a2a694e
manon618 <manon2029@gmail.com>
2025-06-17 10:13:57 +0700

    xxx

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,14 +1,15 @@
 <aside class="main-sidebar main-sidebar-custom sidebar-light-primary elevation-2">
-    <div class="center-content">
-        <a href="{{ route('home') }}" class="brand-link">
-            <img id="logo" src="{{ asset('images/logo.png') }}" alt="..." height="100"><br>
-            <span class="brand-text font-weight-light">{{ config('app.subtitle') }} {{ config('app.name') }} <br>
-                {{ config('app.name2') }}</span>
-        </a>
-    </div>
 
     <div class="sidebar">
-       {{--  <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="center-content">
+            <a href="{{ route('home') }}" class="brand-link">
+                <img id="logo" src="{{ asset('images/logo.png') }}" alt="..." height="100"><br>
+                <span class="brand-text font-weight-light">{{ config('app.subtitle') }} {{ config('app.name') }} <br>
+                    {{ config('app.name2') }}</span>
+            </a>
+        </div>
+
+        {{--  <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/2048px-User_icon_2.svg.png"
                 height="150" class="user-image img-circle elevation-1" alt="User Image">
