336f68c9fc80a7c006659d03d9eb1649e30ef53c
Taitawatchai <sc_player01@hotmail.com>
2025-08-28 12:40:14 +0700

    logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -74,7 +74,7 @@
 
 <body class="hold-transition login-page">
     <div class="login-box">
-        <div class="image-container"><img src="{{ asset('images/logo.png') }}" alt="..." height="160"></div>
+        <div class="image-container"><img src="{{ asset('images/logof.jpg') }}" alt="..." height="160"></div>
         <div class="login-logo">
             <a href="{{ url('/home') }}"><b> {{ config('app.subtitle') }} {{ config('app.name') }} <br>
                     {{ config('app.name2') }} </b></a>
