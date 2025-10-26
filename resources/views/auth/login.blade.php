2976ba9e6bb2c3bb83100c0d6f3bcb04f427ab1f
TawatchaiL <tawatchai.winit@gmail.com>
2025-10-26 15:09:33 +0700

    css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -145,7 +145,30 @@
             display: block;
             margin: 0 auto;
         }
-        
+     /* ปรับ input-group ให้ขอบเนียนไม่จาง */
+.input-group {
+    overflow: hidden; /* รวมขอบให้กลมและเนียน */
+    border-radius: 8px;
+    border: 1px solid #ccc;
+    background-color: #fff;
+}
+
+.input-group .form-control {
+    border: none !important;
+    box-shadow: none !important;
+}
+
+.input-group-append .input-group-text {
+    background-color: #005A41;
+    color: white;
+    border: none;
+    border-left: 1px solid #005A41;
+}
+
+.input-group .form-control:focus {
+    outline: none;
+    box-shadow: none;
+}   
 </style>
 
 <body class="hold-transition login-page">
