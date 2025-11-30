b7e3ec26b97bcb95c7040310ea1101fe89d1b9fc
TawatchaiL <tawatchai.winit@gmail.com>
2025-11-30 17:00:58 +0700

    ะำหะ

--- resources/views/contacts/contact-create.blade.php
+++ resources/views/contacts/contact-create.blade.php
@@ -30,10 +30,10 @@
         <div class="card card-success card-outline card-outline-tabs">
             <div class="card-header p-0 pt-1">
                 <ul class="nav nav-tabs" id="custom-tabs-one-tabp" role="tablist">
-                    <li class="pt-2 px-3" id="phonenosuccess{{ $cardid }}"> 123</li>
-                    {{--   <li class="pt-2 px-3">
-                        <h3 class="card-title" id="contact_name"></h3>
-                    </li> --}}
+                    <li class="pt-2 px-3" id="phonenosuccess{{ $cardid }}"> </li>
+                   <li class="pt-2 px-3">
+                        <h3 class="card-title" id="contact_name">123456</h3>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link active tablistcaseP" id="custom-tabs-one-casedetail-tabp{{ $cardid }}"
                             data-tabid="{{ $cardid }}" data-contactid="{{ $contactd }}" data-toggle="pill"
