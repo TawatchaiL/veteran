082a4bb029170d1b5da7c5e8714790acccadd8a9
TawatchaiL <tawatchai.winit@gmail.com>
2025-12-07 11:31:56 +0700

    tt

--- resources/views/voicerecordpbx/index.blade.php
+++ resources/views/voicerecordpbx/index.blade.php
@@ -49,6 +49,7 @@
                                                 autocomplete="off">
                                         </div>
                                     </div>
+                                    {{--
                                     <div class="col-xs-2 col-sm-2 col-md-2">
                                         <div class="form-group">
                                             <strong><i class="fas fa-user"></i>
@@ -62,6 +63,7 @@
                                             </select>
                                         </div>
                                     </div>
+                                     --}}
                                     <div class="col-xs-2 col-sm-2 col-md-2 align-self-end ">
                                         <div class="form-group">
                                             <a class="btn btn-success" id="searchButton"> <i class="fas fa-search"></i>
