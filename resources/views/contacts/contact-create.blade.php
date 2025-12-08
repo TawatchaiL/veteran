cb79c8b25897aed7bf5742b72f1c77c4db3cf042
Tawatchai <tawatchai.winit@gmail.com>
2025-12-08 12:11:45 +0700

    ธธ

--- resources/views/contacts/contact-create.blade.php
+++ resources/views/contacts/contact-create.blade.php
@@ -482,7 +482,7 @@
                                         :</strong>
                                     <select class="select2 select2_single form-control" id="tranferstatusp{{ $cardid }}"
                                         name="tranferstatusp{{ $cardid }}" multiple="multiple">
-                                        <option value="มีการโอนสาย"  selected>มีการโอนสาย</option>
+                                        <option value="มีการโอนสาย" selected>มีการโอนสาย</option>
                                         <option value="ไม่มีการโอนสาย">ไม่มีการโอนสาย</option>
                                         <option value="รับสาย">รับสาย</option>
                                         <option value="ไม่รับสาย">ไม่รับสาย</option>
