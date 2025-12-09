9edf218897e2fb4f116123c91aec6580c0e3b4ea
manon618 <manon2029@gmail.com>
2025-12-09 15:11:07 +0700

    เปิดการใช้งาน route agent_kick และ agent_kick_api

--- routes/web3.php
+++ routes/web3.php
@@ -44,7 +44,7 @@ Route::group(['middleware' => ['auth']], function () {
     Route::post('/swap', [App\Http\Controllers\PBXController::class, 'call_swap'])->name('swap');
     Route::post('/tranfer', [App\Http\Controllers\PBXController::class, 'call_tranfer'])->name('tranfer');
     Route::post('/tranfer_status', [App\Http\Controllers\PBXController::class, 'TranferStatus'])->name('tranfer_status');
-    //Route::post('/agent_kick', [App\Http\Controllers\PBXController::class, 'AgentKick'])->name('agent.kick');
+    Route::post('/agent_kick', [App\Http\Controllers\PBXController::class, 'AgentKick'])->name('agent.kick');
     
     Route::post('/pause_list', [App\Http\Controllers\PBXController::class, 'pause_list'])->name('pause_list');
     Route::post('/sup_logoff_agent', [App\Http\Controllers\PBXController::class, 'logoffAgentFromQueuebySup'])->name('sup.logoff_agent');
@@ -115,4 +115,4 @@ Route::group(['middleware' => ['auth']], function () {
 });
 
 Route::get('/agent_unwarp_api', [App\Http\Controllers\PBXController::class, 'AgentUnWarpAPI'])->name('agent.unwarp_api');
-//Route::get('/agent_kick_api', [App\Http\Controllers\PBXController::class, 'AgentKickApi'])->name('agent.kick_api');
+Route::get('/agent_kick_api', [App\Http\Controllers\PBXController::class, 'AgentKickApi'])->name('agent.kick_api');
